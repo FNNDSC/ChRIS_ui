431cc90b10d3a80b9578854fe1d59b3ab6aedeea
Rebeca <rkerr@devshop.info>
2019-02-04 15:57:58 -0500

    initial commit: general assets for react, typescript, sass, tsling, type definitions

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Working ChRIS UI
+      </div>
+    );
+  }
+}
+
+export default App;
