9e078395dab8a0383d4d36c348da1ed9568f0861
Rebeca Kerr <rkerr@devshop.info>
2019-02-06 11:46:32 -0500

    working header and background image

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,50 @@
 import * as React from 'react';
-import { 
-    PageHeader, 
-    Brand, 
+import {
+    PageHeader,
+    Brand,
     Avatar
- } from '@patternfly/react-core';
+    
+} from '@patternfly/react-core';
 import brandImg from '../../assets/images/logo_chris-dashboard.png';
 import avatarImg from '../../assets/images/avatar-250x250.png';
 
 
+// const PageToolbar = (
+//     <div>toolbar</div>
+// )
+// const avatar = (
+//     <Avatar src={avatarImg} alt="Avatar image" />
+// )
+// const Header = ({ ...props }) => (
+//     <PageHeader 
+//         logo={<Brand src={brandImg} alt="ChRIS Logo" />}
+//         toolbar={PageToolbar}
+//         avatar={avatar}
+//         aria-label="Page Header"
+//     />
+// )
 class Header extends React.Component {
-  
     render() {
-        const PageToolbar = (
+        const pageToolbar = (
             <div>toolbar</div>
         )
         const avatar = (
             <Avatar src={avatarImg} alt="Avatar image" />
         )
+        const brand = (
+            < Brand src={brandImg} alt="ChRIS Logo" />
+        )
+        
+        // NOTE: this is a way to get around a "Warning: Invalid attribute name: ``" bug in PageHeader ***** Working: will be revised ***** 
+        // The PageHeaderProps defaultProps comes in with and extra empty attribute. See line: 75 PageHeader.js in @patternfly/react-core
+        PageHeader.defaultProps = {
+            'aria-label': "Page Header",
+            'avatar': avatar,
+            'logo': brand,
+            'toolbar': pageToolbar,
+        }
         return (
-            <PageHeader
-                logo={<Brand src={brandImg} alt="ChRIS Logo" />}
-                toolbar={PageToolbar}
-                avatar={avatar}
-                aria-label="Page Header"
-            />
-
+            <PageHeader />
         )
     }
 }
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import {
     PageSidebar,
     Nav,
+    NavExpandable,
     NavItem,
     NavList,
     NavVariants,
 } from '@patternfly/react-core';
-
 
 // interface AllProps {
 //     isDropdownOpen: boolean;
@@ -14,39 +14,49 @@ import {
 //     activeItem: string;
 // }
 
-
 class Sidebar extends React.Component {
     onNavSelect = () => {
         // Change active state ***** working
     };
 
     render() {
-        let activeItem = 0; // Will be transferred to state
+        let activeItem = 'dashboard', activeGroup = 'grp-1'; // Will be transferred to state
 
         const PageNav = (
             <Nav onSelect={this.onNavSelect} aria-label="Nav">
                 <NavList variant={NavVariants.simple}>
-                    <NavItem to="library" itemId={0} isActive={activeItem === 0}>
-                        Library
-                </NavItem>
-                    <NavItem to="studies" itemId={1} isActive={activeItem === 1}>
-                        My Studies
-                </NavItem>
-                    <NavItem to="feeds" itemId={2} isActive={activeItem === 2}>
-                        Feeds
-                </NavItem>
-                    <NavItem to="pipelines" itemId={3} isActive={activeItem === 3}>
+                    <NavExpandable title="Library" groupId="library_grp" isActive={activeGroup === 'library_grp'} isExpanded>
+                        <NavItem to="libraryitem" itemId="library_item" isActive={activeItem === 'library_item'}>
+                                Library Item
+                        </NavItem>
+                    </NavExpandable>
+                    <NavExpandable title="Library" groupId="studies_grp" isActive={activeGroup === 'studies_grp'} isExpanded>
+                        <NavItem to="studies" itemId="my_studies" isActive={activeItem === 'my_studies'}>
+                            My Studies
+                        </NavItem>
+                    </NavExpandable>
+                    <NavExpandable title="Feeds" groupId="grp-1" isActive={activeGroup === 'grp-1'} isExpanded>
+                        <NavItem to="/" groupId="grp-1" itemId="dashboard" isActive={activeItem === 'dashboard'}>
+                            My Dashboard
+                        </NavItem>
+                        <NavItem to="/" groupId="grp-1" itemId="my_feeds" isActive={activeItem === 'my_feeds'}>
+                            My Feeds
+                        </NavItem>
+                        <NavItem to="/" groupId="grp-1" itemId="all_feeds" isActive={activeItem === 'all_feeds'}>
+                            All Feeds
+                        </NavItem>
+                    </NavExpandable>
+                    <NavItem to="pipelines" itemId="pipelines" isActive={activeItem === 'pipelines'}>
                         Pipelines
-                </NavItem>
-                    <NavItem to="plugins" itemId={4} isActive={activeItem === 4}>
+                    </NavItem>
+                    <NavItem to="plugins" itemId="plugins" isActive={activeItem === 'plugins'}>
                         Plugins
-                </NavItem>
+                    </NavItem>
                 </NavList>
             </Nav>
         );
         return (
             <PageSidebar nav={PageNav} />
-
         )
     }
 }
