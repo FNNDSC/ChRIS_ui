4c589d31e2d7bd778a5bcf3bd0fb0bb9a94127b5
Rebeca Kerr <rkerr@devshop.info>
2019-02-13 08:46:15 -0500

    completed sidebar routing, preventing transitions

--- src/containers/layout/Sidebar.tsx
+++ src/containers/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+
 import { ApplicationState } from "../../store/root/applicationState";
 import { IUiState } from "../../store/ui/types";
 import {
@@ -16,49 +18,49 @@ import {
 class Sidebar extends React.Component<IUiState> {
 
     render() {
-        const { isSidebarOpen, sidebarActiveItem, sidebarActiveGroup} = this.props;
-       
+        const { isSidebarOpen, sidebarActiveItem, sidebarActiveGroup } = this.props;
+
         const PageNav = (
-            <Nav  aria-label="ChRIS Demo site navigation">
+            <Nav aria-label="ChRIS Demo site navigation">
                 <NavList>
-                <NavGroup title='Main Navigation'>
-                    <NavExpandable title="Library" groupId="library_grp" isActive={sidebarActiveGroup === 'library_grp'}>
-                        <NavItem to="libraryitem" groupId="library_grp"  itemId="library_item" isActive={sidebarActiveItem === 'library_item'}>
-                                Library Item
+                    <NavGroup title='Main Navigation'>
+                        <NavExpandable title="Library" groupId="library_grp" isActive={sidebarActiveGroup === 'library_grp'}>
+                            <NavItem groupId="library_grp" itemId="library_item" isActive={sidebarActiveItem === 'library_item'}>
+                                <Link to={`/libraryitem`}>Library Item</Link>
+                            </NavItem>
+                        </NavExpandable>
+                        <NavExpandable title="My Studies" groupId="studies_grp" isActive={sidebarActiveGroup === 'studies_grp'}>
+                            <NavItem groupId="studies_grp" itemId="my_studies" isActive={sidebarActiveItem === 'my_studies'}>
+                                <Link to={`/studies`}> My Studies</Link>
+                            </NavItem>
+                        </NavExpandable>
+                        <NavExpandable title="My Feeds" groupId="feeds_grp" isActive={sidebarActiveGroup === 'feeds_grp'} isExpanded >
+                            <NavItem groupId="feeds_grp" itemId="dashboard" isActive={sidebarActiveItem === 'dashboard'}>
+                                <Link to={`/`}>Dashboard</Link>
+                            </NavItem>
+                            <NavItem groupId="feeds_grp" itemId="my_feeds" isActive={sidebarActiveItem === 'my_feeds'}>
+                                <Link to="/feeds/myfeedID"> My Feeds</Link>
+                            </NavItem>
+                            <NavItem groupId="feeds_grp" itemId="all_feeds" isActive={sidebarActiveItem === 'all_feeds'}>
+                                <Link to="/feeds">All Feeds</Link>
+                            </NavItem>
+                        </NavExpandable>
+                        <NavItem to="pipelines" itemId="pipelines" isActive={sidebarActiveItem === 'pipelines'}>
+                            <Link to="/pipelines">Pipelines</Link>
                         </NavItem>
-                    </NavExpandable>
-                    <NavExpandable title="My Studies" groupId="studies_grp" isActive={sidebarActiveGroup === 'studies_grp'}>
-                        <NavItem to="studies" groupId="studies_grp" itemId="my_studies" isActive={sidebarActiveItem === 'my_studies'}>
-                            My Studies
+                        <NavItem itemId="plugins" isActive={sidebarActiveItem === 'plugins'}>
+                            <Link to="/plugins">Plugins</Link>
                         </NavItem>
-                    </NavExpandable>
-                    <NavExpandable title="My Feeds" groupId="feeds_grp" isActive={sidebarActiveGroup === 'feeds_grp'} isExpanded >
-                         <NavItem to="/" groupId="feeds_grp" itemId="dashboard" isActive={sidebarActiveItem === 'dashboard'}>
-                            My Dashboard
-                        </NavItem>
-                        <NavItem to="/feeds/myfeedID" groupId="feeds_grp" itemId="my_feeds" isActive={sidebarActiveItem === 'my_feeds'}>
-                            My Feeds
-                        </NavItem>
-                        <NavItem to="/feeds" groupId="feeds_grp" itemId="all_feeds" isActive={sidebarActiveItem === 'all_feeds'}>
-                            All Feeds
-                        </NavItem> 
-                    </NavExpandable>
-                    <NavItem to="pipelines" itemId="pipelines" isActive={sidebarActiveItem === 'pipelines'}>
-                        Pipelines
-                    </NavItem>
-                    <NavItem to="plugins" itemId="plugins" isActive={sidebarActiveItem === 'plugins'}>
-                        Plugins
-                    </NavItem>
                     </NavGroup>
                     <NavGroup title='Working Pages'>
-                        <NavItem to="/login" itemId="logIn">
-                            Log in
+                        <NavItem itemId="logIn">
+                            <Link to="/login">Log in</Link>
                         </NavItem>
                         <NavItem to="/not-found" itemId="notfound">
-                           Not Found
+                            <Link to="/not-found">Not Found</Link>
                         </NavItem>
                     </NavGroup>
-                 
+
                 </NavList>
             </Nav>
         );
