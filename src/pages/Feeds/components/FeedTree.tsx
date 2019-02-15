121797d0130cb356369120ad344b3480cef9fbf2
Rebeca Kerr <rkerr@devshop.info>
2019-02-15 15:53:49 -0500

    began tree component

--- src/pages/Feeds/components/FeedTree.tsx
+++ src/pages/Feeds/components/FeedTree.tsx
@@ -1,31 +1,47 @@
 import * as React from "react";
 import { Item } from '../../../store/feed/types';
 
-
-
 interface PropsFromDispatch {
 
 }
 
 interface ITreeProps {
-    items: Item[];
+    items: Array<any>;
     // Add more props here if needed
 };
 
 type AllProps = ITreeProps & PropsFromDispatch;
 
 class FeedTree extends React.Component<AllProps> {
-    
+
     public render() {
         const { items } = this.props;
-       
+        console.log(items);
         return (
-            (items.length >0) ?
-                <div>TREE COMPONENT</div> :
+            (items.length > 0) ?
+                <Tree items={items} /> :
                     <div>Empty tree</div>
         )
     }
-
 }
+
+
+class Tree extends React.Component<ITreeProps> {
+    public render() {
+        const { items } = this.props;
+        return (
+            <div>
+                Build Tree in this component
+               {
+                    items.map((item, i) => (
+                        <div key={i}>
+                        id: {item.id}, feed_id: {item.feed_id}, plugin_id: {item.plugin_id}, previous_id: {item.previous_id}</div>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
 
 export default FeedTree;
