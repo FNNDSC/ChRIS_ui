5ad7efbd60bbfff1c145cf71e0fc6fc905f1d562
Rebeca Kerr <rkerr@devshop.info>
2019-02-15 10:26:27 -0500

    Feed page top section styling and markup layout

--- src/pages/Feeds/components/FeedView.tsx
+++ src/pages/Feeds/components/FeedView.tsx
@@ -14,8 +14,9 @@ import {
   DataListToggle,
   DataListContent,
 } from "@patternfly/react-core";
-
 import { pf4UtilityStyles } from '../../../lib/pf4-styleguides';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import imgPlaceholder from '../../../assets/images/feed_ph_70x70.png';
 import './feed.scss';
 interface PropsFromDispatch {
   setSidebarActive: typeof setSidebarActive;
@@ -42,10 +43,33 @@ class FeedView extends React.Component<AllProps> {
 
     return (
       <React.Fragment>
+        {/* Top section with Feed information */}
         <PageSection variant={PageSectionVariants.darker}>
-          <h1>Hippocampal Volume</h1>
+          <div className="feed-info-block pf-l-grid">
+            <div className="pf-l-grid__item pf-m-1-col">
+              <img src={imgPlaceholder} alt="placeholder for feed" />
+            </div>
+            <div className="pf-l-grid__item pf-m-11-col">
+              <h1>Hippocampal Volume - [Feed Title]</h1>
+              <ul className="pf-c-list pf-m-inline">
+                <li>
+                  <small>Creator</small>
+                  <p><FontAwesomeIcon icon={['far', 'user']} /> [user name]</p>
+                </li>
+                <li>
+                  <small>Created</small>
+                  <p><FontAwesomeIcon icon={['far', 'calendar-alt']} /> 2 Jan 2018 @ 14:23</p>
+                </li>
+                <li>
+                  <small>Total Runtime</small>
+                  <p><FontAwesomeIcon icon={['far', 'clock']} /> 3h, 8min</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </PageSection>
-        {/* {`${pf4UtilityStyles.accessibleStyles.hiddenOnLg} ${pf4UtilityStyles.spacingStyles.mr_0}`} */}
+        {/* END Top section with Feed information */}
+        {/* Mid section with Feed and node actions */}
         <PageSection className={pf4UtilityStyles.spacingStyles.p_0} variant={PageSectionVariants.light}>
           <Grid className="feed-view">
             <GridItem className="feed-block pf-u-p-md" sm={12} md={6}  >
@@ -56,6 +80,7 @@ class FeedView extends React.Component<AllProps> {
             </GridItem>
           </Grid>
         </PageSection>
+        {/* END Mid section with Feed and node actions */}
         {/* Bottom section with information */}
         <PageSection>
           <div className="plugin-info pf-u-py-md" >
@@ -65,13 +90,12 @@ class FeedView extends React.Component<AllProps> {
                 <DataList aria-label="Expandable data list example">
                   <DataListItem aria-labelledby="ex-item1" isExpanded={isExpanded}>
                     [Plugin Name]
-              <DataListToggle
+                    <DataListToggle
                       onClick={() => toggle('ex-toggle1')}
                       isExpanded={isExpanded}
                       id="ex-toggle1"
                       aria-labelledby="ex-toggle1 ex-item1"
-                      aria-label="Toggle details for"
-                    />
+                      aria-label="Toggle details for" />
                     <DataListContent aria-label="Primary Content Details" isHidden={!isExpanded}>
                       test
                   </DataListContent>
@@ -100,12 +124,12 @@ class FeedView extends React.Component<AllProps> {
                   <DataListItem aria-labelledby="ex-item1" isExpanded={isExpanded}>
                     [Plugin Name]
                     <DataListToggle
-                            onClick={() => toggle('ex-toggle1')}
-                            isExpanded={isExpanded}
-                            id="ex-toggle1"
-                            aria-labelledby="ex-toggle1 ex-item1"
-                            aria-label="Toggle details for"
-                          />
+                      onClick={() => toggle('ex-toggle1')}
+                      isExpanded={isExpanded}
+                      id="ex-toggle1"
+                      aria-labelledby="ex-toggle1 ex-item1"
+                      aria-label="Toggle details for"
+                    />
                     <DataListContent aria-label="Primary Content Details" isHidden={!isExpanded}>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                         dolore magna aliqua.</p>
