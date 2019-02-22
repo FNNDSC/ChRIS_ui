9d5761a56f92fc690773f6748005b4b559e99764
Rebeca Kerr <rkerr@devshop.info>
2019-02-22 12:05:16 -0500

    update to latest react-core and misc fixes to feed model

--- src/pages/LogIn/components/LoginForm.tsx
+++ src/pages/LogIn/components/LoginForm.tsx
@@ -6,88 +6,92 @@ import { IUserState } from '../../../store/user/types';
 import { withRouter } from 'react-router-dom';
 import { RouteComponentProps } from 'react-router';
 import { LoginForm } from '@patternfly/react-core';
-
-
 interface IPropsFromDispatch {
-    getAuthToken: typeof getAuthToken;
+  getAuthToken: typeof getAuthToken;
 }
 
 interface IState {
-    usernameValue: string;
-    passwordValue: string;
-    isRememberMeChecked: boolean;
+  usernameValue: string;
+  passwordValue: string;
+  isRememberMeChecked: boolean;
+  showHelperText: boolean;
+  isValidUsername: boolean;
+  isValidPassword: boolean;
 }
 
 type AllProps = IPropsFromDispatch & RouteComponentProps;
 
 class LoginFormComponent extends React.Component<AllProps, IState> {
-    constructor(props: AllProps) {
-        super(props);
-        this.state = {
-            usernameValue: 'chris',
-            passwordValue: 'chris1234',
-            isRememberMeChecked: true,
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props: AllProps) {
+    super(props);
+    this.state = {
+      usernameValue: 'chris',
+      passwordValue: 'chris1234',
+      isRememberMeChecked: true,
+      showHelperText: false,
+      isValidUsername: true,
+      isValidPassword: true
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    // Description: Create a fake user to work with API, redux store
-    handleSubmit(event: Event) {
-        const { getAuthToken } = this.props;
-        const authObj = {
-            password: this.state.passwordValue,
-            username: this.state.usernameValue,
-            isRememberMe: this.state.isRememberMeChecked
-        };
-        getAuthToken(authObj);
-        event.preventDefault();
-    }
+  // Description: Create a fake user to work with API, redux store
+  handleSubmit(event: Event) {
+    const { getAuthToken } = this.props;
+    const authObj = {
+      password: this.state.passwordValue,
+      username: this.state.usernameValue,
+      isRememberMe: this.state.isRememberMeChecked
+    };
+    getAuthToken(authObj);
+    event.preventDefault();
+  }
 
-    handleUsernameChange = (value: string) => {
-        this.setState({ usernameValue: value });
-    }
-    handlePasswordChange = (passwordValue: string) => {
-        this.setState({ passwordValue });
-    }
+  handleUsernameChange = (value: string) => {
+    this.setState({ usernameValue: value });
+  }
+  handlePasswordChange = (passwordValue: string) => {
+    this.setState({ passwordValue });
+  }
 
-    onRememberMeClick = () => {
-        this.setState({ isRememberMeChecked: !this.state.isRememberMeChecked });
-    }
+  onRememberMeClick = () => {
+    this.setState({ isRememberMeChecked: !this.state.isRememberMeChecked });
+  }
 
-    render() {
-        return (
-            <LoginForm
-                className="login-form"
-                usernameLabel="Username"
-                usernameValue={this.state.usernameValue}
-                onChangeUsername={this.handleUsernameChange}
-                usernameHelperTextInvalid="Unknown Username"
-                isValidUsername
-                passwordLabel="Password"
-                passwordValue={this.state.passwordValue}
-                onChangePassword={this.handlePasswordChange}
-                passwordHelperTextInvalid="Password Invalid"
-                isValidPassword
-                rememberMeLabel="Keep me logged in for 30 days."
-                isRememberMeChecked={this.state.isRememberMeChecked}
-                onChangeRememberMe={this.onRememberMeClick}
-                rememberMeAriaLabel="Remember me Checkbox"
-                onLoginButtonClick={this.handleSubmit}
-            />
-        );
-    }
+  render() {
+    return (
+      <LoginForm
+        showHelperText={this.state.showHelperText}
+        helperText= "Invalid login credentials."
+        usernameLabel="Username"
+        usernameValue={this.state.usernameValue}
+        onChangeUsername={this.handleUsernameChange}
+        isValidUsername={this.state.isValidUsername}
+        passwordLabel="Password"
+        passwordValue={this.state.passwordValue}
+        onChangePassword={this.handlePasswordChange}
+        isValidPassword={this.state.isValidPassword}
+        rememberMeLabel="Keep me logged in for 30 days."
+        isRememberMeChecked={this.state.isRememberMeChecked}
+        onChangeRememberMe={this.onRememberMeClick}
+        rememberMeAriaLabel="Remember me Checkbox"
+        onLoginButtonClick={this.handleSubmit}
+      />
+    );
+  }
 }
 
 // export default withRouter(LoginFormComponent);
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    getAuthToken: (user: IUserState) => dispatch(getAuthToken(user)),
+  getAuthToken: (user: IUserState) => dispatch(getAuthToken(user))
 });
 
 // const mapStateToProps = ({ ui }: ApplicationState) => ({
 // });
 
-
-export default withRouter(connect(
+export default withRouter(
+  connect(
     null,
-    mapDispatchToProps,
-)(LoginFormComponent));
+    mapDispatchToProps
+  )(LoginFormComponent)
+);
