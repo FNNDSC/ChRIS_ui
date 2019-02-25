93d7aa9b35128cafb7e3e11dcc5ebf3f12c2ebff
Rebeca Kerr <rkerr@devshop.info>
2019-02-25 17:42:36 -0500

    plugin info panel

--- src/pages/Feeds/components/NodeDetails.tsx
+++ src/pages/Feeds/components/NodeDetails.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { IPluginItem } from "../../../store/feed/types";
-
+import { Button, Grid, GridItem } from "@patternfly/react-core";
+import { ShareAltIcon, InfrastructureIcon } from "@patternfly/react-icons";
 interface INodeProps {
-    selected: IPluginItem;
+  selected: IPluginItem;
 }
 
 class NodeDetails extends React.Component<INodeProps> {
   render() {
     const { selected } = this.props;
     return (
-        <div>Selected Node:  {selected.plugin_name}</div>
+      <React.Fragment>
+        <div>
+          <label>Selected Node:</label> {selected.plugin_name}
+        </div>
+        <Grid>
+          <GridItem className="pf-u-p-sm" sm={12} md={6}>
+            Pipeline chart
+          </GridItem>
+          <GridItem className="pf-u-p-sm" sm={12} md={6}>
+
+              <label>From this node:</label>
+              <div className="btn-div">
+                <Button variant="tertiary" isBlock>
+                  <InfrastructureIcon /> Add new node(s)...
+                </Button>
+                <Button variant="tertiary" isBlock>
+                  <ShareAltIcon /> Share this pipeline...
+                </Button>
+              </div>
+
+          </GridItem>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
