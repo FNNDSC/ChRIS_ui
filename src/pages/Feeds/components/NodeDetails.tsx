43f1fc14f5e6994c5e079a962ba19daae11b2067
Rebeca Kerr <rkerr@devshop.info>
2019-02-26 09:56:18 -0500

    added onclick stub handlers for plugin actions

--- src/pages/Feeds/components/NodeDetails.tsx
+++ src/pages/Feeds/components/NodeDetails.tsx
@@ -7,6 +7,16 @@ interface INodeProps {
 }
 
 class NodeDetails extends React.Component<INodeProps> {
+  // Description: Share pipeline with others ***** Working
+  handleSharePipeline() {
+    // Stub - To be done
+  }
+
+  // Description: Add new node to the feed ***** Working
+  handleAddNewNode() {
+    // Stub - To be done
+  }
+
   render() {
     const { selected } = this.props;
     return (
@@ -19,13 +29,12 @@ class NodeDetails extends React.Component<INodeProps> {
             Pipeline chart
           </GridItem>
           <GridItem className="pf-u-p-sm" sm={12} md={6}>
-
               <label>From this node:</label>
               <div className="btn-div">
-                <Button variant="tertiary" isBlock>
+                <Button variant="tertiary" isBlock  onClick={this.handleAddNewNode}>
                   <InfrastructureIcon /> Add new node(s)...
                 </Button>
-                <Button variant="tertiary" isBlock>
+                <Button variant="tertiary" isBlock onClick={this.handleSharePipeline}>
                   <ShareAltIcon /> Share this pipeline...
                 </Button>
               </div>
