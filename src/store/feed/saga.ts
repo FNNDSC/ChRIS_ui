276553fac244278439251e2d5ed85dfe40be4a44
Rebeca Kerr <rkerr@devshop.info>
2019-03-04 17:01:56 -0500

    separated feed and plugin stores

--- src/store/feed/saga.ts
+++ src/store/feed/saga.ts
@@ -1,5 +1,5 @@
 import { all, call, fork, put, takeEvery } from "redux-saga/effects";
-import { IFeedState, FeedActionTypes } from "./types";
+import { FeedActionTypes } from "./types";
 import FeedModel from "../../api/models/feed.model";
 import {
     getFeedDetailsSuccess,
@@ -63,39 +63,12 @@ function* handleGetPluginInstances(action: any) {
 function* watchGetPluginInstances() {
     yield takeEvery(FeedActionTypes.GET_PLUGIN_INSTANCES, handleGetPluginInstances);
 }
-// ------------------------------------------------------------------------
-// Description: Get Plugin Descendants
-// ------------------------------------------------------------------------
-function* handleGetPluginDescendants(action: any) {
-    try {
-        const res = yield call(FeedModel.getPluginRequest, action.payload);
-        if (res.error) {
-            // yield put(handleUIMessage({ message: res.error, type: UIMessageType.error, displayType: MessageHandlerType.toastr }));
-            console.error(res.error); // working user messaging
-        } else {
-            yield put(getPluginDescendantsSuccess(res));
-            // yield put(managerOnCompleteRequest()); // nO need for messaging just loading false
-        }
-    } catch (error) {
-        console.error(error); // working user messaging
-        // yield put(handleUIMessage({ message: (err instanceof Error ? (err.stack!) :
-        //   managerDefaults.defaultMessage.Error), type: UIMessageType.error, displayType: MessageHandlerType.toastr }));
-    }
-}
-
-// This is our watcher function. We use `take*()` functions to watch Redux for a specific action
-// type, and run our saga, for example the `handleFetch()` saga above.
-function* watchGetPluginDescendants() {
-    yield takeEvery(FeedActionTypes.GET_PLUGIN_DESCENDANTS, handleGetPluginDescendants);
-}
-
 
 // ------------------------------------------------------------------------
 // We can also use `fork()` here to split our saga into multiple watchers.
 export function* feedSaga() {
     yield all([
       fork(watchGetFeedRequest),
-      fork(watchGetPluginInstances),
-      fork(watchGetPluginDescendants)
+      fork(watchGetPluginInstances)
     ]);
   }
