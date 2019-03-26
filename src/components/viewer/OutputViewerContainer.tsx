afd4b6ab4c0d50ce0a42da8d1453aaea4d3faf27
Rebeca Kerr <rkerr@devshop.info>
2019-03-26 12:47:38 -0400

    Dynamic tabs on viewer modal

--- src/components/viewer/OutputViewerContainer.tsx
+++ src/components/viewer/OutputViewerContainer.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { FormEvent } from "react";
 import { connect } from "react-redux";
-import { Tabs, Tab } from "@patternfly/react-core";
+import { Tabs, Tab, Alert } from "@patternfly/react-core";
 import { ApplicationState } from "../../store/root/applicationState";
 import { IFeedFile } from "../../api/models/feed-file.model";
 import { IUITreeNode } from "../../api/models/file-explorer";
+import { IPluginItem } from "../../api/models/pluginInstance.model";
 import DicomViewer from "./dicomViewer";
 import DataTableViewer from "./dataTableViewer";
 import FileBrowserViewer from "./fileBrowserViewer";
@@ -15,54 +16,99 @@ import "./viewer.scss";
 type AllProps = {
   files?: IFeedFile[];
   explorer?: IUITreeNode;
+  selected?: IPluginItem;
 };
 
-class OutputViewerContainer extends React.Component<AllProps, { activeTabKey: number }> {
+class OutputViewerContainer extends React.Component<
+  AllProps,
+  { activeTabKey: number }
+> {
   constructor(props: AllProps) {
     super(props);
     this.handleTabClick = this.handleTabClick.bind(this);
   }
+
   state = {
-    activeTabKey: 0 // Temp - set to 0
+    activeTabKey: 0
   };
 
-  // Toggle currently active tab
+  render() {
+    const { files, explorer } = this.props;
+    const tabs = this.buildTabArray();
+    return (
+      <div className="output-viewer">
+        {!!tabs && tabs.length ? (
+          <Tabs
+            activeKey={this.state.activeTabKey}
+            onSelect={this.handleTabClick}
+            children={tabs}
+          />
+        ) : (
+          <Alert variant="info" title="Empty Result Set" />
+        )}
+      </div>
+    );
+  }
+
+  // Description: Toggle currently active tab
   handleTabClick = (event: FormEvent<HTMLInputElement>, tabIndex: number) => {
     this.setState({
       activeTabKey: tabIndex
     });
   }
-  render() {
-    const { files, explorer } = this.props;
-    return (
-      (!!files && files.length && !!explorer ) && <div className="output-viewer">
-        <Tabs
-          activeKey={this.state.activeTabKey}
-          onSelect={this.handleTabClick} >
-          <Tab eventKey={0} title="Viewer">
-            <DicomViewer files={files} />
-          </Tab>
-          <Tab eventKey={1} title="Data Table">
-            <DataTableViewer files={files} />
-          </Tab>
-          <Tab eventKey={2} title="File Browser">
-            <FileBrowserViewer files={files} explorer={explorer} />
-          </Tab>
-          <Tab eventKey={3} title="Volume">
-            <VolumeGrowth />
-          </Tab>
-          <Tab eventKey={4} title="Segment">
-            <SegmentAnalysis />
-          </Tab>
-        </Tabs>
-      </div>
-    );
+  // Description: Build Tabs from data
+  buildTabArray = () => {
+    const { files, explorer, selected } = this.props;
+    const tabs: any[] = [];
+    if (!!selected) {
+      const tabArr = tempMapping[selected.plugin_name] || tempMapping.default;
+      tabArr.forEach((key: string, i: number) => {
+        console.log("key", key, i);
+        let tabContent;
+        let label = "tab";
+        switch (key) {
+          case "DicomViewer":
+            label = "Viewer";
+            tabContent = !!files && <DicomViewer files={files} />;
+            break;
+          case "FileBrowserViewer":
+            label = "File Browser";
+            tabContent = !!files && !!explorer && <FileBrowserViewer files={files} explorer={explorer} />;
+            break;
+          case "DataTableViewer":
+            label = "Data Table";
+            tabContent = !!files && <DataTableViewer files={files} />;
+            break;
+          case "VolumeGrowth":
+            label = "Volume";
+            tabContent = <VolumeGrowth />;
+            break;
+          case "SegmentAnalysis":
+            label = "Segment";
+            tabContent = <SegmentAnalysis />;
+            break;
+        }
+        tabs.push(<Tab eventKey={i} title={label}>
+          {tabContent}
+        </Tab>);
+      });
+    }
+    return tabs;
   }
 }
+// Description: Temporary mapping for plugin tabs
+const tempMapping: any = {
+  default: ["FileBrowserViewer"],
+  dircopy: ["DicomViewer", "FileBrowserViewer"],
+  freesurfer_pp: ["DataTableViewer", "FileBrowserViewer"], // Nice to have viewer 3D Map image
+  simpledsapp: ["DataTableViewer", "VolumeGrowth", "SegmentAnalysis"],
+  z2labelmap: ["DicomViewer", "FileBrowserViewer"]
+};
 
 const mapStateToProps = ({ plugin }: ApplicationState) => ({
   files: plugin.files,
-  explorer: plugin.explorer
+  explorer: plugin.explorer,
+  selected: plugin.selected
 });
 
 export default connect(
