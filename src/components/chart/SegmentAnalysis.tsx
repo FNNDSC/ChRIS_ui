9c3fc3a063d7f7422f9f715fd6673de823107dfa
Gagan Kumar <gaganskumar3@gmail.com>
2019-03-28 18:09:54 -0400

    CSV read

--- src/components/chart/SegmentAnalysis.tsx
+++ src/components/chart/SegmentAnalysis.tsx
@@ -9,27 +9,92 @@ interface ComponentProps {
 
 interface ComponentState {
     pushedSegments: string[];
-    render: boolean;
 }
 
 // csvData format [[segmentName1, LH1, RH1],[segmentName2, LH2, RH2]]
 const csvData = [
-  ["GSFrontToMargin", 3.12, 3.4],
-  ["GOrbital", -2.0, -1.6],
-  ["GTemporalMiddle", 1.0, 1.4],
-  ["SCentral", 4.0, 4.4],
-  ["SFrontSup", -1.5, -1.1],
-  ["STemporalInf", 2.5, 2.0]
+  ["G_and_S_frontomargin", 1.1234, 1.1234],
+  ["G_and_S_occipital_inf", 1.2345, 1.2345],
+  ["G_and_S_paracentral", 1.3456, 1.3456],
+  ["G_and_S_subcentral", 1.4567, 1.4567],
+  ["G_and_S_transv_frontopol", 1.5678, 1.5678],
+  ["G_and_S_cingul-Ant", 1.6789, 1.6789],
+  ["G_and_S_cingul-Mid-Ant", 1.6789, 1.6789],
+  ["G_and_S_cingul-Mid-Post", 1.6789, 1.6789],
+  ["G_cingul-Post-dorsal", 1.6789, 1.6789],
+  ["G_cingul-Post-ventral", 1.6789, 1.6789],
+  ["G_cuneus,2230,1543,3356", 1.6789, 1.6789],
+  ["G_front_inf-Opercular", 1.6789, 1.6789],
+  ["G_front_inf-Orbital", 1.6789, 1.6789],
+  ["G_front_inf-Triangul", 1.6789, 1.6789],
+  ["G_front_middle", 1.6789, 1.6789],
+  ["G_front_sup", 1.6789, 1.6789],
+  ["G_Ins_lg_and_S_cent_ins", 1.6789, 1.6789],
+  ["G_insular_short", 1.6789, 1.6789],
+  ["G_occipital_middle", 1.6789, 1.6789],
+  ["G_occipital_sup", 1.6789, 1.6789],
+  ["G_oc-temp_lat-fusifor", 1.6789, 1.6789],
+  ["G_oc-temp_med-Lingual", 1.6789, 1.6789],
+  ["G_oc-temp_med-Parahip", 1.6789, 1.6789],
+  ["G_orbital", 1.6789, 1.6789],
+  ["G_pariet_inf-Angular", 1.6789, 1.6789],
+  ["G_pariet_inf-Supramar", 1.6789, 1.6789],
+  ["G_parietal_sup", 1.6789, 1.6789],
+  ["G_postcentral", 3.7890, 3.8900],
+  ["G_precentral", 3.1111, 3.2122],
+  ["G_precuneus", -3.1111, -3.2122],
+  ["G_rectus", -2.6789, -2.5890],
+  ["G_subcallosal", 1.6789, 1.6789],
+  ["G_temp_sup-G_T_transv", 1.6789, 1.6789],
+  ["G_temp_sup-Lateral", 1.6789, 1.6789],
+  ["G_temp_sup-Plan_polar", 1.6789, 1.6789],
+  ["G_temp_sup-Plan_tempo", 1.6789, 1.6789],
+  ["G_temporal_inf", 1.6789, 1.6789],
+  ["G_temporal_middle", 1.6789, 1.6789],
+  ["Lat_Fis-ant-Horizont", 1.6789, 1.6789],
+  ["Lat_Fis-ant-Vertical", 1.6789, 1.6789],
+  ["Lat_Fis-post", 1.6789, 1.6789],
+  ["Pole_occipital", 1.6789, 1.6789],
+  ["Pole_temporal", 1.6789, 1.6789],
+  ["S_calcarine", 1.6789, 1.6789],
+  ["S_central", 1.6789, 1.6789],
+  ["S_cingul-Marginalis", 1.6789, 1.6789],
+  ["S_circular_insula_ant", 1.6789, 1.6789],
+  ["S_circular_insula_inf", 1.6789, 1.6789],
+  ["S_circular_insula_sup", 1.6789, 1.6789],
+  ["S_collat_transv_ant", 1.6789, 1.6789],
+  ["S_collat_transv_post", 1.6789, 1.6789],
+  ["S_front_inf", 1.6789, 1.6789],
+  ["S_front_middle", 1.6789, 1.6789],
+  ["S_front_sup", 1.6789, 1.6789],
+  ["S_interm_prim-Jensen", 1.6789, 1.6789],
+  ["S_intrapariet_and_P_trans", 1.6789, 1.6789],
+  ["S_oc_middle_and_Lunatus", 1.6789, 1.6789],
+  ["S_oc_sup_and_transversal", 1.6789, 1.6789],
+  ["S_occipital_ant", 1.3456, 1.3456],
+  ["S_oc-temp_lat", 1.3456, 1.3456],
+  ["S_oc-temp_med_and_Lingual", 1.3456, 1.3456],
+  ["S_orbital_lateral", 1.3456, 1.3456],
+  ["S_orbital_med-olfact", 1.3456, 1.3456],
+  ["S_orbital-H_Shaped", 1.3456, 1.3456],
+  ["S_parieto_occipital", 1.1234, 1.1234],
+  ["S_pericallosal", 1.1234, 1.1234],
+  ["S_postcentral", 1.1234, 1.1234],
+  ["S_precentral-inf-part", 1.1234, 1.1234],
+  ["S_precentral-sup-part", 1.1234, 1.1234],
+  ["S_suborbital", 1.1234, 1.1234],
+  ["S_subparietal", 1.1234, 1.1234],
+  ["S_temporal_inf", 1.1234, 1.1234],
+  ["S_temporal_sup", 1.1234, 1.1234],
+  ["S_temporal_transverse", 1.1234, 1.1234]
 ];
 
 const defaultSegments: any[] = [];
 const defaultLeft : any[] = [];
 const defaultRight : any[] = [];
 
-const segments = ["GSFrontToMargin", "GOrbital", "GTemporalMiddle",
-                    "SCentral", "SFrontSup", "STemporalInf"];
-const segmentValues = [[3.12, 3.4], [-2.0, -1.6], [1.0, 1.4],
-                          [4.0, 4.4], [-1.5, -1.1], [2.5, 2.0]];
+const segments: any[] = [];
+const segmentValues: any[] = [];
 
 class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
   constructor(props: ComponentProps) {
@@ -37,10 +102,16 @@ class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
 
     this.state = {
       pushedSegments: [],
-      render: false
     };
 
     this.changeData = this.changeData.bind(this);
+  }
+
+  readData() {
+    for(let i = 0; i < csvData.length; i++){
+      segments.push(csvData[i][0]);
+      segmentValues.push([csvData[i][1], csvData[i][2]]);
+    }
   }
 
   pickDefaultSegments(segmentOffSet: any) {
@@ -57,15 +128,6 @@ class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
       }
     }
 
-    //console.log("this.setState.pushedSegments", this.state.pushedSegments);
-    //console.log("defaultSegments", defaultSegments)
-    /*this.setState({
-      pushedSegments : defaultSegments
-    }, () => {
-
-      this.callChart([defaultLeft, defaultRight], this.state.pushedSegments);
-    });*/
-    this.setState({ render : true});
     this.setState({ pushedSegments: defaultSegments})
   }
 
@@ -90,15 +152,16 @@ class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
 
   componentWillMount() {
     //Calculate Offset
+    this.readData();
     this.calculateOffset();
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
-    this.callChart([defaultLeft, defaultRight], defaultSegments);
+    //console.log('componentDidMount');
+    this.callChart([defaultLeft, defaultRight]);
   }
 
-  callChart(inputChart: any, segments: any) {
+  callChart(inputChart: any) {
     c3.generate({
       bindto: "#SegmentAnalysis",
       data: {
@@ -112,7 +175,7 @@ class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
       axis: {
         x: {
             type: "category",
-            categories: segments,
+            categories: this.state.pushedSegments,
         },
         y: {
             label: {
@@ -177,7 +240,7 @@ class SegmentAnalysis extends React.Component<ComponentProps, ComponentState> {
     }, () => {
       // Input processing
       processedData = this.setFilter();
-      this.callChart(processedData, this.state.pushedSegments);
+      this.callChart(processedData);
     });
   }
 
