c44bbc17c26f38f3364f742b69dba7be3171c804
Rebeca Kerr <rkerr@devshop.info>
2019-04-15 15:39:59 -0400

    file explorer work

--- src/api/models/file-viewer.ts
+++ src/api/models/file-viewer.ts
@@ -5,3 +5,15 @@ export interface IFileState {
     blobText: any;
     fileType: string;
   }
+
+
+// Description: get file type by file extention
+export function downloadFile(Fileblob: any, fileName: string) {
+  const url = window.URL.createObjectURL(new Blob([Fileblob]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", fileName);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
--- src/components/viewer/displays/FileBrowserViewer.tsx
+++ src/components/viewer/displays/FileBrowserViewer.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { Grid, GridItem } from "@patternfly/react-core";
+import { Grid, GridItem, Alert } from "@patternfly/react-core";
 import { ApplicationState } from "../../../store/root/applicationState";
-import { setExplorerRequest, setSelectedNode } from "../../../store/explorer/actions";
+import { setExplorerRequest, setSelectedFile, setSelectedFolder } from "../../../store/explorer/actions";
 import { IExplorerState } from "../../../store/explorer/types";
 import { IFeedFile } from "../../../api/models/feed-file.model";
 import { IPluginItem } from "../../../api/models/pluginInstance.model";
 import { IUITreeNode } from "../../../api/models/file-explorer";
+import { downloadFile } from "../../../api/models/file-viewer";
 import FileExplorer from "../../explorer/FileExplorer";
 import FileTableView from "../../explorer/FileTableView";
-
+import FileDetailView from "../../explorer/FileDetailView";
+import FeedFileModel from "../../../api/models/feed-file.model";
 
 interface IPropsFromDispatch {
   setExplorerRequest: typeof setExplorerRequest;
-  setSelectedNode: typeof setSelectedNode;
+  setSelectedFile: typeof setSelectedFile;
+  setSelectedFolder: typeof setSelectedFolder;
 }
 
 type AllProps = {
@@ -32,27 +35,27 @@ class FileBrowserViewer extends React.Component<AllProps> {
 
   // Description: handle active node and render FileDetailView
   setActiveNode = (node: IUITreeNode) => {
-    const { selectedNode, setSelectedNode } = this.props;
-    (node !== selectedNode) && setSelectedNode(node);
+    const { setSelectedFile, setSelectedFolder } = this.props;
+    (!!node.leaf && node.leaf) ? setSelectedFile(node) : setSelectedFolder(node);
   };
 
   render() {
-    const { explorer, selectedNode } = this.props;
+    const { explorer, selectedFile, selectedFolder } = this.props;
     return (
-      !!explorer &&
+      (!!explorer) &&
       <div className="pf-u-px-lg">
         <Grid>
           <GridItem className="pf-u-p-sm" sm={12} md={3}>
             {<FileExplorer
               explorer={explorer}
-              selectedNode={selectedNode}
+              selectedNode={selectedFile || selectedFolder}
               onClickNode={this.setActiveNode} />}
           </GridItem>
           <GridItem className="pf-u-py-sm pf-u-px-xl" sm={12} md={9}>
             {
-              !!selectedNode ?
-                <FileTableView selectedNode={selectedNode} onClickNode={this.setActiveNode} /> :
-                <div>Empty Message</div>
+              !!selectedFolder ? <FileTableView selectedFolder={selectedFolder} onClickNode={this.setActiveNode} downloadFileNode={this.handleFileDownload} /> :
+                !!selectedFile ? <FileDetailView active={selectedFile} downloadFileNode={this.handleFileDownload} /> :
+                <Alert variant="info" title="Please select a file or folder from the file explorer" className="empty" />
             }
           </GridItem>
         </Grid>
@@ -60,16 +63,33 @@ class FileBrowserViewer extends React.Component<AllProps> {
     );
   }
 
+  // Description: handle file download first get file blob
+  handleFileDownload(node: IUITreeNode) {
+    const downloadUrl = node.file.file_resource;
+    const _self = this;
+    if (!!node.file) {
+      FeedFileModel.getFileBlob(downloadUrl)
+        .then((result: any) => {
+          downloadFile(result.data, node.module);
+        })
+        .catch((error: any) => console.error("(1) Inside error:", error));
+    } else {
+      console.error("ERROR DOWNLOADING: download url is not defined");
+    }
+  }
 };
+
 
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   setExplorerRequest: (files: IFeedFile[], selected: IPluginItem) => dispatch(setExplorerRequest(files, selected)),
-  setSelectedNode: (node: IUITreeNode) => dispatch(setSelectedNode(node))
+  setSelectedFile: (node: IUITreeNode) => dispatch(setSelectedFile(node)),
+  setSelectedFolder: (node: IUITreeNode) => dispatch(setSelectedFolder(node))
 });
 
 const mapStateToProps = ({ explorer }: ApplicationState) => ({
-  selectedNode: explorer.selectedNode,
+  selectedFile: explorer.selectedFile,
+  selectedFolder: explorer.selectedFolder,
   explorer: explorer.explorer
 });
 
