026ea4ed98284919725dd872ed90cfacf7f1c9fd
Rebeca Kerr <rkerr@devshop.info>
2019-04-16 16:20:39 -0400

    Exit full screen and state support

--- src/components/gallery/GalleryWrapper.tsx
+++ src/components/gallery/GalleryWrapper.tsx
@@ -11,9 +11,17 @@ interface IOtherProps {
 interface IPropsFromDispatch {
     // onSidebarToggle: typeof onSidebarToggle;
 }
+interface IState {
+    isFullscreen: boolean;
+}
 type AllProps = IExplorerState & IOtherProps & IPropsFromDispatch;
 
-class GalleryWrapper extends React.Component<AllProps> {
+class GalleryWrapper extends React.Component<AllProps, IState> {
+
+    state = {
+        isFullscreen: false
+    }
+
     render() {
         const { children } = this.props;
 
@@ -22,26 +30,48 @@ class GalleryWrapper extends React.Component<AllProps> {
                 {children}
                 <GalleryArrows param={"tbd"} />
                 <GalleryToolbar isPlaying={false} />
-                <GalleryFullScreen onFullScreenGallery={this.handlefullscreen} isFullscreen={true} />
+                <GalleryFullScreen onFullScreenGallery={this.handlefullscreen} isFullscreen={this.state.isFullscreen} />
             </div>
         );
     }
-     // Description: will make the view full screen
-     handlefullscreen = () => {
-        const elem = document.getElementById("gallery") as any;
-        if (!!elem) {
-            if (elem.requestFullscreen) {
-                elem.requestFullscreen();
-            } else if (elem.mozRequestFullScreen) { /* Firefox */
-                elem.mozRequestFullScreen();
-            } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-                elem.webkitRequestFullscreen();
-            } else if (elem.msRequestFullscreen) { /* IE/Edge */
-                elem.msRequestFullscreen();
-            }
-        }
+    // Description: will make the view full screen ***** WORKING *****
+    handlefullscreen = () => {
+        const elem = document.getElementById("gallery");
+        const isOpened = this.state.isFullscreen; // Get from state ***** TBD
+        (!!elem) && (isOpened ? closeFullScreen() : openFullScreen(elem));
+        this.setState({
+            isFullscreen: !isOpened
+        });
     };
 
+}
+
+// --------------------------------------------------------
+// Description: handle full screen open and close
+const openFullScreen = (elem: any) => {
+    console.log("openFullScreen", elem);
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+const closeFullScreen = () => {
+    const elem = document as any;
+    if (elem.exitFullscreen) {
+        elem.exitFullscreen();
+    } else if (elem.mozCancelFullScreen) { /* Firefox */
+        elem.mozCancelFullScreen();
+    } else if (elem.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitExitFullscreen();
+    } else if (elem.msExitFullscreen) { /* IE/Edge */
+        elem.msExitFullscreen();
+    }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
