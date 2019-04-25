9b5ad3ed4df0355e40f418f69214add86b344eec
Rebeca Kerr <rkerr@devshop.info>
2019-04-25 15:46:53 -0400

    toggle file information visibility

--- src/components/gallery/GalleryInfoPanel/GalleryInfoPanel.tsx
+++ src/components/gallery/GalleryInfoPanel/GalleryInfoPanel.tsx
@@ -1,27 +1,19 @@
 import * as React from "react";
-import { CloseIcon } from "@patternfly/react-icons";
-import { Button } from "@patternfly/react-core";
 import { IGalleryItem } from "../../../api/models/gallery.model";
 import "./GalleryInfoPanel.scss";
 type AllProps = {
     galleryItem?: IGalleryItem;
-    toggleViewerMode: () => void;
 };
 
 const GalleryInfoPanel: React.FunctionComponent<AllProps> = (props: AllProps) => {
-    const {galleryItem} = props;
+    const { galleryItem } = props;
     return (
         <div className="gallery-info">
-            <Button
-                className="close-btn"
-                variant="link"
-                onClick={props.toggleViewerMode} ><CloseIcon size="md" />
-            </Button>
-           {
-               !!galleryItem && <div>
-                <p>File name: {galleryItem.fileName}</p>
-            </div>
-        }
+            {!!galleryItem &&
+                <React.Fragment>
+                    <p>File name: {galleryItem.fileName}</p>
+                    {/* Add more metadata here */}
+                </React.Fragment>}
         </div>
     )
 }
