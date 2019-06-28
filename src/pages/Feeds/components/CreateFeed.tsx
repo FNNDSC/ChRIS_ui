8d4ec92fe5a3d240a595d925ed241269b723177c
Matan Kotler-Berkowitz <205matan@gmail.com>
2019-06-28 15:50:40 -0400

    Refactor Review and Local File Upload pages of Create Feed Wizard

--- src/pages/Feeds/components/CreateFeed.tsx
+++ src/pages/Feeds/components/CreateFeed.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 
-import { Typeahead } from "react-bootstrap-typeahead";
 import { 
   Button, Wizard, Form, FormGroup, Checkbox,
-  TextInput, TextArea, Split, SplitItem
+  TextInput, TextArea, Split, SplitItem, WizardStepFunctionType
 } from "@patternfly/react-core";
-import { FileIcon, CloseIcon } from "@patternfly/react-icons";
+import { FileIcon, CloseIcon, FolderOpenIcon, FolderCloseIcon } from "@patternfly/react-icons";
+
+import { Typeahead } from "react-bootstrap-typeahead";
 import Tree from "react-ui-tree";
 
-import FileExplorer from "../../../components/explorer/FileExplorer";
 import { IUITreeNode } from "../../../api/models/file-explorer.model";
 
 interface LocalFile {
@@ -47,7 +47,7 @@ const BasicInformation:React.FunctionComponent<BasicInformationProps> = (
 ) => {
     
   return (
-    <Form className="pf-u-w-75">
+    <Form className="pf-u-w-75 basic-information">
       <h1 className="pf-c-title pf-m-2xl">Basic Information</h1>
       <FormGroup
         label="Feed Name"
@@ -270,12 +270,12 @@ class ChrisFileSelect extends React.Component<{}, ChrisFileSelectState> {
               onChange={this.handleFilterChange}
             />
             <Tree
-              tree={ explorer }
+              tree={this.state.visibleTree}
               renderNode={ this.renderTreeNode }
               paddingLeft={ 20 }
             />
           </SplitItem>
-          <SplitItem isMain>
+          <SplitItem isMain className="file-list">
             <p className="section-header">Files to add to new feed:</p>
             { fileList }
           </SplitItem>
@@ -285,139 +285,149 @@ class ChrisFileSelect extends React.Component<{}, ChrisFileSelectState> {
   }
 }
 
-function readFileFromInput(file: File): Promise<string> {
-  return new Promise(res => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (typeof reader.result === 'string') {
-        const data = reader.result || '';
-        res(data);
-      }
-    }
-    reader.readAsText(file);
-  })
-}  
-
-function openLocalFilePicker(): Promise<Array<LocalFile>> {
-  const input = document.createElement('input');
-  input.type = 'file';
-  input.multiple = true;
-  input.click();
-  return new Promise((res) => {
-    input.onchange = async () => {
-      if (input.files) {
-        const files = await Promise.all(Array.from(input.files).map(async file => {
-          return {
-            name: file.name,
-            data: await readFileFromInput(file),
-          }
-        }))
-        res(files);
-      }
-    }
-  })
-}
-
 interface LocalFileUploadProps {
   files: CreateFeedData['localFiles'],
   handleFilesAdd: (files: Array<LocalFile>) => void,
   handleFileRemove: (name: string) => void,
 }
 
-const LocalFileUpload:React.FunctionComponent<LocalFileUploadProps> = ({
-  files,
-  handleFilesAdd,
-  handleFileRemove,
-}) => {
+class LocalFileUpload extends React.Component<LocalFileUploadProps> {
+  
+  readFileFromInput = (file: File):Promise<string> => {
+    return new Promise(res => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          const data = reader.result || '';
+          res(data);
+        }
+      }
+      reader.readAsText(file);
+    })
+  }  
+  
+  openLocalFilePicker(): Promise<Array<LocalFile>> {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.multiple = true;
+    input.click();
+    return new Promise((res) => {
+      input.onchange = async () => {
+        if (input.files) {
+          const files = await Promise.all(Array.from(input.files).map(async file => {
+            return {
+              name: file.name,
+              data: await this.readFileFromInput(file),
+            }
+          }))
+          res(files);
+        }
+      }
+    })
+  }
 
-  const fileList = files.map(file => (
-    <div className="file-preview" key={ file.name }>
-      <FileIcon />
-      <span className="file-name">{ file.name }</span>
-      <CloseIcon className="file-remove" onClick={ () => handleFileRemove(file.name) }/>
-    </div>
-  ))
+  handleChoseFilesClick = () => {
+    this.openLocalFilePicker().then(this.props.handleFilesAdd);
+  }
 
-  const chooseFilesButton = <Button 
-    onClick={ () => openLocalFilePicker().then(handleFilesAdd)}
-  >
-    Choose Files...
-  </Button>
+  render() {
+    const {
+      files,
+      handleFileRemove,
+    } = this.props;
 
-  return (
-    <div className="local-file-upload">
-      <h1 className="pf-c-title pf-m-2xl">Data Configuration: Local File Upload</h1>
-      <p>Please choose the data files you'd like to add to your feed.</p>
-      <br />
-      <Split gutter="lg">
-        <SplitItem isMain>
-          <p className="section-header">File Upload</p>
-          { chooseFilesButton }
-        </SplitItem>
-        <SplitItem isMain>
-          <p className="section-header">Local files to add to new feed:</p>
-          { fileList }
-        </SplitItem>
-      </Split>
-    </div>
-  )
+    const fileList = files.map(file => (
+      <div className="file-preview" key={ file.name }>
+        <FileIcon />
+        <span className="file-name">{ file.name }</span>
+        <CloseIcon className="file-remove" onClick={ () => handleFileRemove(file.name) }/>
+      </div>
+    ))
+  
+    return (
+      <div className="local-file-upload">
+        <h1 className="pf-c-title pf-m-2xl">Data Configuration: Local File Upload</h1>
+        <p>Please choose the data files you'd like to add to your feed.</p>
+        <br />
+        <Split gutter="lg">
+          <SplitItem isMain>
+            <p className="section-header">File Upload</p>
+            <Button onClick={this.handleChoseFilesClick}>
+              Choose Files...
+            </Button>
+          </SplitItem>
+          <SplitItem isMain className="file-list">
+            <p className="section-header">Local files to add to new feed:</p>
+            { fileList }
+          </SplitItem>
+        </Split>
+      </div>
+    )
+  }
+
 }
 
 interface ReviewProps {
   data: CreateFeedData,
 }
 
-const Review:React.FunctionComponent<ReviewProps> = (props: ReviewProps) => {
-  const { data } = props;
+class Review extends React.Component<ReviewProps> {
+
+  generateFileList(files: Array<LocalFile>) {
+    return files.map(file => (
+      <div className="file-preview" key={ file.name }>
+        <FileIcon />
+        <span className="file-name">{ file.name }</span>
+      </div>
+    ))
+  }
   
-  // the installed version of @patternfly/react-core doesn't support read-only chips
-  const tags = data.tags.map(tag => (
-    <div className="pf-c-chip pf-m-read-only review-tag">
-      <span className="pf-c-chip__text">
-          { tag }
-      </span>
-    </div> 
-  ))
+  render() {
+    const { data } = this.props;
+  
+    // the installed version of @patternfly/react-core doesn't support read-only chips
+    const tags = data.tags.map(tag => (
+      <div className="pf-c-chip pf-m-read-only tag">
+        <span className="pf-c-chip__text">
+            { tag }
+        </span>
+      </div> 
+    ))
 
-  const files = data.localFiles.map(file => (
-    <div className="file-preview" key={ file.name }>
-      <FileIcon />
-      <span className="file-name">{ file.name }</span>
-    </div>
-  ))
+    return (
+      <div className="review">
+        <h1 className="pf-c-title pf-m-2xl">Review</h1>
+        <p>Review the information below and click 'Finish' to create your new feed.</p>
+        <p>Use the 'Back' button to make changes.</p>
+        <br /><br />
 
-  return (
-    <div className="review">
-      <h1 className="pf-c-title pf-m-2xl">Review</h1>
-      <p>Review the information below and click 'Finish' to create your new feed.</p>
-      <p>Use the 'Back' button to make changes.</p>
-      <br /><br />
+        <Split gutter="lg">
+          <SplitItem isMain>
+            <div>Feed Name</div>
+            <div>Feed Description</div>
+            <div>Tags</div>
+          </SplitItem>
+          <SplitItem isMain>
+            <div>{ data.feedName }</div>
+            <div>{ data.feedDescription || <span>&nbsp;</span> }</div>
+            <div>{ tags}</div>
+          </SplitItem>
+        </Split>
 
-      <Split gutter="lg">
-        <SplitItem isMain={ false }>
-          <div>Feed Name</div>
-          <div>Feed Description</div>
-          <div>Tags</div>
-        </SplitItem>
-        <SplitItem isMain>
-          <div>{ data.feedName }</div>
-          <div>{ data.feedDescription || '&nbsp;' }</div>
-          <div>{ tags}</div>
-        </SplitItem>
-      </Split>
+        <br />
+        <Split>
+          <SplitItem isMain className="file-list">
+            <p>ChRIS files to add to new feed:</p>
+          </SplitItem>
+          <SplitItem isMain className="file-list">
+            <p>Local files to add to new feed:</p>
+            { this.generateFileList(data.localFiles) }
+          </SplitItem>
+        </Split>
+      </div>
+    )
 
-      <br />
-      <Split>
-        <SplitItem isMain>
-          <p>ChRIS files to add to new feed:</p>
-        </SplitItem>
-        <SplitItem isMain>
-          <p>Local files to add to new feed:</p>
-          { files }
-        </SplitItem>
-      </Split>
-    </div>
-  )
+  }
 }
 
 interface CreateFeedState {
