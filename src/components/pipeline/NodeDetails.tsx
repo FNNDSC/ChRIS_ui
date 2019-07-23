2984bd3436e131c883fbd032855e391d50febc9f
matankb <205matan@gmail.com>
2019-07-23 15:55:31 -0400

    Redesign NodeDetails

--- src/components/pipeline/NodeDetails.tsx
+++ src/components/pipeline/NodeDetails.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { IPluginItem } from "../../api/models/pluginInstance.model";
-import { Button, Grid, GridItem } from "@patternfly/react-core";
-import { ShareAltIcon, InfrastructureIcon } from "@patternfly/react-icons";
-import TreeNodeModel, { INode } from "../../api/models/tree-node.model";
+import Moment from "react-moment";
+
+import { Button, Grid, GridItem, Title } from "@patternfly/react-core";
+import { ShareAltIcon, InfrastructureIcon, TerminalIcon, CaretDownIcon, CheckIcon, CalendarDayIcon } from "@patternfly/react-icons";
+import { PopoverPosition } from "@patternfly/react-core-updated";
+
+import { IPluginItem, statusLabels } from "../../api/models/pluginInstance.model";
+import TreeNodeModel from "../../api/models/tree-node.model";
 import { getPluginInstanceTitle } from "../../api/models/pluginInstance.model";
-import PipelineTree from "./PipelineTree";
+
+import TextCopyPopover from "../common/textcopypopover/TextCopyPopover";
+
 interface INodeProps {
   selected: IPluginItem;
   descendants: IPluginItem[];
@@ -29,35 +35,84 @@ class NodeDetails extends React.Component<INodeProps> {
   }
 
   render() {
-    const { selected, descendants } = this.props;
+    const { selected } = this.props;
+    
+    const pluginTitle = `${getPluginInstanceTitle(selected)} v. ${selected.plugin_version}`;
+    const command = `
+      docker run -v $(pwd)/in:/incoming -v
+      $(pwd)/out:outgoing
+      fnndsc/pl-antsreg antsreg.py
+      /incoming /outgoing
+    `;
+
     return (
       <React.Fragment>
-        <div className="capitalize">
-          <label>Selected Node:</label> {getPluginInstanceTitle(selected)}
-        </div>
-        <Grid>
-          <GridItem className="pf-u-p-sm" sm={12} md={6}>
-            <PipelineTree items={descendants} selected={selected} />
-          </GridItem>
-          <GridItem className="pf-u-p-sm" sm={12} md={6}>
-            <label>From this node:</label>
-            <div className="btn-div">
-              <Button
-                variant="tertiary"
-                isBlock
-                onClick={this.handleAddNewNode} >
-                <InfrastructureIcon /> Add new node(s)...
+
+        <div className="header-wrap">
+          <div>
+            Selected Node:
+            <Title headingLevel="h2" size="xl">
+              { pluginTitle }
+            </Title>
+          </div>
+          <div>
+            <TextCopyPopover
+              text={command.trim()}
+              headerContent={`Command for ${pluginTitle}`}
+              subheaderContent="This plugin was run via the following command:"
+              position={ PopoverPosition.bottom }
+              className="view-command-wrap"
+              maxWidth="27rem"
+            >
+              <Button>
+                <TerminalIcon />
+                View Command
+                <CaretDownIcon />
               </Button>
-              {
-                this.isNodePipelineRoot(selected) && (
-                  <Button variant="tertiary" isBlock onClick={this.handleSharePipeline}>
-                    <ShareAltIcon /> Share this pipeline...
-                  </Button>
-                )
-              }
-            </div>
+            </TextCopyPopover>
+          </div>
+        </div>
+
+        <Grid gutter="sm" className="node-details-grid">
+          <GridItem span={2} className="title">Status</GridItem>
+          <GridItem span={10} className="value">
+            <CheckIcon />
+            { statusLabels[selected.status] || selected.status }
           </GridItem>
+          <GridItem span={2} className="title">Created</GridItem>
+          <GridItem span={10} className="value">
+            <CalendarDayIcon />
+            <Moment format="DD MMM YYYY @ HH:mm">
+              {selected.start_date}
+            </Moment>
+          </GridItem>
+          <GridItem span={2} className="title">Node ID</GridItem>
+          <GridItem span={10} className="value">{ selected.id }</GridItem>
         </Grid>
+
+        <br /><br />
+        <label>Actions:</label>
+        <div className="btn-div">
+          <Button
+            variant="tertiary"
+            isBlock
+            onClick={this.handleAddNewNode}
+          >
+            <InfrastructureIcon />
+            Add new node(s)...
+          </Button>
+          {
+            this.isNodePipelineRoot(selected) && (
+              <Button variant="tertiary" isBlock onClick={this.handleSharePipeline}>
+                <ShareAltIcon /> Share this pipeline...
+              </Button>
+            )
+          }
+        </div>
+
+        <br /><br />
+        <label>Plugin output may be viewed below.</label>
+
       </React.Fragment>
     );
   }
