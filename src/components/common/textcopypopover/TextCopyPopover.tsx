6fb6316015248027514af471c987b8e0d425d10a
matankb <205matan@gmail.com>
2019-07-26 14:21:49 -0400

    Use patternfly component in TextCopyPopover

--- src/components/common/textcopypopover/TextCopyPopover.tsx
+++ src/components/common/textcopypopover/TextCopyPopover.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Popover, TextArea } from '@patternfly/react-core-updated';
+import { Popover, TextArea, ClipboardCopy } from '@patternfly/react-core-updated';
 import { CopyIcon } from '@patternfly/react-icons';
 import { Instance, BasicPlacement } from 'tippy.js';
 
@@ -15,53 +15,19 @@ interface TextCopyPopoverProps {
   rows?: number,
 
   tabIndex?: number,
-  className?: string
+  className?: string,
   shouldClose?: (instance: Instance) => void,
   onMouseDown?: (event: React.MouseEvent) => void,
   onBlur?: (event: React.FocusEvent) => void,
   maxWidth?: string,
 }
 
-interface TextCopyPopoverState {
-  copied: boolean
-}
-
-class TextCopyPopover extends React.Component<TextCopyPopoverProps, TextCopyPopoverState> {
-
-  copiedTimerId?: number;
+class TextCopyPopover extends React.Component<TextCopyPopoverProps> {
 
   constructor(props: TextCopyPopoverProps) {
     super(props);
-    this.state = { 
-      copied: false,
-    };
-
-    this.handleCopyClick = this.handleCopyClick.bind(this);
   }
 
-  componentWillUnmount() {
-    if (this.copiedTimerId) {
-      window.clearTimeout(this.copiedTimerId);
-    }
-  }
-
-  handleCopyClick(e: React.MouseEvent) {
-    e.preventDefault();
-    const button = e.target as HTMLElement;
-    const input = document.createElement('input');
-    input.value = this.props.text;
-    button.appendChild(input);
-    input.focus();
-    input.select();
-    document.execCommand('copy');
-    button.focus();
-    button.removeChild(input);
-    this.setState({ copied: true });
-    this.copiedTimerId = window.setTimeout(() => {
-      this.setState({ copied: false });
-    }, 5000);
-  }
-  
   mergeClassnames(...names: (string | undefined)[]) {
     return names.join(' ');
   }
@@ -79,14 +45,7 @@ class TextCopyPopover extends React.Component<TextCopyPopoverProps, TextCopyPopo
           spellCheck={false}
           aria-label="full-path"
         />
-        <a href="#" tabIndex={0} onClick={this.handleCopyClick}>
-          <CopyIcon />
-          {
-            this.state.copied ?
-              'Text copied' :
-              'Copy text'
-          }
-        </a>
+        <ClipboardCopy>{ text }</ClipboardCopy>
       </React.Fragment>
     )
 
