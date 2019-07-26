f22b21339ffec1d881001356c1e79c7b3d2287f4
matankb <205matan@gmail.com>
2019-07-26 10:30:34 -0400

    Correctly display full command in NodeDetails

--- src/components/feed/NodeDetails.tsx
+++ src/components/feed/NodeDetails.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Moment from "react-moment";
 
 import { Button, Grid, GridItem, Title } from "@patternfly/react-core";
+import { Plugin, PluginParameter } from "@fnndsc/chrisapi";
 import { ShareAltIcon, InfrastructureIcon, TerminalIcon, CaretDownIcon, CheckIcon, CalendarDayIcon } from "@patternfly/react-icons";
 import { PopoverPosition } from "@patternfly/react-core-updated";
 
 import { IPluginItem, statusLabels } from "../../api/models/pluginInstance.model";
+import ChrisAPIClient from "../../api/chrisapiclient";
 import TreeNodeModel from "../../api/models/tree-node.model";
 import { getPluginInstanceTitle } from "../../api/models/pluginInstance.model";
 
@@ -16,11 +18,46 @@ interface INodeProps {
   descendants: IPluginItem[];
 }
 
-class NodeDetails extends React.Component<INodeProps> {
+interface INodeState {
+  plugin?: Plugin // the plugin which the currently selected instance is an instance of
+  params?: PluginParameter[]
+}
+
+class NodeDetails extends React.Component<INodeProps, INodeState> {
+
+  constructor(props: INodeProps) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    this.fetchPluginData(this.props.selected);
+  }
+
+  async componentDidUpdate(prevProps: INodeProps) {
+    const { selected: prevSelected } = prevProps;
+    const { selected } = this.props;
+    if (prevSelected.id !== selected.id) {
+      this.fetchPluginData(selected);
+    }
+  }
+
+  async fetchPluginData(selected: IPluginItem) {
+    const { id, plugin_id } = this.props.selected;
+    const client = ChrisAPIClient.getClient();
+    const plugin = await client.getPlugin(plugin_id);
+    const params = await (await client.getPluginInstance(id as number)).getParameters();
+    this.setState({
+      plugin,
+      params: params.getItems()
+    });
+  }
+
   // Description: Share pipeline with others ***** Working
   handleSharePipeline() {
     // Stub - To be done 
   }
+
 
   // Description: Add new node to the feed ***** Working
   handleAddNewNode() {
@@ -34,16 +71,39 @@ class NodeDetails extends React.Component<INodeProps> {
     return (!TreeNodeModel.isRootNode(item) && !TreeNodeModel.isLeafNode(item, descendants));
   }
 
+  getCommand(plugin: Plugin, params: PluginParameter[]) {
+    const { dock_image, selfexec } = plugin.data;
+    let command = `
+    docker run -v $(pwd)/in:/incoming -v 
+    $(pwd)/out:outgoing
+    ${dock_image} ${selfexec}`
+
+    if (params.length) {
+      command += '\n' + params.map(param => `    --${param.data.param_name} ${param.data.value}`).join('\n');
+    }
+
+    command = `${command}\n    /incoming /outgoing`.trim();
+
+    // append backslashes
+    const lines = command.split('\n');
+    const longest = lines.reduce((a, b) => a.length > b.length ? a : b).length;
+    return lines
+      .map(line => `${line.padEnd(longest)}  \\`)
+      .join('\n')
+      .slice(0, -1); // remove final backslash
+
+  }
+
   render() {
     const { selected } = this.props;
+    const { plugin, params } = this.state;
     
     const pluginTitle = `${getPluginInstanceTitle(selected)} v. ${selected.plugin_version}`;
-    const command = `
-      docker run -v $(pwd)/in:/incoming -v
-      $(pwd)/out:outgoing
-      fnndsc/pl-antsreg antsreg.py
-      /incoming /outgoing
-    `;
+    const command = plugin && params ?
+      this.getCommand(plugin, params) :
+      'Loading command...';
+
+    const commandRows = command.split('\n').length;
 
     return (
       <React.Fragment>
@@ -57,10 +117,11 @@ class NodeDetails extends React.Component<INodeProps> {
           </div>
           <div>
             <TextCopyPopover
-              text={command.trim()}
+              text={command}
               headerContent={`Command for ${pluginTitle}`}
               subheaderContent="This plugin was run via the following command:"
               position={ PopoverPosition.bottom }
+              rows={commandRows}
               className="view-command-wrap"
               maxWidth="27rem"
             >
