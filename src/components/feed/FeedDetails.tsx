a9788869729c6a6b1e4d0a9e028e810b8c5f1d1a
PintoGideon <gideonpinto123@gmail.com>
2019-08-08 13:54:59 -0400

    feed-description

--- src/components/feed/FeedDetails.tsx
+++ src/components/feed/FeedDetails.tsx
@@ -1,157 +1,157 @@
-import React from 'react';
-import Moment from 'react-moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import imgPlaceholder from '../../assets/images/feed_ph_70x70.png';
-import { IFeedState } from '../../store/feed/types';
-import { Title } from '@patternfly/react-core';
-import ChrisAPIClient from '../../api/chrisapiclient';
+import React from "react";
+import Moment from "react-moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import imgPlaceholder from "../../assets/images/feed_ph_70x70.png";
+import { IFeedState } from "../../store/feed/types";
+import { Title } from "@patternfly/react-core";
+import ChrisAPIClient from "../../api/chrisapiclient";
 
 /* Import Note for the Feed Description */
-import { Note } from '@fnndsc/chrisapi';
+import { Note } from "@fnndsc/chrisapi";
 /*****       *****/
 
 interface INoteState {
-	feedDescription?: Note;
+  feedDescription?: Note;
 }
 type AllProps = IFeedState;
 
 class FeedDetails extends React.Component<AllProps, INoteState> {
-	constructor(props: AllProps) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props: AllProps) {
+    super(props);
+    this.state = {};
+  }
 
-	/* Code to display Feed Description */
+  /* Code to display Feed Description */
 
-	async componentDidMount() {
-		const { details } = this.props;
-		if (details) {
-			// tslint:disable-next-line: indent
-			const id = details.id as number;
-			console.log('Component did mount', id);
-			this.fetchNote(id);
-		}
-	}
+  async componentDidMount() {
+    const { details } = this.props;
+    if (details) {
+      // tslint:disable-next-line: indent
+      const id = details.id as number;
+      console.log("Component did mount", id);
+      this.fetchNote(id);
+    }
+  }
 
-	componentDidUpdate(prevProps: AllProps) {
-		const { details } = this.props;
-		if (!details) {
-			return;
-		}
-		const id = details.id as number;
-		console.log('Component did mount', id);
+  componentDidUpdate(prevProps: AllProps) {
+    const { details } = this.props;
+    if (!details) {
+      return;
+    }
+    const id = details.id as number;
+    console.log("Component did mount", id);
 
-		if (!prevProps.details || prevProps.details.id !== details.id) {
-			this.fetchNote(id);
-		}
-	}
+    if (!prevProps.details || prevProps.details.id !== details.id) {
+      this.fetchNote(id);
+    }
+  }
 
-	async fetchNote(feed_id: number) {
-		const client = ChrisAPIClient.getClient();
-		const feed = await client.getFeed(feed_id);
-		const note = await feed.getNote();
-		const { data } = note;
-		console.log('Fetch Note', note.data);
+  async fetchNote(feed_id: number) {
+    const client = ChrisAPIClient.getClient();
+    const feed = await client.getFeed(feed_id);
+    const note = await feed.getNote();
+    const { data } = note;
+    console.log("Fetch Note", note.data);
 
-		this.setState({
-			feedDescription: data.content
-		});
-	}
+    this.setState({
+      feedDescription: data.content
+    });
+  }
 
-	/*******        ****** */
+  /*******        ****** */
 
-	calculateTotalRuntime() {
-		const plugins = this.props.items;
-		if (!plugins) {
-			return 0;
-		}
-		let runtime = 0;
-		for (const plugin of plugins) {
-			const start = new Date(plugin.start_date);
-			const end = new Date(plugin.end_date);
-			const elapsed = end.getTime() - start.getTime(); // milliseconds between start and end
-			runtime += elapsed;
-		}
-		// format millisecond amount into human-readable string
-		let runtimeStrings = [];
-		const timeParts = [
-			['day', Math.floor(runtime / (1000 * 60 * 60 * 24))],
-			['hr', Math.floor((runtime / (1000 * 60 * 60)) % 24)],
-			['min', Math.floor((runtime / 1000 / 60) % 60)],
-			['sec', Math.floor((runtime / 1000) % 60)]
-		];
-		for (const part of timeParts) {
-			const [name, value] = part;
-			if (value > 0) {
-				runtimeStrings.push(`${value} ${name}`);
-			}
-		}
-		return runtimeStrings.join(', ');
-	}
+  calculateTotalRuntime() {
+    const plugins = this.props.items;
+    if (!plugins) {
+      return 0;
+    }
+    let runtime = 0;
+    for (const plugin of plugins) {
+      const start = new Date(plugin.start_date);
+      const end = new Date(plugin.end_date);
+      const elapsed = end.getTime() - start.getTime(); // milliseconds between start and end
+      runtime += elapsed;
+    }
+    // format millisecond amount into human-readable string
+    let runtimeStrings = [];
+    const timeParts = [
+      ["day", Math.floor(runtime / (1000 * 60 * 60 * 24))],
+      ["hr", Math.floor((runtime / (1000 * 60 * 60)) % 24)],
+      ["min", Math.floor((runtime / 1000 / 60) % 60)],
+      ["sec", Math.floor((runtime / 1000) % 60)]
+    ];
+    for (const part of timeParts) {
+      const [name, value] = part;
+      if (value > 0) {
+        runtimeStrings.push(`${value} ${name}`);
+      }
+    }
+    return runtimeStrings.join(", ");
+  }
 
-	render() {
-		const { details } = this.props;
-		const { feedDescription } = this.state;
-		console.log('Feed Description', feedDescription);
+  render() {
+    const { details } = this.props;
+    const { feedDescription } = this.state;
+    console.log("Feed Description", feedDescription);
 
-		const runtime = this.calculateTotalRuntime();
+    const runtime = this.calculateTotalRuntime();
 
-		return (
-			!!details && (
-				<div className="feed-details">
-					<ul className="feed-breadcrumbs">
-						<li>
-							<a href="/feeds">My feeds</a>
-						</li>
-						<li>{details.name}</li>
-					</ul>
-					<div className="feed-info-block pf-l-grid">
-						<div className="pf-l-grid__item pf-m-1-col">
-							<img src={imgPlaceholder} alt="placeholder for feed" />
-						</div>
-						<div className="pf-l-grid__item pf-m-11-col">
-							<Title headingLevel="h1" size="3xl" className="capitalize">
-								{details.name}
-							</Title>
-							<ul className="pf-c-list pf-m-inline">
-								<li>
-									<small>Creator</small>
-									<p>
-										<FontAwesomeIcon icon={['far', 'user']} />{' '}
-										{details.creator_username}
-									</p>
-								</li>
-								<li>
-									<small>Created</small>
-									<p>
-										<FontAwesomeIcon icon={['far', 'calendar-alt']} />
-										<Moment format="DD MMM YYYY @ HH:mm">
-											{details.creation_date}
-										</Moment>
-									</p>
-								</li>
-								<li>
-									<small>Total Runtime</small>
-									<p>
-										<FontAwesomeIcon icon={['far', 'calendar-alt']} />
-										{runtime}
-									</p>
-								</li>
+    return (
+      !!details && (
+        <div className="feed-details">
+          <ul className="feed-breadcrumbs">
+            <li>
+              <a href="/feeds">My feeds</a>
+            </li>
+            <li>{details.name}</li>
+          </ul>
+          <div className="feed-info-block pf-l-grid">
+            <div className="pf-l-grid__item pf-m-1-col">
+              <img src={imgPlaceholder} alt="placeholder for feed" />
+            </div>
+            <div className="pf-l-grid__item pf-m-11-col">
+              <Title headingLevel="h1" size="3xl" className="capitalize">
+                {details.name}
+              </Title>
+              <ul className="pf-c-list pf-m-inline">
+                <li>
+                  <small>Creator</small>
+                  <p>
+                    <FontAwesomeIcon icon={["far", "user"]} />{" "}
+                    {details.creator_username}
+                  </p>
+                </li>
+                <li>
+                  <small>Created</small>
+                  <p>
+                    <FontAwesomeIcon icon={["far", "calendar-alt"]} />
+                    <Moment format="DD MMM YYYY @ HH:mm">
+                      {details.creation_date}
+                    </Moment>
+                  </p>
+                </li>
+                <li>
+                  <small>Total Runtime</small>
+                  <p>
+                    <FontAwesomeIcon icon={["far", "calendar-alt"]} />
+                    {runtime}
+                  </p>
+                </li>
 
-								<li>
-									<small>Feed Description</small>
-									<p>
-										<FontAwesomeIcon icon={['far', 'calendar-alt']} />
-										{feedDescription}
-									</p>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			)
-		);
-	}
+                <li>
+                  <small>Feed Description</small>
+                  <p>
+                    <FontAwesomeIcon icon={["far", "calendar-alt"]} />
+                    {feedDescription}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )
+    );
+  }
 }
 
 export default FeedDetails;
