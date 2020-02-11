87b95ce356a690c3c575892ac20054f55a3e9a41
PintoGideon <gideonpinto123@gmail.com>
2020-02-11 17:10:26 -0500

    temporarily silenced patternfly components error for production build

--- src/components/common/textcopypopover/TextCopyPopover.tsx
+++ src/components/common/textcopypopover/TextCopyPopover.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Popover, TextArea, ClipboardCopy } from '@patternfly/react-core';
-import { Instance, BasicPlacement } from 'tippy.js';
+import { Popover, TextArea, ClipboardCopy } from "@patternfly/react-core";
+import { Instance, BasePlacement } from "tippy.js";
 
-import './textcopypopover.scss';
+import "./textcopypopover.scss";
 
 interface TextCopyPopoverProps {
-  text: string,
-  headerContent: JSX.Element | string,
-  isVisible?: boolean,
-  position?: BasicPlacement,
-  subheaderContent?: string,
-  rows?: number,
+  text: string;
+  headerContent: JSX.Element | string;
+  isVisible?: boolean;
+  position?: BasePlacement;
+  subheaderContent?: string;
+  rows?: number;
 
-  tabIndex?: number,
-  className?: string,
-  shouldClose?: (instance: Instance) => void,
-  onMouseDown?: (event: React.MouseEvent) => void,
-  onBlur?: (event: React.FocusEvent) => void,
-  maxWidth?: string,
+  tabIndex?: number;
+  className?: string;
+  shouldClose?: (instance: Instance) => void;
+  onMouseDown?: (event: React.MouseEvent) => void;
+  onBlur?: (event: React.FocusEvent) => void;
+  maxWidth?: string;
 }
 
 class TextCopyPopover extends React.Component<TextCopyPopoverProps> {
@@ -28,34 +28,40 @@ class TextCopyPopover extends React.Component<TextCopyPopoverProps> {
   }
 
   mergeClassnames(...names: (string | undefined)[]) {
-    return names.join(' ');
+    return names.join(" ");
   }
 
   render() {
-
-    const { text, children, subheaderContent, className, rows, ...props } = this.props;
+    const {
+      text,
+      children,
+      subheaderContent,
+      className,
+      rows,
+      ...props
+    } = this.props;
 
     const body = (
       <React.Fragment>
-        { subheaderContent }
+        {subheaderContent}
         <TextArea
           value={text}
           rows={rows}
           spellCheck={false}
           aria-label="full-path"
         />
-        <ClipboardCopy>{ text }</ClipboardCopy>
+        <ClipboardCopy>{text}</ClipboardCopy>
       </React.Fragment>
-    )
+    );
 
     return (
       <Popover
         bodyContent={body}
-        className={this.mergeClassnames('text-copy-popover', className)}
+        className={this.mergeClassnames("text-copy-popover", className)}
         {...props}
       >
-        { children as React.ReactElement<any> }
-      </Popover >
+        {children as React.ReactElement<any>}
+      </Popover>
     );
   }
 }
