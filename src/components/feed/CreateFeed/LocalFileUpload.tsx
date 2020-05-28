61945c4a687919d4465754d0636f7d9bcaadc137
PintoGideon <gideonpinto123@gmail.com>
2020-05-28 12:18:57 -0400

    A Rewrite of the Feature using Context API and useReducer

--- src/components/feed/CreateFeed/LocalFileUpload.tsx
+++ src/components/feed/CreateFeed/LocalFileUpload.tsx
@@ -1,77 +1,82 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { CreateFeedContext } from "./context";
 import { Split, SplitItem, Button } from "@patternfly/react-core";
 import { FileIcon, CloseIcon } from "@patternfly/react-icons";
+import { LocalFile, Types } from "./types";
 
-import { LocalFile } from "./CreateFeed";
-
-interface LocalFileUploadProps {
-  files: LocalFile[];
-  handleFilesAdd: (files: LocalFile[]) => void;
-  handleFileRemove: (name: string) => void;
-}
-
-class LocalFileUpload extends React.Component<LocalFileUploadProps> {
-  openLocalFilePicker(): Promise<LocalFile[]> {
+const LocalFileUpload: React.FC = () => {
+  const { state, dispatch } = useContext(CreateFeedContext);
+  const { localFiles } = state.data;
+  const openLocalFilePicker = (): Promise<LocalFile[]> => {
     const input = document.createElement("input");
     input.type = "file";
     input.multiple = true;
     input.click();
-    return new Promise(res => {
+    return new Promise((res) => {
       input.onchange = async () => {
         if (input.files) {
-          const files = Array.from(input.files).map(file => {
+          const files = Array.from(input.files).map((file) => {
             return {
               name: file.name,
-              blob: file
+              blob: file,
             };
           });
           res(files);
         }
       };
     });
-  }
+  };
 
-  handleChoseFilesClick() {
-    this.openLocalFilePicker().then(this.props.handleFilesAdd);
-  }
+  const handleChoseFilesClick = () => {
+    openLocalFilePicker().then((files: LocalFile[]) => {
+      dispatch({
+        type: Types.AddLocalFile,
+        payload: {
+          files,
+        },
+      });
+    });
+  };
 
-  render() {
-    const { files, handleFileRemove } = this.props;
+  const fileList = localFiles.map((file) => (
+    <div className="file-preview" key={file.name}>
+      <FileIcon />
+      <span className="file-name">{file.name}</span>
+      <CloseIcon
+        className="file-remove"
+        onClick={() => {
+          dispatch({
+            type: Types.RemoveLocalFile,
+            payload: {
+              file: file.name,
+            },
+          });
+        }}
+      />
+    </div>
+  ));
 
-    const fileList = files.map(file => (
-      <div className="file-preview" key={file.name}>
-        <FileIcon />
-        <span className="file-name">{file.name}</span>
-        <CloseIcon
-          className="file-remove"
-          onClick={() => handleFileRemove(file.name)}
-        />
-      </div>
-    ));
-
-    return (
-      <div className="local-file-upload">
-        <h1 className="pf-c-title pf-m-2xl">
-          Data Configuration: Local File Upload
-        </h1>
-        <p>Please choose the data files you'd like to add to your feed.</p>
-        <br />
-        <Split gutter="lg">
-          <SplitItem isFilled>
-            <p className="section-header">File Upload</p>
-            <Button onClick={this.handleChoseFilesClick.bind(this)}>
-              Choose Files...
-            </Button>
-          </SplitItem>
-          <SplitItem isFilled className="file-list-wrap">
-            <p className="section-header">Local files to add to new feed:</p>
-            <div className="file-list">{fileList}</div>
-          </SplitItem>
-        </Split>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="local-file-upload">
+      <h1 className="pf-c-title pf-m-2xl">
+        Data Configuration: Local File Upload
+      </h1>
+      <p>Please choose the data files you'd like to add to your feed.</p>
+      <br />
+      <Split gutter="lg">
+        <SplitItem isFilled>
+          <p className="section-header">File Upload</p>
+          <Button onClick={() => handleChoseFilesClick()}>
+            Choose Files...
+          </Button>
+        </SplitItem>
+        <SplitItem isFilled className="file-list-wrap">
+          <p className="section-header">Local files to add to new feed:</p>
+          <div className="file-list">{fileList}</div>
+        </SplitItem>
+      </Split>
+    </div>
+  );
+};
 
 export default LocalFileUpload;
