834adc2846ba207202abd59a434714ce5542192d
PintoGideon <gideonpinto123@gmail.com>
2020-09-03 02:39:06 -0400

    Added Cine Player with Cornerstone React Viewport

--- src/components/dicomViewer/DcmHeader/DcmHeader.tsx
+++ src/components/dicomViewer/DcmHeader/DcmHeader.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Tooltip,
-  TooltipPosition,
-  Button,
-  Grid,
-  GridItem,
-} from "@patternfly/react-core";
+import { Tooltip, TooltipPosition, Button } from "@patternfly/react-core";
 import {
   SearchPlusIcon,
   SearchIcon,
@@ -14,6 +8,7 @@ import {
   BurnIcon,
   UndoIcon,
   DownloadIcon,
+  BarsIcon,
 } from "@patternfly/react-icons";
 import "./DcmHeader.scss";
 
@@ -24,120 +19,114 @@ interface DicomHeaderProps {
 const DcmHeader: React.FC<DicomHeaderProps> = ({ handleToolbarAction }) => {
   return (
     <div className="dicom-header">
-      <Grid>
-        <GridItem span={4}>
-          <div className="dicom-logo">
-            <span className="dicom-logo__text">Image Viewer</span>
-            <span className="dicom-logo__subtext">
-              Powered by cornerstone.js
-            </span>
-          </div>
-        </GridItem>
+      <div className="dicom-logo">
+        <span className="dicom-logo__text">Image Viewer</span>
+        <span className="dicom-logo__subtext">Powered by cornerstone.js</span>
+      </div>
 
-        <GridItem span={4}>
-          <div className="dicom-header__body-tools">
-            <span
-              style={{
-                marginRight: "0.3rem",
-                color: "white",
-              }}
-            >
-              Tools
-            </span>
+      <div className="dicom-header__body-tools">
+        <span
+          style={{
+            marginRight: "0.3rem",
+            color: "white",
+          }}
+        >
+          Tools
+        </span>
 
-            <Tooltip
-              position={TooltipPosition.bottom}
-              isContentLeftAligned
-              content={<span>LMB + Drag</span>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("zoom");
-                }}
-              >
-                <SearchPlusIcon size="md" />
-              </Button>
-            </Tooltip>
-            <Tooltip
-              position={TooltipPosition.bottom}
-              isContentLeftAligned
-              content={<span>Magnify</span>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("magnify");
-                }}
-              >
-                <SearchIcon size="md" />
-              </Button>
-            </Tooltip>
-            <Tooltip
-              position={TooltipPosition.bottom}
-              isContentLeftAligned
-              content={<span>Scroll</span>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("pan");
-                }}
-              >
-                <HandPaperIcon size="md" />
-              </Button>
-            </Tooltip>
-            <Tooltip
-              position={TooltipPosition.bottom}
-              isContentLeftAligned
-              content={<span>Adjust Brightness</span>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("wwwc");
-                }}
-              >
-                <AdjustIcon size="md" />
-              </Button>
-            </Tooltip>
-            <Tooltip
-              position={TooltipPosition.bottom}
-              content={<div>Invert</div>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("invert");
-                }}
-              >
-                <BurnIcon size="md"></BurnIcon>
-              </Button>
-            </Tooltip>
-            <Tooltip
-              position={TooltipPosition.bottom}
-              content={<div>Invert</div>}
-            >
-              <Button
-                variant="link"
-                onClick={() => {
-                  handleToolbarAction("rotate");
-                }}
-              >
-                <UndoIcon size="md"></UndoIcon>
-              </Button>
-            </Tooltip>
-          </div>
-        </GridItem>
-
-        <GridItem span={4}>
-          <div className="dicom-header__body-functions">
-            <Button variant="link">
-              <DownloadIcon size="md" />
-            </Button>
-          </div>
-        </GridItem>
-      </Grid>
+        <Tooltip
+          position={TooltipPosition.bottom}
+          isContentLeftAligned
+          content={<span>LMB + Drag</span>}
+        >
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("zoom");
+            }}
+          >
+            <SearchPlusIcon size="md" />
+          </Button>
+        </Tooltip>
+        <Tooltip
+          position={TooltipPosition.bottom}
+          isContentLeftAligned
+          content={<span>Magnify</span>}
+        >
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("magnify");
+            }}
+          >
+            <SearchIcon size="md" />
+          </Button>
+        </Tooltip>
+        <Tooltip
+          position={TooltipPosition.bottom}
+          isContentLeftAligned
+          content={<span>Scroll</span>}
+        >
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("pan");
+            }}
+          >
+            <HandPaperIcon size="md" />
+          </Button>
+        </Tooltip>
+        <Tooltip
+          position={TooltipPosition.bottom}
+          isContentLeftAligned
+          content={<span>Adjust Brightness</span>}
+        >
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("wwwc");
+            }}
+          >
+            <AdjustIcon size="md" />
+          </Button>
+        </Tooltip>
+        <Tooltip position={TooltipPosition.bottom} content={<div>Invert</div>}>
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("invert");
+            }}
+          >
+            <BurnIcon size="md"></BurnIcon>
+          </Button>
+        </Tooltip>
+        <Tooltip position={TooltipPosition.bottom} content={<div>Invert</div>}>
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("rotate");
+            }}
+          >
+            <UndoIcon size="md"></UndoIcon>
+          </Button>
+        </Tooltip>
+        <Tooltip
+          position={TooltipPosition.bottom}
+          content={<div>Stack Scroll</div>}
+        >
+          <Button
+            variant="link"
+            onClick={() => {
+              handleToolbarAction("stackScroll");
+            }}
+          >
+            <BarsIcon size="md"></BarsIcon>
+          </Button>
+        </Tooltip>
+        <Button variant="link">
+          <DownloadIcon size="md" />
+        </Button>
+      </div>
     </div>
   );
 };
