131bae1ad4261294714ce2d588e6a5280b0288d4
PintoGideon <gideonpinto123@gmail.com>
2021-01-25 16:07:54 -0500

    Added markers to links in the Feed Tree

---
+++ src/components/feed/FeedTree/Marker.tsx
@@ -0,0 +1,26 @@
+import React, { useRef, useEffect } from "react";
+import { select } from "d3";
+
+const Marker = () => {
+  const markerRef = useRef<SVGDefsElement>(null);
+
+  useEffect(() => {
+    select(markerRef.current)
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 6)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+      .append("svg:path")
+      .attr("d", "M0,-5L10,0L0,5")
+      .attr("fill", "#fff");
+  });
+
+  return (
+    <defs ref={markerRef}>
+      <marker id="end-arrow"></marker>
+    </defs>
+  );
+};
+
+export default Marker;
