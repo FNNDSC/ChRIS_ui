80ab0c5cf02b94e8dc0b049dace6aa44fa66142a
PintoGideon <gideonpinto123@gmail.com>
2021-01-28 19:41:27 -0500

    Final Update to the Click events in the Node Tree

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -48,7 +48,7 @@ class Header extends React.Component<IHeaderProps> {
         <Badge key={3} style={BadgeStyle}>
           <span>
             Latest update:{" "}
-            <Moment format="DD MMM YYYY @ HH:mm">{`2021-01-28T20:15:14.297464-04:00`}</Moment>
+            <Moment format="DD MMM YYYY @ HH:mm">{`2021-01-28T20:45:14.297464-04:00`}</Moment>
           </span>
         </Badge>
       </React.Fragment>
