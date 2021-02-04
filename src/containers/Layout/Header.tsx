63a8a24d92e99915f07520ed7ddf36e468e68a11
PintoGideon <gideonpinto123@gmail.com>
2021-02-04 12:32:26 -0500

    Bug Fixed for svg overflow

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -43,12 +43,12 @@ class Header extends React.Component<IHeaderProps> {
       <React.Fragment>
         <Brand src={brandImg} alt="ChRIS Logo" />
         <Badge key={4} style={BadgeStyle}>
-          <span>Version: 1.1.0</span>
+          <span>Version: 1.1.1</span>
         </Badge>
         <Badge key={3} style={BadgeStyle}>
           <span>
             Latest update:{" "}
-            <Moment format="DD MMM YYYY @ HH:mm">{`2021-02-03T15:10:14.297464-04:00`}</Moment>
+            <Moment format="DD MMM YYYY @ HH:mm">{`2021-02-04T13:30:14.297464-04:00`}</Moment>
           </span>
         </Badge>
       </React.Fragment>
