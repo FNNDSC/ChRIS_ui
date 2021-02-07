5ab68beb86b13d046e60f4f1876253b7f974abaf
PintoGideon <gideonpinto123@gmail.com>
2021-02-06 19:57:54 -0500

    Optimized the data tree generation

--- src/components/feed/FeedTree/Link.tsx
+++ src/components/feed/FeedTree/Link.tsx
@@ -18,61 +18,63 @@ type LinkState = {
   initialStyle: { opacity: number };
 };
 
-export default class Link extends React.Component<LinkProps, LinkState> {
-  private linkRef: SVGPathElement | null=null;
-  state = {
-    initialStyle: {
-      opacity: 0,
-    },
-  };
+class Link extends React.Component<LinkProps, LinkState> {
+   private linkRef: SVGPathElement | null = null;
+   state = {
+     initialStyle: {
+       opacity: 0,
+     },
+   };
 
-  componentDidMount() {
-    this.applyOpacity(1, 500);
-  }
+   componentDidMount() {
+     this.applyOpacity(1, 0);
+   }
 
-  componentWillLeave(done:()=>{}) {
-    this.applyOpacity(0, 500, done);
-  }
+   componentWillLeave(done: () => {}) {
+     this.applyOpacity(1, 0, done);
+   }
 
-  applyOpacity(opacity: number, transitionDuration: number, done = () => {}) {
-    select(this.linkRef).transition().duration(transitionDuration).style('opacity',opacity).on('end',done)
-  }
+   applyOpacity(opacity: number, transitionDuration: number, done = () => {}) {
+     select(this.linkRef).style("opacity", opacity).on("end", done);
+   }
 
-  nodeRadius = 12;
-  drawPath = () => {
-    const { linkData, orientation } = this.props;
-    const { source, target } = linkData;
-    const deltaX = target.x - source.x,
-      deltaY = target.y - source.y,
-      dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-      normX = deltaX / dist,
-      normY = deltaY / dist,
-      sourcePadding = this.nodeRadius,
-      targetPadding = this.nodeRadius + 4,
-      sourceX = source.x + sourcePadding * normX,
-      sourceY = source.y + sourcePadding * normY,
-      targetX = target.x - targetPadding * normX,
-      targetY = target.y - targetPadding * normY;
-    return orientation === "horizontal"
-      ? `M ${sourceY} ${sourceX} L ${targetY} ${targetX}`
-      : `M ${sourceX} ${sourceY} L ${targetX} ${targetY}`;
-  };
+   nodeRadius = 12;
+   drawPath = () => {
+     const { linkData, orientation } = this.props;
+     const { source, target } = linkData;
+     const deltaX = target.x - source.x,
+       deltaY = target.y - source.y,
+       dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+       normX = deltaX / dist,
+       normY = deltaY / dist,
+       sourcePadding = this.nodeRadius,
+       targetPadding = this.nodeRadius + 4,
+       sourceX = source.x + sourcePadding * normX,
+       sourceY = source.y + sourcePadding * normY,
+       targetX = target.x - targetPadding * normX,
+       targetY = target.y - targetPadding * normY;
+     return orientation === "horizontal"
+       ? `M ${sourceY} ${sourceX} L ${targetY} ${targetX}`
+       : `M ${sourceX} ${sourceY} L ${targetX} ${targetY}`;
+   };
 
-  render() {
-    const { linkData } = this.props;
-    return (
-      <Fragment>
-        <path
-          ref={l=>{
-            this.linkRef=l;
-          }}
-          className="link"
-          d={this.drawPath()}
-          style={{   ...this.state.initialStyle   }}
-          data-source-id={linkData.source.id}
-          data-target-id={linkData.target.id}
-        />
-      </Fragment>
-    );
-  }
-}
+   render() {
+     const { linkData } = this.props;
+     return (
+       <Fragment>
+         <path
+           ref={(l) => {
+             this.linkRef = l;
+           }}
+           className="link"
+           d={this.drawPath()}
+           style={{ ...this.state.initialStyle }}
+           data-source-id={linkData.source.id}
+           data-target-id={linkData.target.id}
+         />
+       </Fragment>
+     );
+   }
+ }
+
+export default React.memo(Link);
---
+++ src/components/feed/FeedTree/ParentComponent.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { Spinner } from "@patternfly/react-core";
+import { ApplicationState } from "../../../store/root/applicationState";
+import { PluginInstancePayload, FeedTreeProp } from "../../../store/feed/types";
+import { setFeedTreeProp } from "../../../store/feed/actions";
+import { PluginInstance } from "@fnndsc/chrisapi";
+import FeedTree from "./FeedTree";
+import { getFeedTree, TreeNodeDatum } from "./data";
+
+interface ParentComponentProps {
+  pluginInstances: PluginInstancePayload;
+  onNodeClick: (node: PluginInstance) => void;
+  feedTreeProp: FeedTreeProp;
+  setFeedTreeProp: (orientation: string) => void;
+}
+
+const ParentComponent = (props: ParentComponentProps) => {
+  const { onNodeClick, pluginInstances, feedTreeProp, setFeedTreeProp } = props;
+  const { data: instances } = pluginInstances;
+  const [data, setData] = React.useState<TreeNodeDatum[]>([]);
+
+  React.useEffect(() => {
+    if (instances && instances.length > 0) {
+      const data = getFeedTree(instances);
+      setData(data);
+    }
+  }, [instances]);
+
+  const changeOrientation = (orientation: string) => {
+    setFeedTreeProp(orientation);
+  };
+
+  return data && data.length > 0 ? (
+    <FeedTree
+      data={data}
+      onNodeClick={onNodeClick}
+      zoom={1}
+      nodeSize={{ x: 85, y: 60 }}
+      separation={
+        instances && instances.length > 30
+          ? {
+              siblings: 0.5,
+              nonSiblings: 0.5,
+            }
+          : {
+              siblings: 1.0,
+              nonSiblings: 2.0,
+            }
+      }
+      feedTreeProp={feedTreeProp}
+      instances={instances}
+      changeOrientation={changeOrientation}
+    />
+  ) : (
+    <Spinner size="lg" />
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  pluginInstances: state.feed.pluginInstances,
+  feedTreeProp: state.feed.feedTreeProp,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setFeedTreeProp: (orientation: string) =>
+    dispatch(setFeedTreeProp(orientation)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ParentComponent);
