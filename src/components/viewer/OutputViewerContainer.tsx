cff3791249688371d386d4e49f58a7f238358e71
PintoGideon <gideonpinto123@gmail.com>
2021-03-10 10:48:31 -0500

    Refactored Output Viewer Container

--- src/components/viewer/OutputViewerContainer.tsx
+++ src/components/viewer/OutputViewerContainer.tsx
@@ -1,92 +1,53 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useTypedSelector } from "../../store/hooks";
 import { Tabs, Tab, Alert } from "@patternfly/react-core";
-import { ApplicationState } from "../../store/root/applicationState";
-import { FeedFile, PluginInstance } from "@fnndsc/chrisapi";
-import { pluginViewerMap } from "../../api/models/file-viewer.model";
-import { FileBrowserViewer } from "./displays";
-import "./viewer.scss";
-import { getSelectedFiles } from "../../store/feed/selector";
 
-type AllProps = {
-  files?: FeedFile[];
-  selected?: PluginInstance;
-};
+const OutputViewerContainer = () => {
+  const { pluginFiles, selectedPlugin } = useTypedSelector(
+    (state) => state.feed
+  );
 
-class OutputViewerContainer extends React.Component<
-  AllProps,
-  { activeTabKey: number }
-> {
-  constructor(props: AllProps) {
-    super(props);
-    this.handleTabClick = this.handleTabClick.bind(this);
-  }
+  const [activeTabKey, setActiveTabKey] = React.useState(0);
 
-  state = {
-    activeTabKey: 0,
-  };
+  if (!selectedPlugin || !pluginFiles) {
+    return <Alert variant="info" title="Empty Result Set" className="empty" />;
+  } else {
+    const selectedFiles = pluginFiles[selectedPlugin.data.id];
 
-  render() {
-    const tabs = this.buildTabArray();
+    const buildTabs = () => {
+      const tabContent = !!selectedFiles && !!selectedPlugin && <div>Test</div>;
+      const tabs = [];
+
+      tabs.push(
+        <Tab
+          key={selectedPlugin.data.id}
+          title="Tab"
+          eventKey={selectedPlugin.data.id}
+        >
+          {tabContent}
+        </Tab>
+      );
+
+      return tabs;
+    };
+
+    const tabs = buildTabs();
+    const handleTabClick = (
+      event: React.MouseEvent<HTMLElement, MouseEvent>,
+      tabIndex: React.ReactText
+    ) => {
+      setActiveTabKey(tabIndex as number);
+    };
     return (
       <div className="output-viewer">
-        {!!tabs && tabs.length ? (
-          <Tabs
-            activeKey={this.state.activeTabKey}
-            onSelect={this.handleTabClick}
-          >
+        {
+          <Tabs activeKey={activeTabKey} onSelect={handleTabClick}>
             {tabs}
           </Tabs>
-        ) : (
-          <Alert variant="info" title="Empty Result Set" className="empty" />
-        )}
+        }
       </div>
     );
   }
+};
 
-  // Description: Toggle currently active tab
-  handleTabClick = (
-    event: React.MouseEvent<HTMLElement, MouseEvent>,
-    tabIndex: React.ReactText
-  ) => {
-    this.setState({
-      activeTabKey: tabIndex as number,
-    });
-  };
-  // Description: Build Tabs from data
-  buildTabArray = () => {
-    const { files, selected } = this.props;
-
-    const tabs: any[] = [];
-    if (!!selected) {
-      const plugin_name = selected.data.plugin_name.split("-")[1];
-      const tabArr = pluginViewerMap[plugin_name] || pluginViewerMap.default;
-
-      tabArr.forEach((key: string, i: number) => {
-        let tabContent;
-        let label = "tab";
-        switch (key) {
-          case "FileBrowserViewer":
-            label = "File Browser";
-            tabContent = !!files && !!selected && (
-              <FileBrowserViewer files={files} selected={selected} />
-            );
-            break;
-        }
-        tabs.push(
-          <Tab key={i} eventKey={i} title={label}>
-            {tabContent}
-          </Tab>
-        );
-      });
-    }
-    return tabs;
-  };
-}
-
-const mapStateToProps = (state: ApplicationState) => ({
-  files: getSelectedFiles(state),
-  selected: state.feed.selectedPlugin,
-});
-
-export default connect(mapStateToProps, null)(OutputViewerContainer);
+export default OutputViewerContainer;
