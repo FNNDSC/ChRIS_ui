0686c2cac18d2b344c394f24e3fab1daaac20fbb
PintoGideon <gideonpinto123@gmail.com>
2021-03-25 23:26:31 -0400

    Reworked File Output Browser Viewer

--- src/components/detailedView/displays/FileBrowserViewer.tsx
+++ src/components/detailedView/displays/FileBrowserViewer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useTypedSelector } from "../../../store/hooks";
 import { useDispatch } from "react-redux";
 import { Tree } from "antd";
-import { GridItem, Grid } from "@patternfly/react-core";
+import {
+  GridItem,
+  Grid,
+  BreadcrumbItem,
+  Breadcrumb,
+} from "@patternfly/react-core";
 import { Key } from "../../../store/explorer/types";
 import FileDetailView from "../../feed/Preview/FileDetailView";
 import GalleryDicomView from "../../dicomViewer/GalleryDicomView";
@@ -12,20 +17,30 @@ const FileBrowserViewer = () => {
   const { explorer, selectedFile, viewerMode } = useTypedSelector(
     (state) => state.explorer
   );
+  const selectedPlugin = useTypedSelector((state) => state.feed.selectedPlugin);
   const dispatch = useDispatch();
 
   const onSelect = (selectedKeys: Key[], info: any) => {
     dispatch(setSelectedFile(info.node));
   };
 
+  //@ts-ignore
+  const splitPath = selectedPlugin?.data.output_path.split("/");
+  const breadcrumbItems = splitPath.slice(0, splitPath.length - 1);
+
   const selectedKeys = selectedFile ? [selectedFile.key] : [];
-  console.log("SelectedKeys", selectedKeys);;
 
   return (
     <div className="pf-u-px-lg">
       {!viewerMode ? (
         <Grid>
           <GridItem className="pf-u-p-sm" sm={12} md={4}>
+            <Breadcrumb>
+              {breadcrumbItems.map((item: string, index: number) => {
+                return <BreadcrumbItem key={index}>{item}</BreadcrumbItem>;
+              })}
+            </Breadcrumb>
+
             <Tree
               defaultExpandedKeys={selectedKeys}
               selectedKeys={selectedKeys}
