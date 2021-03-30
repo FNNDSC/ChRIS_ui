3cf934f38591f2b4adc3573ba7bc13a38d68cf55
PintoGideon <gideonpinto123@gmail.com>
2021-03-30 16:14:45 -0400

    Modifications to the Feed List

--- src/components/feed/FeedOutputBrowser/utils/index.ts
+++ src/components/feed/FeedOutputBrowser/utils/index.ts
@@ -39,7 +39,7 @@ export function bytesToSize(bytes: number) {
   if (bytes === 0) return "N/A";
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
   if (i === 0) return `${bytes} ${sizes[i]}`;
-  return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`;
+  return `${(bytes / Math.pow(1024, i)).toFixed(0)} ${sizes[i]}`;
 }
 
 // Format plugin name to "Name_vVersion_ID"
