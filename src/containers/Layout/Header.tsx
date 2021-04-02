8b52b6a7ba1a8023d74a2772c728f97d76fc51c9
PintoGideon <gideonpinto123@gmail.com>
2021-04-02 11:40:59 -0400

    Update Version String

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -48,7 +48,7 @@ class Header extends React.Component<IHeaderProps> {
         <Badge key={3} style={BadgeStyle}>
           <span>
             Latest update:{" "}
-            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-2T11:45:10.297464-04:00`}</Moment>
+            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-02T11:45:10.297464-04:00`}</Moment>
           </span>
         </Badge>
       </React.Fragment>
