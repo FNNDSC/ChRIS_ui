d68da4e77b7b9c6db0e2955272d21c4b10723c96
PintoGideon <gideonpinto123@gmail.com>
2021-04-03 12:19:16 -0400

    Update CSS for plugin version

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -43,12 +43,12 @@ class Header extends React.Component<IHeaderProps> {
       <React.Fragment>
         <Brand src={brandImg} alt="ChRIS Logo" />
         <Badge key={4} style={BadgeStyle}>
-          <span>Version: 1.3.8</span>
+          <span>Version: 1.3.9</span>
         </Badge>
         <Badge key={3} style={BadgeStyle}>
           <span>
             Latest update:{" "}
-            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-02T11:45:10.297464-04:00`}</Moment>
+            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-03T12:10:10.297464-04:00`}</Moment>
           </span>
         </Badge>
       </React.Fragment>
