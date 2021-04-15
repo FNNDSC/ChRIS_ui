872e8def5f5ad2952d05695446b859f04d8a7017
Alisamar Husain <zrthxn@gmail.com>
2021-04-15 17:48:04 +0530

    ChRIS logo brand image

--- src/pages/LogIn/Login.tsx
+++ src/pages/LogIn/Login.tsx
@@ -1,15 +1,40 @@
 import * as React from "react";
-import { RouteComponentProps } from "react-router-dom";
-import { LoginPage, LoginMainFooterBandItem } from "@patternfly/react-core";
-import LoginFormComponent from "./components/LoginForm";
-import "./login.scss";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { 
+  LoginPage, LoginMainFooterBandItem,
+  LoginFooterItem,
+  ListItem
+} from "@patternfly/react-core";
 
-import { Link } from "react-router-dom";
+import brandImg from '../../assets/images/chris-logo-inline.png'
+import LoginFormComponent from "./components/LoginForm";
+
+import "./login.scss";
 
 type AllProps = RouteComponentProps;
 
+const loginTextDesc = `
+  ChRIS is an open source platform that connects an open community of researchers 
+  and developers to data quickly and reliably from one computing environment to another.
+  `
+
+const listItem = (
+  <React.Fragment>
+    <ListItem>
+      <LoginFooterItem href="#">Terms of Use </LoginFooterItem>
+    </ListItem>
+    <ListItem>
+      <LoginFooterItem href="#">Help</LoginFooterItem>
+    </ListItem>
+    <ListItem>
+      <LoginFooterItem href="#">Privacy Policy</LoginFooterItem>
+    </ListItem>
+  </React.Fragment>
+);
+
 const LogInPage: React.FC<AllProps> = () => {
   React.useEffect(() => {
+    // Consider switching to react-helmet?
     document.title = "Log in into your ChRIS Account";
   }, []);
 
@@ -23,6 +48,10 @@ const LogInPage: React.FC<AllProps> = () => {
       className="login pf-background"
       loginTitle="Log in to your account"
       signUpForAccountMessage={signUpForAccountMessage}
+      textContent={loginTextDesc}
+      brandImgSrc={brandImg}
+      brandImgAlt="ChRIS logo"
+      // footerListItems={listItem}
     >
       <LoginFormComponent />
     </LoginPage>
