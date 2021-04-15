e14b0a2095f8864337a51d5342d6d5ccb7220f40
PintoGideon <gideonpinto123@gmail.com>
2021-04-14 20:25:12 -0400

    Update Version String

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -43,12 +43,12 @@ class Header extends React.Component<IHeaderProps> {
       <React.Fragment>
         <Brand src={brandImg} alt="ChRIS Logo" />
         <Badge key={4} style={BadgeStyle}>
-          <span>Version: 1.3.9</span>
+          <span>Version: 1.4.0</span>
         </Badge>
         <Badge key={3} style={BadgeStyle}>
           <span>
             Latest update:{" "}
-            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-08T17:00:10.297464-04:00`}</Moment>
+            <Moment format="DD MMM YYYY @ HH:mm">{`2021-04-14T20:30:10.297464-04:00`}</Moment>
           </span>
         </Badge>
       </React.Fragment>
