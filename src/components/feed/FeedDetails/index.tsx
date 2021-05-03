b51de6ee1d6d307cebc5d22ef30f8da0b001122d
PintoGideon <gideonpinto123@gmail.com>
2021-05-03 18:58:34 -0400

    Minimal Feed Details Panel

--- src/components/feed/FeedDetails/index.tsx
+++ src/components/feed/FeedDetails/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Moment from "react-moment";
-import { connect } from "react-redux";
 import { Skeleton } from "@patternfly/react-core";
 import ShareFeed from "../ShareFeed/ShareFeed";
 import { FeedPayload, PluginInstancePayload } from "../../../store/feed/types";
@@ -12,44 +11,27 @@ import {
   CalendarAltIcon,
   FileAltIcon,
 } from "@patternfly/react-icons";
+import { useTypedSelector } from "../../../store/hooks";
 
 interface INoteState {
   feedDescription?: string;
 }
 
-interface ReduxProps {
-  currentFeed?: FeedPayload;
-  instances?: PluginInstancePayload;
-}
+const FeedDetails = () => {
+  const [feedDescription, setFeedDescription] = React.useState("");
+  const currentFeedPayload = useTypedSelector(
+    (state) => state.feed.currentFeed
+  );
+  const instancesPayload = useTypedSelector(
+    (state) => state.feed.pluginInstances
+  );
+  const { data: feed, error, loading } = currentFeedPayload;
+  const { data: plugins } = instancesPayload;
 
-class FeedDetails extends React.Component<ReduxProps, INoteState> {
-  state = {
-    feedDescription: "",
-  };
-
-  componentDidMount() {
-    this.fetchNote();
-  }
-
-  async fetchNote() {
-    const { currentFeed } = this.props;
-    if (currentFeed && currentFeed.data) {
-      const note = await currentFeed.data.getNote();
-      const { data } = note;
-
-      this.setState({
-        feedDescription: data.content,
-      });
-    }
-  }
-
-  calculateTotalRuntime() {
-    if (this.props.instances) {
-      const { data: plugins } = this.props.instances;
-
-      if (!plugins) {
-        return 0;
-      }
+  const calculateTotalRuntime = () => {
+    if (!plugins) {
+      return "";
+    } else {
       let runtime = 0;
       for (const plugin of plugins) {
         const start = new Date(plugin.data.start_date);
@@ -73,93 +55,64 @@ class FeedDetails extends React.Component<ReduxProps, INoteState> {
       }
       return runtimeStrings.join(", ");
     }
-    return "";
-  }
+  };
 
-  render() {
-    const { currentFeed } = this.props;
-    const { feedDescription } = this.state;
+  React.useEffect(() => {
+    async function fetchNode() {
+      if (feed) {
+        const note = await feed.getNote();
+        const { data: noteData } = note;
+        setFeedDescription(noteData.content);
+      }
+    }
+    fetchNode();
+  }, [feed]);
 
-    if (currentFeed) {
-      const {
-        data: feed,
-        error: feedError,
-        loading: feedLoading,
-      } = currentFeed;
+  if (feed) {
+    return (
+      <ul className='feed-details'>
+        <li>
+          <CodeBranchIcon />
+          {feed && <span> {feed.data.name} </span>}
+        </li>
+        <li>
+          <small>Creator</small>
+          <p>
+            <UserIcon size="sm" />{" "}
+            {feed && <span> {feed.data.creator_username} </span>}
+          </p>
+        </li>
+        <li>
+          <small>Created</small>
+          <p>
+            <CalendarAltIcon size="sm" />
+            <Moment format="DD MMM YYYY @ HH:mm">
+              {feed && feed.data.creation_date}
+            </Moment>
+          </p>
+        </li>
 
-      if (feedLoading) {
-        return (
-          <div className="feed-details">
-            <Skeleton screenreaderText="Fetching Content" />
-          </div>
-        );
-      } else if (feedError) {
-        return (
-          <div className="feed-details">
-            <p>
-              Oh snap! Looks like something went wrong. Please try again later.
-            </p>
-          </div>
-        );
-      } else if (feed) {
-        return (
-          <React.Fragment>
-            <div className="feed-details">
-              <div className="feed-details__list">
-                <ul>
-                  <div className="feed-details__list--container-1">
-                    <li className="feed-details__list--first">
-                      <CodeBranchIcon />
-                      {feed && <span> {feed.data.name} </span>}
-                    </li>
-                    <li>
-                      <small>Creator</small>
-                      <p>
-                        <UserIcon size="sm" />{" "}
-                        {feed && <span> {feed.data.creator_username} </span>}
-                      </p>
-                    </li>
-                    <li>
-                      <small>Created</small>
-                      <p>
-                        <CalendarAltIcon size="sm" />
-                        <Moment format="DD MMM YYYY @ HH:mm">
-                          {feed && feed.data.creation_date}
-                        </Moment>
-                      </p>
-                    </li>
+        <li>
+          <small>Feed Description</small>
+          <p>
+            <FileAltIcon />
+            {!feedDescription ? (
+              <span>None Provided</span>
+            ) : (
+              <span>{feedDescription}</span>
+            )}
+          </p>
+        </li>
+        <li>
+          <ShareFeed feed={feed} />
+        </li>
+      </ul>
+    );
+  } else if (loading) {
+    return <Skeleton />;
+  } else if (error) {
+    return <div>Error Found</div>;
+  } else return null;
+};
 
-                    <li>
-                      <small>Feed Description</small>
-                      <p>
-                        <FileAltIcon />
-                        {!feedDescription ? (
-                          <span>None Provided</span>
-                        ) : (
-                          <span>{feedDescription}</span>
-                        )}
-                      </p>
-                    </li>
-                  </div>
-
-                  <div className="feed-details__list--container-2">
-                    <li>
-                      <ShareFeed feed={feed} />
-                    </li>
-                  </div>
-                </ul>
-              </div>
-            </div>
-          </React.Fragment>
-        );
-      } else return null;
-    } 
-  }
-}
-
-const mapStateToProps = ({ feed }: ApplicationState) => ({
-  currentFeed: feed.currentFeed,
-  instances: feed.pluginInstances,
-});
-
-export default connect(mapStateToProps, null)(FeedDetails);
+export default FeedDetails;
