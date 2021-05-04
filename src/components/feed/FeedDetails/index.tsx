65310531d17bb7022cba7775ea75992916cf31f4
PintoGideon <gideonpinto123@gmail.com>
2021-05-03 22:37:50 -0400

    Added Responsives to the Feed Details Panel

--- src/components/feed/FeedDetails/index.tsx
+++ src/components/feed/FeedDetails/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import Moment from "react-moment";
 import { Skeleton } from "@patternfly/react-core";
 import ShareFeed from "../ShareFeed/ShareFeed";
-import { FeedPayload, PluginInstancePayload } from "../../../store/feed/types";
-import { ApplicationState } from "../../../store/root/applicationState";
-import "./FeedDetails.scss";
+
 import {
   UserIcon,
   CodeBranchIcon,
@@ -12,50 +10,15 @@ import {
   FileAltIcon,
 } from "@patternfly/react-icons";
 import { useTypedSelector } from "../../../store/hooks";
-
-interface INoteState {
-  feedDescription?: string;
-}
+import "./FeedDetails.scss";
 
 const FeedDetails = () => {
   const [feedDescription, setFeedDescription] = React.useState("");
   const currentFeedPayload = useTypedSelector(
     (state) => state.feed.currentFeed
   );
-  const instancesPayload = useTypedSelector(
-    (state) => state.feed.pluginInstances
-  );
-  const { data: feed, error, loading } = currentFeedPayload;
-  const { data: plugins } = instancesPayload;
 
-  const calculateTotalRuntime = () => {
-    if (!plugins) {
-      return "";
-    } else {
-      let runtime = 0;
-      for (const plugin of plugins) {
-        const start = new Date(plugin.data.start_date);
-        const end = new Date(plugin.data.end_date);
-        const elapsed = end.getTime() - start.getTime(); // milliseconds between start and end
-        runtime += elapsed;
-      }
-      // format millisecond amount into human-readable string
-      const runtimeStrings = [];
-      const timeParts = [
-        ["day", Math.floor(runtime / (1000 * 60 * 60 * 24))],
-        ["hr", Math.floor((runtime / (1000 * 60 * 60)) % 24)],
-        ["min", Math.floor((runtime / 1000 / 60) % 60)],
-        ["sec", Math.floor((runtime / 1000) % 60)],
-      ];
-      for (const part of timeParts) {
-        const [name, value] = part;
-        if (value > 0) {
-          runtimeStrings.push(`${value} ${name}`);
-        }
-      }
-      return runtimeStrings.join(", ");
-    }
-  };
+  const { data: feed, error, loading } = currentFeedPayload;
 
   React.useEffect(() => {
     async function fetchNode() {
@@ -70,7 +33,7 @@ const FeedDetails = () => {
 
   if (feed) {
     return (
-      <ul className='feed-details'>
+      <ul className="feed-details">
         <li>
           <CodeBranchIcon />
           {feed && <span> {feed.data.name} </span>}
@@ -103,7 +66,9 @@ const FeedDetails = () => {
             )}
           </p>
         </li>
-        <li>
+        <li
+         
+        >
           <ShareFeed feed={feed} />
         </li>
       </ul>
