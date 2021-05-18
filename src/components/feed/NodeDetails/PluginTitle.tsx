c72cdd729054830d2638832d217649208cb66966
Gideon Pinto <gideonpinto123@gmail.com>
2021-05-18 11:59:02 -0400

    Implement Feedback by Rudolph (#260)
    * Bug fix in the workflows table
    
    * Add an info alert to the workflows page
    
    * Update Patternfly

--- src/components/feed/NodeDetails/PluginTitle.tsx
+++ src/components/feed/NodeDetails/PluginTitle.tsx
@@ -4,22 +4,31 @@ import { TextInput, Button, Title, Alert } from "@patternfly/react-core";
 import { useTypedSelector } from "../../../store/hooks";
 import { EditIcon } from "@patternfly/react-icons";
 import { setPluginTitle } from "../../../store/pluginInstance/actions";
+import { PluginInstance } from "@fnndsc/chrisapi";
+
+function getDefaultTitle(selectedPlugin?: PluginInstance) {
+  return selectedPlugin?.data.title || selectedPlugin?.data.plugin_name;
+}
 
 const PluginTitle = () => {
   const dispatch = useDispatch();
-  const [value, setValue] = React.useState("");
   const [showInput, setShowInput] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState();
   const selectedPlugin = useTypedSelector(
     (state) => state.instance.selectedPlugin
   );
-
+  const [value, setValue] = React.useState(getDefaultTitle(selectedPlugin));
   const title = selectedPlugin?.data.title || selectedPlugin?.data.plugin_name;
 
   const handleOnChange = (value: string) => {
     setValue(value);
   };
+
+  React.useEffect(()=>{
+    const title = getDefaultTitle(selectedPlugin);
+    setValue(title);
+  },[selectedPlugin])
 
   const handleSubmit = async () => {
     try {
@@ -47,8 +56,19 @@ const PluginTitle = () => {
             value={value}
             className="node-details__title--formInput"
           />
-          <Button onClick={handleSubmit}>
+          <Button
+           
+            onClick={handleSubmit}
+            className="node-details__title--formButton"
+          >
             {loading ? "Confirming" : "Confirm"}
+          </Button>
+          <Button
+            onClick={() => {
+              setShowInput(!showInput);
+            }}
+          >
+            Cancel
           </Button>
         </>
       ) : (
