547cfd3d3ebba04c8a337e57d6a7ab7c9cac9a40
Matan Kotler-Berkowitz <205matan@gmail.com>
2021-06-18 16:20:35 -0400

    Improve PFDCM type specificity

--- src/api/pfdcm/pfdcm-utils.ts
+++ src/api/pfdcm/pfdcm-utils.ts
@@ -1,4 +1,4 @@
-import { PACSStudy } from "./client";
+import { PACSStudy } from ".";
 
 /**
  * Parse Raw DCM Data
@@ -27,6 +27,39 @@ const isRawDcmItem = (item: RawDcmItem | RawDcmObject[]): item is RawDcmItem => 
   (item as RawDcmObject[]).length === undefined
 );
 
+function parseRawDcmValue(label: string, item: RawDcmItem) {
+  const { value } = item;
+
+  // treat as dates (typescript forbids fallthrough switches)
+  const dateLabels = [
+    'studyDate',
+    'patientBirthDate'
+  ]
+
+  // treat as numbers
+  const numberLabels = [
+    'numberOfStudyRelatedInstances', 
+    'numberOfStudyRelatedSeries',
+    'numberOfSeriesRelatedInstances',
+  ];
+
+  if (typeof value === 'string') {
+    if (dateLabels.includes(label)) {
+      const date = new Date();
+      date.setFullYear(parseInt(value.slice(0, 4)));
+      date.setMonth(parseInt(value.slice(4, 6)));
+      date.setDate(parseInt(value.slice(6)));
+      return date;
+    }
+    if (numberLabels.includes(label)) {
+      return parseInt(value);
+    }
+  }
+
+  return value;
+}
+
+
 export function flattenDcmArray(dcmArray: RawDcmObject[]) {
   return dcmArray.map((dcmObject) => {
     const flatObject: any = {};
@@ -39,7 +72,7 @@ export function flattenDcmArray(dcmArray: RawDcmObject[]) {
       const camelCaseLabel = `${label[0].toLowerCase()}${label.slice(1)}`;
 
       if (isRawDcmItem(item)) {
-        flatObject[camelCaseLabel] = item.value;
+        flatObject[camelCaseLabel] = parseRawDcmValue(camelCaseLabel, item);
       } else {
         flatObject[label] = flattenDcmArray(item);
       }
