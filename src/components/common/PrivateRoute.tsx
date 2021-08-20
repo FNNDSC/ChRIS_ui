6acfc78e8381c8a750b93de5ef56207f46fd1ec7
Alisamar Husain <zrthxn@gmail.com>
2021-08-20 13:57:40 +0530

    New simpler login and simpler private route

--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -1,31 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
-import { ApplicationState } from "../../store/root/applicationState";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+import { useTypedSelector } from "../../store/hooks";
 
+const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+  const isLoggedIn = useTypedSelector(({ user }) => user.isLoggedIn);
 
-// Description: Protected routes. Only show if user is logged in
-// Pass an optional param: redirectPath - for passing a redirect option to another path / defaults to "/login"
-interface PrivateRouteProps {
-  component: React.FC<any>;
-  path: string;
-  exact?: boolean;
-  isLoggedIn?: boolean;
-}
+  if (isLoggedIn)
+    return <Route {...props} />
 
-
-
-const PrivateRoute: React.FC<PrivateRouteProps> = (
-  props: PrivateRouteProps
-) => {
-  const { isLoggedIn } = props;
-
-  return isLoggedIn ? <Route {...props} /> : <Redirect to="/login" />;
+  return <Redirect to={`/login?then=${props.location?.pathname}`} />;
 };
 
-const mapStateToProps = ({ user }: ApplicationState) => ({
-  isLoggedIn: user.isLoggedIn,
-});
-
-// export default PrivateRoute;
-export default connect(mapStateToProps, null)(PrivateRoute);
+export default PrivateRoute;
