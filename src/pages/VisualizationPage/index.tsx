0c99d207d5fbac3ddc9dcc7f1c45c35ba931cc5a
PintoGideon <gideonpinto123@gmail.com>
2021-08-30 23:26:05 -0400

    Add a general purpose viewer

--- src/pages/VisualizationPage/index.tsx
+++ src/pages/VisualizationPage/index.tsx
@@ -1,79 +1,64 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import Wrapper from "../../containers/Layout/PageWrapper";
-import { Upload, message } from "antd";
-import { AiOutlineInbox } from "react-icons/ai";
-const { Dragger } = Upload;
-
-export interface OriRcFile extends File {
-  uid: string;
-}
-
-export interface RcFile extends OriRcFile {
-  readonly lastModifiedDate: Date;
-}
-
-export declare type UploadFileStatus =
-  | "error"
-  | "success"
-  | "done"
-  | "uploading"
-  | "removed";
-
-export interface UploadFile<T = any> {
-  uid: string;
-  size?: number;
-  name: string;
-  fileName?: string;
-  lastModified?: number;
-  lastModifiedDate?: Date;
-  url?: string;
-  status?: UploadFileStatus;
-  percent?: number;
-  thumbUrl?: string;
-  originFileObj?: RcFile;
-  response?: T;
-  error?: any;
-  linkProps?: any;
-  type?: string;
-  xhr?: T;
-  preview?: string;
-}
+import { Button } from "antd";
+import { AiOutlineUpload } from "react-icons/ai";
+import { ModalVariant, Modal } from "@patternfly/react-core";
 
 const VisualizationPage = () => {
-  const [fileList, setFileList] = useState<UploadFile<any>[]>([]);
+  const fileOpen = useRef<HTMLInputElement>(null);
+  const folderOpen = useRef<HTMLInputElement>(null);
+  const handleOpenFolder = (files: any) => {
+    console.log("Files", files);
+  };
+
+  const handleOpenLocalFs = (files: any) => {
+    console.log("Files", files);
+  };
+
+  const showOpenFolder = () => {
+    if (folderOpen.current) {
+      folderOpen.current.click();
+    }
+  };
+  const showOpenFile = () => {
+    if (fileOpen.current) {
+      fileOpen.current.click();
+    }
+  };
 
   return (
     <Wrapper>
-      <>
-        <Dragger
-          fileList={fileList}
-          name="file"
-          multiple={true}
-          onChange={(info) => {
-            const { status } = info.file;
-            if (status !== "uploading") {
-            }
-            if (status === "done") {
-              message.success(`${info.file.name} file uploaded successfully.`);
-            } else if (status === "error") {
-              message.error(`${info.file.name} file upload failed.`);
-            }
-            setFileList(info.fileList);
-          }}
-          onDrop={(e) => {
-            // setFileList(e.dataTransfer.files);
-          }}
-        >
-          <p className="ant-upload-drag-icon">
-            <AiOutlineInbox />
-          </p>
-          <p className="ant-upload-text">
-            Click or drag file to this area to upload
-          </p>
-          <p className="ant-upload-hint">Support for a single or bulk upload</p>
-        </Dragger>
-        <span>{fileList.length}</span>
-      </>
+      <div>
+        <Button onClick={showOpenFolder} icon={<AiOutlineUpload />}>
+          Upload a Directory
+        </Button>
+        <Button onClick={showOpenFile} icon={<AiOutlineUpload />}>
+          Upload Files
+        </Button>
+      </div>
+      <div>
+        <input
+          type="file"
+          id="file_open"
+          style={{ display: "none" }}
+          ref={fileOpen}
+          multiple
+          onChange={(e) => handleOpenLocalFs(e.target.files)}
+        />
+
+        <input
+          type="file"
+          id="file_folder"
+          style={{ display: "none" }}
+          onChange={(e) => handleOpenFolder(e.target.files)}
+          multiple
+          //@ts-ignore
+          webkitdirectory=""
+          mozdirectory=""
+          directory=""
+          ref={folderOpen}
+        />
+      </div>
     </Wrapper>
   );
 };
