037934b78a7f0a1ae83a0de0a7a7a803543fc27a
PintoGideon <gideonpinto123@gmail.com>
2021-09-04 08:14:51 -0400

    Modify Dicom Header

--- src/components/detailedView/displays/DicomViewer/index.tsx
+++ src/components/detailedView/displays/DicomViewer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import * as dicomParser from "dicom-parser";
 import * as cornerstone from "cornerstone-core";
