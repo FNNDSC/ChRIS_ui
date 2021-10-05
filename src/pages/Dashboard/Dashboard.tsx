8207a86380359e0751ba0075f0e47f8819b91a84
PintoGideon <gideonpinto123@gmail.com>
2021-10-04 21:54:23 -0400

    Overview

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,24 +1,44 @@
-import * as React from "react";
+import React, { CSSProperties } from "react";
 import { useDispatch } from "react-redux";
 import Wrapper from "../../containers/Layout/PageWrapper";
-import { Alert, PageSection } from "@patternfly/react-core";
+import {
+  Title,
+  PageSection,
+  Card,
+  CardBody,
+  CardTitle,
+  CardFooter,
+  CardHeader,
+  CardHeaderMain,
+  Grid,
+  GridItem,
+  Button,
+} from "@patternfly/react-core";
+import { MdOutlineImageSearch } from "react-icons/md";
+import { FaMagic } from "react-icons/fa";
 import { RouteComponentProps } from "react-router-dom";
 import { setSidebarActive } from "../../store/ui/actions";
+import FirstPng from "../../assets/images/img_1.png";
+import SecondPng from "../../assets/images/img_2.png";
+import ThirdPng from "../../assets/images/img_3.png";
+import FourthPng from "../../assets/images/img_4.png";
+import TreeOne from "../../assets/images/tree_1.png";
+import TreeTwo from "../../assets/images/tree_2.png";
+import TreeThree from "../../assets/images/tree_3.png";
+import TreeFour from "../../assets/images/tree_4.png";
 
 interface DashboardProps extends RouteComponentProps {
   children: React.ReactNode;
 }
 
+const style = {
+  height: "5em",
+  width: "5em",
+};
+
 const DashboardPage = (props: DashboardProps) => {
   const dispatch = useDispatch();
   const { children } = props;
-  const getTitle = () => {
-    return (
-      <>
-        <span>Welcome to the ChRIS UI Dashboard</span>
-      </>
-    );
-  };
 
   React.useEffect(() => {
     dispatch(
@@ -29,19 +49,112 @@ const DashboardPage = (props: DashboardProps) => {
     );
   }, [dispatch]);
 
+  const outlineSearch = <MdOutlineImageSearch style={style} />;
+  const magicWand = <FaMagic style={style} />;
 
   return (
     <Wrapper>
-      <PageSection>
-        <Alert
-          aria-label="welcome wagon"
-          variant="info"
-          title={getTitle()}
-        ></Alert>
+      <PageSection hasShadowBottom variant="light">
+        <Title headingLevel="h1">Welcome to ChRIS</Title>
+        <p>
+          Retrieve, analyze, and visualize image data using a powerful cloud
+          computing platform: ChRIS.
+          <b> Let&apos;s get started</b>
+        </p>
         {children}
+      </PageSection>
+      <PageSection>
+        <Grid hasGutter>
+          <GridItem style={{ marginBottom: "1rem" }} lg={6}>
+            <CardDisplay
+              component={
+                <div style={{ display: "flex" }}>
+                  <ImageComponent img={FirstPng} />
+                  <ImageComponent img={SecondPng} />
+                  <ImageComponent img={ThirdPng} />
+                  <ImageComponent img={FourthPng} />
+                </div>
+              }
+              title="You've got data!"
+              body='Visit "My Library" in the main navigation to review your data collection'
+              buttonText="Go to My Library"
+            />
+          </GridItem>
+          <GridItem lg={6}>
+            <CardDisplay
+              component={
+                <div style={{ display: "flex" }}>
+                  <ImageComponent img={TreeOne} />
+                  <ImageComponent img={TreeTwo} />
+                  <ImageComponent img={TreeThree} />
+                  <ImageComponent img={TreeFour} />
+                </div>
+              }
+              title="You've got analyses!"
+              body='Visit "My Analyses" in the main navigation to review your data analyses'
+              buttonText="Go to My Analyses"
+            />
+          </GridItem>
+
+          <GridItem lg={6}>
+            <CardDisplay
+              component={<LogoComponent logo={outlineSearch} />}
+              title="Discover and collect new data"
+              body='Visit "PACS" in the main navigation to look up data and save it your library'
+              buttonText="Query PACS"
+            />
+          </GridItem>
+          <GridItem lg={6}>
+            <CardDisplay
+              component={<LogoComponent logo={magicWand} />}
+              title="Analyze your data"
+              body="You can analyze data using an analysis template or creating a new custom analysis"
+              buttonText="Create New Analysis"
+            />
+          </GridItem>
+        </Grid>
       </PageSection>
     </Wrapper>
   );
 };
 
 export default DashboardPage;
+
+const CardDisplay = ({
+  component,
+  title,
+  body,
+  buttonText,
+}: {
+  component: React.ReactElement;
+  title: string;
+  body: string;
+  buttonText: string;
+}) => {
+  return (
+    <Card style={{ overflow: "hidden", width: "70%" }}>
+      <CardHeader
+        style={{ margin: "0 2rem", display: "flex", justifyContent: "center" }}
+      >
+        <CardHeaderMain>{component}</CardHeaderMain>
+      </CardHeader>
+      <div style={{ margin: "0 auto", textAlign: "center" }}>
+        <CardTitle>{title}</CardTitle>
+        <CardBody>{body}</CardBody>
+        <CardFooter>
+          <Button>{buttonText}</Button>
+        </CardFooter>
+      </div>
+    </Card>
+  );
+};
+
+const styleImg = { marginRight: "1em" };
+
+const ImageComponent = ({ img }: { img: string }) => {
+  return <img style={styleImg} src={img} />;
+};
+
+const LogoComponent = ({ logo }: { logo: JSX.Element }) => {
+  return <>{logo}</>;
+};
