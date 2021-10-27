b29251a87b62cde4ef8a5cddd09f9bdc1eacf5d4
bolajahmad <56865496+bolajahmad@users.noreply.github.com>
2021-10-27 12:42:32 +0100

    converting class components to hooks

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,28 +1,31 @@
 import * as React from "react";
 import Wrapper from "../../containers/Layout/PageWrapper";
-import { Alert, PageSection, PageSectionVariants } from "@patternfly/react-core";
+import {
+  Alert,
+  PageSection,
+  PageSectionVariants
+} from "@patternfly/react-core";
 import "./not-found.scss";
+import { useEffect } from "hoist-non-react-statics/node_modules/@types/react";
 
+const NotFoundPage: React.FunctionComponent = () => {
+  useEffect(() => {
+    document.title = "Page Not Found";
+  }, []);
 
-class NotFoundPage extends React.Component {
-    componentDidMount() {
-        document.title = "Page Not Found";
-    }
-
-    render() {
-        return (
-            <Wrapper>
-                <PageSection variant={PageSectionVariants.default}>
-                    <Alert
-                        aria-label="Page Not Found"
-                        variant="danger"
-                        title="Page Not Found!"  >
-                        Page Not Found! Go <a href="/">Home</a>
-                    </Alert>
-                </PageSection>
-            </Wrapper>
-        );
-    }
-}
+  return (
+    <Wrapper>
+      <PageSection variant={PageSectionVariants.default}>
+        <Alert
+          aria-label="Page Not Found"
+          variant="danger"
+          title="Page Not Found!"
+        >
+          Page Not Found! Go <a href="/">Home</a>
+        </Alert>
+      </PageSection>
+    </Wrapper>
+  );
+};
 
 export { NotFoundPage as NotFound };
