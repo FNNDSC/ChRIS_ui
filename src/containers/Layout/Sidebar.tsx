2764e30116d061f5adfd24f1e68e3ba68fdfe68e
bolajahmad <56865496+bolajahmad@users.noreply.github.com>
2021-10-29 07:06:47 +0100

    merging outreahy

--- src/containers/Layout/Sidebar.tsx
+++ src/containers/Layout/Sidebar.tsx
@@ -10,7 +10,7 @@ import {
   Nav,
   NavItem,
   NavList,
-  NavGroup,
+  NavGroup
 } from "@patternfly/react-core";
 import { setSidebarActive } from "../../store/ui/actions";
 import { Dispatch } from "redux";
@@ -22,12 +22,13 @@ type ReduxProp = {
 
 const Sidebar: React.FC<AllProps> = ({
   isNavOpen,
-  sidebarActiveItem,
+  sidebarActiveItem
 }: AllProps) => {
+  console.log("SidebarActiveItem", sidebarActiveItem);
   const onSelect = (selectedItem: any) => {
     const { itemId } = selectedItem;
     setSidebarActive({
-      activeItem: itemId,
+      activeItem: itemId
     });
   };
 
@@ -54,9 +55,6 @@ const Sidebar: React.FC<AllProps> = ({
           >
             <Link to="/feeds">My Analyses</Link>
           </NavItem>
-          <NavItem itemId="catalog" isActive={sidebarActiveItem === "catalog"}>
-            <Link to="/catalog">Analysis Catalog</Link>
-          </NavItem>
           <NavItem
             itemId="workflows"
             isActive={sidebarActiveItem === "workflows"}
@@ -71,20 +69,14 @@ const Sidebar: React.FC<AllProps> = ({
           >
             <Link to="/visualization">DICOM Viewer</Link>
           </NavItem>
+        </NavGroup>
+
+        <NavGroup title="Apps">
           <NavItem
-            itemId="sliceDrop"
-            isActive={sidebarActiveItem === "sliceDrop"}
+            itemId="covidnet"
+            isActive={sidebarActiveItem === "covidnet"}
           >
-            <Link to="/slicedrop">SliceDrop</Link>
-          </NavItem>
-          <NavItem itemId="medview" isActive={sidebarActiveItem === "medview"}>
-            <Link to="/medview">Medview</Link>
-          </NavItem>
-          <NavItem
-            itemId="fetalmri"
-            isActive={sidebarActiveItem === "fetalmri"}
-          >
-            <Link to="/fetalmri">Fetal MRI</Link>
+            <Link to="/covidnet">COVID-Net</Link>
           </NavItem>
         </NavGroup>
       </NavList>
@@ -96,12 +88,12 @@ const Sidebar: React.FC<AllProps> = ({
 
 const mapStateToProps = ({ user, ui }: ApplicationState) => ({
   isLoggedIn: user.isLoggedIn,
-  sidebarActiveItem: ui.sidebarActiveItem,
+  sidebarActiveItem: ui.sidebarActiveItem
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   setSidebarActive: (active: { activeItem: string }) =>
-    dispatch(setSidebarActive(active)),
+    dispatch(setSidebarActive(active))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
