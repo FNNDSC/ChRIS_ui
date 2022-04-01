c0fdf129db9f9ca3e62041759111c6fef4ac9cf2
Prakash Kumar <prakashope@gmail.com>
2022-04-01 19:21:14 +0530

    added code to open link in the parent window out side of the iframe

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -9,19 +9,20 @@ import "./not-found.scss";
 
 export const NotFoundPage: React.FC = () => {
   useEffect(() => {
-    document.title = "Page Not Found";
+    document.title = "Page Not Found";    
   }, []);
+
+
+
   return (
-    
       <PageSection variant={PageSectionVariants.default}>
         <Alert
           aria-label="Page Not Found"
           variant="danger"
           title="Page Not Found!"
         >
-          Page Not Found! Go <a href="/">Home</a>
+          Page Not Found! Go <a href="/" target="_PARENT">Home</a>
         </Alert>
       </PageSection>
-    
   );
 };
