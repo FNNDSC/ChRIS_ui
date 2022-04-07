29d42b6665d52ae773f3c7632aa54aeec1e83197
AryaArunB <arya2001bhosale@gmail.com>
2022-04-07 23:16:36 +0530

    Button for Already have an account?
    Signed-off-by: AryaArunB <arya2001bhosale@gmail.com>

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -24,6 +24,21 @@ const BadgeStyle = {
   color: "#8b8d8f",
 };
 
+const BadgeStyleLeft = {
+  ...BadgeStyle,
+  marginRight: "0px",
+  marginLeft:"-1rem",
+  minWidth:"30px",
+
+}
+
+const BadgeStyleRight = {
+  ...BadgeStyle,
+  marginRight: "-1rem",
+  marginLeft: "1.50rem",
+  minWidth:"50px",
+}
+
 const Header: React.FC<IHeaderProps> = ({
   onNavToggle,
   user,
@@ -41,6 +56,9 @@ const Header: React.FC<IHeaderProps> = ({
   const brand = (
     <React.Fragment>
       <Brand src={brandImg} alt="ChRIS Logo" />
+       <Badge key={4} style={BadgeStyleLeft}>
+        <span>Version: 3.1.3</span>
+      </Badge>
     </React.Fragment>
   );
 
