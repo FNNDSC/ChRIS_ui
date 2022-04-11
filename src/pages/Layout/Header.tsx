3d212e42105ef86a05d7bda0ef48f8d87f689948
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-04-11 11:44:31 -0400

    Initial WIP to show UI ver from env vars (using .env)

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -42,7 +42,7 @@ const Header: React.FC<IHeaderProps> = ({
     <React.Fragment>
       <Brand src={brandImg} alt="ChRIS Logo" />
       <Badge key={4} style={BadgeStyle}>
-        <span>Version: 3.1.4</span>
+        <span>Version: {process.env.REACT_APP_CHRIS_UI_VERSION} </span>
       </Badge>
       <Badge key={3} style={BadgeStyle}>
         <span>
