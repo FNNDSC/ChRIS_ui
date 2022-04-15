6b511a1bf3b949f76c516799077738a0307b1617
Prakash <prakashope@gmail.com>
2022-04-15 10:30:32 +0530

    added styles in main.scss and gave separate style objects to version and release date in  Header.tsx

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -57,10 +57,10 @@ const Header: React.FC<IHeaderProps> = ({
     <React.Fragment>
       <Brand src={brandImg} alt="ChRIS Logo" />
 
-      <Badge key={4} style={BadgeStyle}>
+      <Badge key={4} style={BadgeStyleLeft}>
         <span>Version: {process.env.REACT_APP_CHRIS_UI_VERSION} </span>
       </Badge>
-      <Badge key={3} style={BadgeStyle}>
+      <Badge key={3} style={BadgeStyleRight}>
         <span>
           Latest update:{" "}
           <Moment format="DD MMM YYYY @ HH:mm">{`2022-04-04T13:00:10.297464-04:00`}</Moment>
