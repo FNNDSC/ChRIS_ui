e04e69bf98a640e77e0a237fa329dab9e8b54a7a
Sandip Samal <sandip@havana.localdomain>
2022-05-23 12:58:49 -0400

    Added merge options and tool tips

--- src/store/feed/actions.ts
+++ src/store/feed/actions.ts
@@ -39,12 +39,22 @@ export const setFeedTreeProp = (orientation: string) =>
 
 export const deleteFeed = (feed: Feed[]) =>
   action(FeedActionTypes.DELETE_FEED, feed)
+  
+// Feed download
 export const downloadFeedRequest = (feed: Feed[]) =>
   action(FeedActionTypes.DOWNLOAD_FEED_REQUEST, feed)
 export const downloadFeedError = (error: string) =>
   action(FeedActionTypes.DOWNLOAD_FEED_ERROR, error)
 export const downloadFeedSuccess = (feed: Feed[]) =>
   action(FeedActionTypes.DOWNLOAD_FEED_SUCCESS, feed)
+ 
+// Feed Merge 
+export const mergeFeedRequest = (feed: Feed[]) =>
+  action(FeedActionTypes.MERGE_FEED_REQUEST, feed)
+export const mergeFeedError = (error: string) =>
+  action(FeedActionTypes.MERGE_FEED_ERROR, error)
+export const mergeFeedSuccess = (feed: Feed[]) =>
+  action(FeedActionTypes.MERGE_FEED_SUCCESS, feed)
 export const setFeedLayout = () => action(FeedActionTypes.SET_LAYOUT)
 
 export const resetFeed = () => action(FeedActionTypes.RESET_FEED)
