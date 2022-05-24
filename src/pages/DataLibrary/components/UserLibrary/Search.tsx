625ece205fa16d405a72b3e0aeb5d8291c872075
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2022-05-24 17:20:07 -0400

    Search-MVP

--- src/pages/DataLibrary/components/UserLibrary/Search.tsx
+++ src/pages/DataLibrary/components/UserLibrary/Search.tsx
@@ -10,6 +10,7 @@ import {
   setPagination,
   setRoot,
 } from './context/actions'
+import { useTypedSelector } from '../../../../store/hooks'
 
 type Paginate = {
   limit: number
@@ -23,12 +24,11 @@ const lookDeeper = async (
   maxDepth: number,
   type: string,
 ) => {
-  console.log('PATH', path)
   const client = ChrisAPIClient.getClient()
   const results: string[] = []
   if (maxDepth < 0) return []
   const subpaths = await client.getFileBrowserPath(path)
-  console.log('SUBPATHS', subpaths)
+  console.log('Subpaths', subpaths)
   if (
     subpaths &&
     subpaths.data &&
@@ -44,15 +44,15 @@ const lookDeeper = async (
     }
 
     if (maxDepth === 5 && type === 'pacs') {
-      console.log('FOLDERS', folders)
       folders = folders.filter((folder: string) => folder === 'SERVICES')
     }
 
     if (folders.length > 0) {
       for (let i = 0; i < folders.length; i++) {
         if (folders[i] === value || folders[i].includes(value)) {
-          if (type === 'feed') results.push(path)
+          if (type === 'feed') results.push(`${path}/${folders[i]}`)
           if (type === 'pacs') results.push(`${path}/${folders[i]}`)
+          if (type === 'uploads') results.push(`${path}/${folders[i]}`)
         }
       }
 
@@ -75,169 +75,168 @@ const lookDeeper = async (
   return results
 }
 
-const searchFeedFiles = async (value: string) => {
-  const path = 'chris'
+const searchFeedFiles = async (value: string, path: string) => {
   const maxDepth = 5
   const results = await lookDeeper(path, value, maxDepth, 'feed')
   return results
 }
 
-const searchPacsFiles = async (value: string) => {
-  const path = ''
+const searchPacsFiles = async (value: string, path: string) => {
   const maxDepth = 5
   const results = await lookDeeper(path, value, maxDepth, 'pacs')
   return results
 }
 
+const searchUploadedFiles = async (value: string, path: string) => {
+  const maxDepth = 5
+  const results = await lookDeeper(path, value, maxDepth, 'uploads')
+  return results
+}
+
 const Search = () => {
   const { dispatch } = useContext(LibraryContext)
+  const username = useTypedSelector((state) => state.user.username)
   const [value, setValue] = React.useState('')
   const [loading, setLoading] = React.useState(false)
   const [emptySet, setEmptySet] = React.useState('')
 
   const handleSearch = async () => {
-    const paginate: Paginate = {
-      limit: 100,
-      offset: 0,
-      fname_icontains: value,
-    }
-    const client = ChrisAPIClient.getClient()
-    const uploadFn = client.getUploadedFiles
+    if (value && username) {
+      setLoading(true)
 
-    const boundUploadFn = uploadFn.bind(client)
+      const client = ChrisAPIClient.getClient()
+      const uploadedFiles = await searchUploadedFiles(
+        value,
+        `${username}/uploads`,
+      )
+      const feedFiles = await searchFeedFiles(value, username)
+      const pacsFiles = await searchPacsFiles(value, '')
 
-    const uploadedFiles = await fetchResource(
-      { ...paginate, fname_nslashes: '3' },
-      boundUploadFn,
-    )
+      const isUploadedRoot =
+        uploadedFiles.length > 0 &&
+        feedFiles.length === 0 &&
+        pacsFiles.length === 0
 
-    const feedFiles = await searchFeedFiles(value)
-    const pacsFiles = await searchPacsFiles(value)
+      const isFeedRoot =
+        feedFiles.length > 0 &&
+        uploadedFiles.length === 0 &&
+        pacsFiles.length === 0
 
-    const isUploadedRoot =
-      uploadedFiles.length > 0 &&
-      feedFiles.length === 0 &&
-      pacsFiles.length === 0
+      const isPacsRoot =
+        pacsFiles.length > 0 &&
+        uploadedFiles.length === 0 &&
+        feedFiles.length === 0
 
-    const isFeedRoot =
-      feedFiles.length > 0 &&
-      uploadedFiles.length === 0 &&
-      pacsFiles.length === 0
+      if (uploadedFiles && uploadedFiles.length > 0) {
+        const uploadedFolders: string[] = []
+        let path = ''
 
-    const isPacsRoot =
-      pacsFiles.length > 0 &&
-      uploadedFiles.length === 0 &&
-      feedFiles.length === 0
+        uploadedFiles.forEach((file: any) => {
+          const names = file.split('/')
 
-    if (uploadedFiles && uploadedFiles.length > 0) {
-      const uploadedFolders: string[] = []
-      let path = ''
+          const index = names.findIndex((name: any, index: number) => {
+            if (name.toLowerCase() === value.toLowerCase()) {
+              return index
+            }
+          })
 
-      uploadedFiles.forEach((file: any) => {
-        const names = file.data.fname.split('/')
-
-        const index = names.findIndex((name: any, index: number) => {
-          if (name.toLowerCase() === value.toLowerCase()) {
-            return index
+          if (index !== -1) {
+            path = `${names[0]}/${names[1]}`
+            const folder = index === 2 ? names[index] : names[index - 1]
+            if (!uploadedFolders.includes(folder)) uploadedFolders.push(folder)
           }
         })
 
-        if (index !== -1) {
-          path = `${names[0]}/${names[1]}`
-          const folder = index === 2 ? names[index] : names[index - 1]
-          if (!uploadedFolders.includes(folder)) uploadedFolders.push(folder)
-        }
-      })
-
-      if (uploadedFolders.length > 0) {
-        dispatch(setFolders(uploadedFolders, path))
-        dispatch(setInitialPath(path, 'uploads'))
-        dispatch(setPaginatedFolders([], path))
-        dispatch(
-          setPagination(path, {
-            hasNext: false,
-            limit: 30,
-            offset: 0,
-            totalCount: 0,
-          }),
-        )
-        if (isUploadedRoot) {
-          dispatch(setRoot(true, 'uploads'))
+        if (uploadedFolders.length > 0) {
+          dispatch(setFolders(uploadedFolders, path))
+          dispatch(setInitialPath(path, 'uploads'))
+          dispatch(setPaginatedFolders([], path))
+          dispatch(
+            setPagination(path, {
+              hasNext: false,
+              limit: 30,
+              offset: 0,
+              totalCount: 0,
+            }),
+          )
+          if (isUploadedRoot) {
+            dispatch(setRoot(true, 'uploads'))
+          }
         }
       }
-    }
 
-    if (feedFiles && feedFiles.length > 0) {
-      const path = 'chris'
-      const feedFolders: string[] = []
-      feedFiles.forEach((file: any) => {
-        const folder = file.split('/')[1]
-        if (!feedFolders.includes(folder)) feedFolders.push(folder)
-      })
-      if (feedFolders.length > 0) {
-        dispatch(setFolders(feedFolders, path))
-        dispatch(setInitialPath(path, 'feed'))
-        dispatch(setPaginatedFolders([], path))
-        dispatch(
-          setPagination(path, {
-            hasNext: false,
-            limit: 30,
-            offset: 0,
-            totalCount: 0,
-          }),
-        )
-        if (isFeedRoot) {
-          dispatch(setRoot(true, 'feed'))
+      if (feedFiles && feedFiles.length > 0) {
+        const path = username
+        const feedFolders: string[] = []
+        feedFiles.forEach((file: any) => {
+          const folder = file.split('/')[1]
+          if (!feedFolders.includes(folder)) feedFolders.push(folder)
+        })
+        if (feedFolders.length > 0) {
+          dispatch(setFolders(feedFolders, path))
+          dispatch(setInitialPath(path, 'feed'))
+          dispatch(setPaginatedFolders([], path))
+          dispatch(
+            setPagination(path, {
+              hasNext: false,
+              limit: 30,
+              offset: 0,
+              totalCount: 0,
+            }),
+          )
+          if (isFeedRoot) {
+            dispatch(setRoot(true, 'feed'))
+          }
         }
       }
-    }
 
-    if (pacsFiles && pacsFiles.length > 0) {
-      const pacsFolders: string[] = []
-      const pacsDict: {
-        [key: string]: string[]
-      } = {}
+      if (pacsFiles && pacsFiles.length > 0) {
+        const pacsFolders: string[] = []
+        const pacsDict: {
+          [key: string]: string[]
+        } = {}
 
-      pacsFiles.forEach((file: any) => {
-        const fileName = file.split('/')
-        const folder = fileName[3]
-        const path = `${fileName[0]}/${fileName[1]}/${fileName[2]}`
-        if (!pacsFolders.includes(folder)) {
-          pacsFolders.push(folder)
-          pacsDict[path] = folder
+        pacsFiles.forEach((file: any) => {
+          const fileName = file.split('/')
+          const folder = fileName[3]
+          const path = `${fileName[0]}/${fileName[1]}/${fileName[2]}`
+          if (!pacsFolders.includes(folder)) {
+            pacsFolders.push(folder)
+            pacsDict[path] = folder
+          }
+        })
+
+        for (const i in pacsDict) {
+          dispatch(setFolders([pacsDict[i]], i))
+          dispatch(setInitialPath(i, 'services'))
         }
-      })
 
-      for (const i in pacsDict) {
-        dispatch(setFolders([pacsDict[i]], i))
-        dispatch(setInitialPath(i, 'services'))
+        if (pacsFolders.length > 0) {
+          dispatch(setPaginatedFolders([], 'SERVICES'))
+          dispatch(
+            setPagination('SERVICES', {
+              hasNext: false,
+              limit: 30,
+              offset: 0,
+              totalCount: 0,
+            }),
+          )
+          if (isPacsRoot) {
+            dispatch(setRoot(true, 'services'))
+          }
+        }
       }
 
-      if (pacsFolders.length > 0) {
-        dispatch(setPaginatedFolders([], 'SERVICES'))
-        dispatch(
-          setPagination('SERVICES', {
-            hasNext: false,
-            limit: 30,
-            offset: 0,
-            totalCount: 0,
-          }),
-        )
-        if (isPacsRoot) {
-          dispatch(setRoot(true, 'services'))
-        }
+      if (
+        feedFiles.length === 0 &&
+        uploadedFiles.length === 0 &&
+        pacsFiles.length === 0
+      ) {
+        setEmptySet('No dataset found')
       }
+      setLoading(false)
+      setValue('')
     }
-
-    if (
-      feedFiles.length === 0 &&
-      uploadedFiles.length === 0 &&
-      pacsFiles.length === 0
-    ) {
-      setEmptySet('No dataset found')
-    }
-    setLoading(false)
-    setValue('')
   }
 
   return (
@@ -264,10 +263,19 @@ const Search = () => {
         </Button>
       </Card>
       {loading && (
-        <>
-          <Spinner size="md" />
+        <div
+          style={{
+            marginTop: '1em',
+          }}
+        >
+          <Spinner
+            style={{
+              marginRight: '1em',
+            }}
+            size="md"
+          />
           <span>Fetching Search Results....</span>
-        </>
+        </div>
       )}
       {emptySet && <div>No Dataset Found</div>}
     </div>
