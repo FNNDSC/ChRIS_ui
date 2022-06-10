10d38bf437ae99ddbded73a457793e0eb9972c0d
PintoGideon <gideonpinto123@gmail.com>
2022-06-10 14:47:08 -0400

    Bug Fixes

--- src/components/catalog/DisplayPage.tsx
+++ src/components/catalog/DisplayPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react'
 import {
   Pagination,
   Card,
@@ -20,22 +20,21 @@ import {
   Button,
   Alert,
   TextInput,
-} from "@patternfly/react-core";
-import ReactJSON from "react-json-view";
-import { ImTree } from "react-icons/im";
-import { GrCloudComputer } from "react-icons/gr";
-import { FaCode } from "react-icons/fa";
-import Tree from "./Tree";
-import { PipelineList } from "@fnndsc/chrisapi";
-import ChrisAPIClient from "../../api/chrisapiclient";
-import { generatePipelineWithData } from "../feed/CreateFeed/utils/pipelines";
-
+} from '@patternfly/react-core'
+import ReactJSON from 'react-json-view'
+import { ImTree } from 'react-icons/im'
+import { GrCloudComputer } from 'react-icons/gr'
+import { FaCode } from 'react-icons/fa'
+import Tree from './Tree'
+import { PipelineList } from '@fnndsc/chrisapi'
+import ChrisAPIClient from '../../api/chrisapiclient'
+import { generatePipelineWithData } from '../feed/CreateFeed/utils/pipelines'
 
 interface PageState {
-  perPage: number;
-  page: number;
-  search: string;
-  itemCount: number;
+  perPage: number
+  page: number
+  search: string
+  itemCount: number
 }
 
 const DisplayPage = ({
@@ -53,114 +52,124 @@ const DisplayPage = ({
   handlePluginSearch,
   search,
 }: {
-  resources?: any[];
-  pageState: PageState;
-  onPerPageSelect: (_event: any, perPage: number) => void;
-  handleFilterChange: (value: string) => void;
-  onSetPage: (_event: any, page: number) => void;
-  selectedResource: any;
-  setSelectedResource: (resource: any) => void;
-  title: string;
-  showPipelineButton?: boolean;
-  fetch?: (id?: number) => void;
-  handlePluginSearch?: (search: string) => void;
-  handlePipelineSearch?: (search: string) => void;
-  handleComputeSearch?: (search: string) => void;
-  search: string;
+  resources?: any[]
+  pageState: PageState
+  onPerPageSelect: (_event: any, perPage: number) => void
+  handleFilterChange: (value: string) => void
+  onSetPage: (_event: any, page: number) => void
+  selectedResource: any
+  setSelectedResource: (resource: any) => void
+  title: string
+  showPipelineButton?: boolean
+  fetch?: (id?: number) => void
+  handlePluginSearch?: (search: string) => void
+  handlePipelineSearch?: (search: string) => void
+  handleComputeSearch?: (search: string) => void
+  search: string
 }) => {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
-  const fileOpen = React.useRef<HTMLInputElement>(null);
-  const [error, setError] = React.useState({});
-  const [deleteError, setDeleteError] = React.useState("");
+  const scrollRef = useRef<HTMLDivElement | null>(null)
+  const fileOpen = React.useRef<HTMLInputElement>(null)
+  const [error, setError] = React.useState({})
+  const [deleteError, setDeleteError] = React.useState('')
 
-  const [warningMessage, setWarningMessage] = React.useState("");
-  const { perPage, page, itemCount } = pageState;
-  const [isExpanded, setIsExpanded] = React.useState(false);
+  const [warningMessage, setWarningMessage] = React.useState('')
+  const { perPage, page, itemCount } = pageState
+  const [isExpanded, setIsExpanded] = React.useState(false)
   useEffect(() => {
     if (isExpanded) {
-      scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+      scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
     }
-  }, [isExpanded]);
+  }, [isExpanded])
 
-  const isPlugin = title === "Plugins" ? true : false;
-  const isPipeline = title === "Pipelines" ? true : false;
-  const isCompute = title === "Compute" ? true : false;
+  const isPlugin = title === 'Plugins' ? true : false
+  const isPipeline = title === 'Pipelines' ? true : false
+  const isCompute = title === 'Compute' ? true : false
 
   const iconStyle = {
     fill: isPlugin
-      ? "#0066CC"
+      ? '#0066CC'
       : isPipeline
-      ? "#1F0066"
+      ? '#1F0066'
       : isCompute
-      ? "red"
-      : "",
-    height: "1.5em",
-    width: "1.25em",
-    marginRight: "0.5em",
-    marginTop: "0.25em",
-  };
+      ? 'red'
+      : '',
+    height: '1.5em',
+    width: '1.25em',
+    marginRight: '0.5em',
+    marginTop: '0.25em',
+  }
 
   const showOpenFile = () => {
     if (fileOpen.current) {
-      fileOpen.current.click();
+      fileOpen.current.click()
     }
-  };
+  }
 
-  const readFile = (file: any) => {
-    const reader = new FileReader();
+  const cleanUp = (event: any) => {
+    event.target.value = null
+    if (fileOpen.current) {
+      fileOpen.current.value = ''
+    }
+  }
+
+  const readFile = (file: any, event: any) => {
+    const reader = new FileReader()
     reader.onloadend = async () => {
       try {
         if (reader.result) {
-          const client = ChrisAPIClient.getClient();
-          const result = JSON.parse(reader.result as string);
-          result["plugin_tree"] = JSON.stringify(result["plugin_tree"]);
+          const client = ChrisAPIClient.getClient()
+          const result = JSON.parse(reader.result as string)
+          result['plugin_tree'] = JSON.stringify(result['plugin_tree'])
           const pipelineInstanceList: PipelineList = await client.getPipelines({
             name: result.name,
-          });
+          })
 
           if (pipelineInstanceList.data) {
             setWarningMessage(
-              `pipeline with the name ${result.name} already exists`
-            );
+              `pipeline with the name ${result.name} already exists`,
+            )
           } else {
-            await generatePipelineWithData(result);
-            fetch && fetch();
+            await generatePipelineWithData(result)
+            fetch && fetch()
           }
         }
-      } catch (error: any) {
-        setError(error.response.data);
-      }
-    };
-    if (file && file.type === "application/json") {
-      try {
-        reader.readAsText(file);
+        cleanUp(event)
       } catch (error) {
-        setWarningMessage("The Pipeline upload requires a json file");
+        //@ts-ignore
+        setError(error.response.data)
+        cleanUp(event)
       }
     }
-  };
+    if (file && file.type === 'application/json') {
+      try {
+        reader.readAsText(file)
+      } catch (error) {
+        setWarningMessage('The Pipeline upload requires a json file')
+      }
+    }
+  }
 
   const handleUpload = (event: any) => {
-    const file = event.target.files && event.target.files[0];
-    setError("");
-    setWarningMessage("");
-    readFile(file);
-  };
+    const file = event.target.files && event.target.files[0]
+    readFile(file, event)
+    setWarningMessage('')
+    setError({})
+  }
 
   const drawerContent = (
     <Grid hasGutter={true}>
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "0.8rem 1rem",
-
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '0.8rem 1rem',
         }}
       >
-        <Title headingLevel="h2"
+        <Title
+          headingLevel="h2"
           style={{
-            minWidth:"5em",
+            minWidth: '5em',
           }}
         >
           {title}
@@ -168,21 +177,16 @@ const DisplayPage = ({
 
         <div
           style={{
-            display: "flex",
-            gap:"1rem",
+            display: 'flex',
+            gap: '1rem',
           }}
         >
           {showPipelineButton && (
             <div>
-              <Button
-                onClick={showOpenFile}
-              >
-                Upload a Pipeline
-              </Button>
-
+              <Button onClick={showOpenFile}>Upload a Pipeline</Button>
               <input
                 ref={fileOpen}
-                style={{ display: "none" }}
+                style={{ display: 'none' }}
                 type="file"
                 onChange={handleUpload}
               />
@@ -196,13 +200,13 @@ const DisplayPage = ({
             aria-label="search"
             onChange={(value: string) => {
               if (isPlugin) {
-                handlePluginSearch && handlePluginSearch(value);
+                handlePluginSearch && handlePluginSearch(value)
               }
               if (isPipeline) {
-                handlePipelineSearch && handlePipelineSearch(value);
+                handlePipelineSearch && handlePipelineSearch(value)
               }
               if (isCompute) {
-                handleComputeSearch && handleComputeSearch(value);
+                handleComputeSearch && handleComputeSearch(value)
               }
             }}
           />
@@ -231,13 +235,13 @@ const DisplayPage = ({
                   selectedResource.data.id === resource.data.id
                 }
                 onClick={() => {
-                  setSelectedResource(resource);
-                  setIsExpanded(true);
+                  setSelectedResource(resource)
+                  setIsExpanded(true)
                 }}
                 onKeyDown={(event: any) => {
                   if ([13, 32].includes(event.keyCode)) {
-                    setSelectedResource(resource);
-                    setIsExpanded(true);
+                    setSelectedResource(resource)
+                    setIsExpanded(true)
                   }
                 }}
                 className="pluginList"
@@ -274,14 +278,14 @@ const DisplayPage = ({
                 </CardBody>
               </Card>
             </GridItem>
-          );
+          )
         })}
     </Grid>
-  );
+  )
 
   const handleUpdate = (id: number) => {
-    fetch && fetch(id);
-  };
+    fetch && fetch(id)
+  }
 
   const panelContent = (
     <DrawerPanelContent>
@@ -289,7 +293,7 @@ const DisplayPage = ({
         <DrawerActions>
           <DrawerCloseButton
             onClick={() => {
-              setIsExpanded(false);
+              setIsExpanded(false)
             }}
           />
         </DrawerActions>
@@ -301,7 +305,7 @@ const DisplayPage = ({
             </p>
             {isPlugin && (
               <p className="pluginList__version">
-                Public Repo:{" "}
+                Public Repo:{' '}
                 <a
                   target="_blank"
                   rel="noreferrer"
@@ -314,8 +318,8 @@ const DisplayPage = ({
             {deleteError && (
               <div
                 style={{
-                  display: "flex",
-                  flexDirection: "row",
+                  display: 'flex',
+                  flexDirection: 'row',
                 }}
               >
                 <Alert variant="danger" title={deleteError} />
@@ -323,8 +327,8 @@ const DisplayPage = ({
                   onClick={async () => {
                     selectedResource.put({
                       locked: true,
-                    });
-                    setDeleteError("");
+                    })
+                    setDeleteError('')
                   }}
                   variant="link"
                 >
@@ -335,18 +339,17 @@ const DisplayPage = ({
             {showPipelineButton && (
               <Button
                 style={{
-                  width: "fit-content",
-
+                  width: 'fit-content',
                 }}
                 onClick={async () => {
                   if (selectedResource.data.locked === true) {
-                    handleUpdate(selectedResource.data.id);
-                    selectedResource.delete();
-                    setIsExpanded(false);
+                    handleUpdate(selectedResource.data.id)
+                    selectedResource.delete()
+                    setIsExpanded(false)
                   } else {
                     setDeleteError(
-                      "Pipeline cannot be deleted as it is not locked"
-                    );
+                      'Pipeline cannot be deleted as it is not locked',
+                    )
                   }
                 }}
               >
@@ -356,7 +359,7 @@ const DisplayPage = ({
 
             <Divider
               style={{
-                paddingTop: "2em",
+                paddingTop: '2em',
               }}
             />
             <p>{selectedResource.data.description}</p>
@@ -367,7 +370,7 @@ const DisplayPage = ({
         )}
       </DrawerHead>
     </DrawerPanelContent>
-  );
+  )
 
   return (
     <>
@@ -386,7 +389,7 @@ const DisplayPage = ({
         </Drawer>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default DisplayPage;
+export default DisplayPage
