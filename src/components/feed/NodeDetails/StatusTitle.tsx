2240edc21abe12be6d1dcd531ab27c53c0a93857
PintoGideon <gideonpinto123@gmail.com>
2022-08-18 15:06:59 -0400

    Rework Subway Stops

--- src/components/feed/NodeDetails/StatusTitle.tsx
+++ src/components/feed/NodeDetails/StatusTitle.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Skeleton } from '@patternfly/react-core'
+
 import usePluginInstanceResource from './usePluginInstanceResource'
 import { SpinContainer } from '../../common/loading/LoadingContent'
+import { useTypedSelector } from '../../../store/hooks'
+import { AiFillCheckCircle, AiFillExclamationCircle } from 'react-icons/ai'
 
 const StatusTitle = () => {
   const pluginInstanceResource = usePluginInstanceResource()
+  const selected = useTypedSelector((state) => state.instance.selectedPlugin)
 
   let statusTitle:
     | {
@@ -15,8 +18,17 @@ const StatusTitle = () => {
   const pluginStatus =
     pluginInstanceResource && pluginInstanceResource.pluginStatus
 
+  const finishedStatuses = [
+    'finishedSuccessfully',
+    'finishedWithError',
+    'cancelled',
+  ]
+
   if (pluginStatus) {
-    statusTitle = getCurrentTitleFromStatus(pluginStatus)
+    statusTitle =
+      selected && finishedStatuses.includes(selected.data.status) === true
+        ? getFinishedTitle(selected.data.status)
+        : getCurrentTitleFromStatus(pluginStatus)
   }
 
   if (statusTitle) {
@@ -43,4 +55,26 @@ export function getCurrentTitleFromStatus(statusLabels: any[]) {
   })
 
   return { title, icon }
+}
+
+export function getFinishedTitle(pluginStatus: string) {
+  const title =
+    pluginStatus === 'finishedSuccessfully'
+      ? 'Finished Successfully'
+      : pluginStatus === 'cancelled'
+      ? 'Cancelled'
+      : pluginStatus === 'finishedWithError'
+      ? 'FinishedWithError'
+      : ''
+  const icon =
+    pluginStatus === 'finishedSuccessfully'
+      ? AiFillCheckCircle
+      : pluginStatus === 'cancelled' || pluginStatus === 'finishedWithError'
+      ? AiFillExclamationCircle
+      : null
+
+  return {
+    title,
+    icon,
+  }
 }
