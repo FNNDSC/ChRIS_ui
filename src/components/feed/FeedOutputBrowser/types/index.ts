499188a9f83ba7e79c9d10d9aa298c329873058b
PintoGideon <gideonpinto123@gmail.com>
2022-08-22 14:41:03 -0400

    Moving Download files to the Node Details panel

--- src/components/feed/FeedOutputBrowser/types/index.ts
+++ src/components/feed/FeedOutputBrowser/types/index.ts
@@ -1,36 +1,30 @@
-import { FeedFile, PluginInstance } from "@fnndsc/chrisapi";
-import { DataNode } from "../../../../store/explorer/types";
+import { FeedFile, PluginInstance } from '@fnndsc/chrisapi'
+import { DataNode } from '../../../../store/explorer/types'
 
 export interface FileBrowserProps {
   pluginFilesPayload: {
-    files: FeedFile[];
-    folders: string[];
-    error: any;
-    path: string;
-  };
-  handleFileClick: (path: string) => void;
-  selected: PluginInstance;
-  handleFileBrowserToggle: () => void;
-  handleDicomViewerOpen: () => void;
-  handleXtkViewerOpen: () => void;
-  expandDrawer: (panel: string) => void;
-  downloadAllClick: () => void;
-  download: {
-    status: boolean;
-    count: number;
-    path: string;
-  };
-  pluginModalOpen: boolean;
-  filesLoading: boolean;
+    files: FeedFile[]
+    folders: string[]
+    error: any
+    path: string
+  }
+  handleFileClick: (path: string) => void
+  selected: PluginInstance
+  handleFileBrowserToggle: () => void
+  handleDicomViewerOpen: () => void
+  handleXtkViewerOpen: () => void
+  expandDrawer: (panel: string) => void
+  pluginModalOpen: boolean
+  filesLoading: boolean
 }
 
 export interface FileBrowserState {
-  directory: DataNode;
-  breadcrumbs: DataNode[];
-  currentFile: DataNode[];
-  previewingFile?: DataNode; // file selected for preview
+  directory: DataNode
+  breadcrumbs: DataNode[]
+  currentFile: DataNode[]
+  previewingFile?: DataNode // file selected for preview
 }
 
 export interface Label {
-  [key: string]: boolean;
+  [key: string]: boolean
 }
