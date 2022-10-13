f3332f7d93c6e7b890f5362ff6173fbf0b3f3a18
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-10-13 10:19:14 +0100

    restyled the tooltip width and position.

--- src/components/common/info/InfoIcon.tsx
+++ src/components/common/info/InfoIcon.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { FcInfo } from 'react-icons/fc'
-import { Hint } from '@patternfly/react-core'
-import { Popover, Typography } from 'antd'
+import React from "react";
+import { FcInfo } from "react-icons/fc";
+import { Hint } from "@patternfly/react-core";
+import { Popover, Typography } from "antd";
 
-const { Title } = Typography
+const { Title } = Typography;
 
 const InfoIcon = ({
   title,
@@ -12,11 +12,11 @@ const InfoIcon = ({
   p3,
   p4,
 }: {
-  title: string
-  p1?: any
-  p2?: any
-  p3?: any
-  p4?: any
+  title: string;
+  p1?: any;
+  p2?: any;
+  p3?: any;
+  p4?: any;
 }) => {
   const content = (
     <Hint>
@@ -25,24 +25,22 @@ const InfoIcon = ({
       {p3}
       {p4}
     </Hint>
-  )
+  );
 
   return (
-    <div>
-      <Title>
-        {title}
-        <Popover placement="bottomLeft" trigger="hover" content={content}>
-          <FcInfo
-            style={{
-              height: '1em',
-              width: '0.75em',
-              marginLeft: '0.25em',
-            }}
-          />
-        </Popover>
-      </Title>
-    </div>
-  )
-}
+    <Title>
+      {title}
+      <Popover placement="bottom" trigger="hover" content={content}>
+        <FcInfo
+          style={{
+            height: "1em",
+            width: "0.75em",
+            marginLeft: "0.25em",
+          }}
+        />
+      </Popover>
+    </Title>
+  );
+};
 
-export default InfoIcon
+export default InfoIcon;
