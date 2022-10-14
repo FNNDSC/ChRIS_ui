d0f10e5b2a17fb20871f55472be0303ebcc7ee71
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-10-14 03:03:22 +0100

    added prettier config and undid my auto prettier format.

--- src/pages/VisualizationPage/index.tsx
+++ src/pages/VisualizationPage/index.tsx
@@ -1,58 +1,52 @@
-import React, { useRef, useMemo } from "react";
-import { useNavigate } from "react-router";
-import Wrapper from "../Layout/PageWrapper";
-import { Button } from "antd";
-import { AiOutlineUpload } from "react-icons/ai";
-import { useDispatch } from "react-redux";
-import { setSidebarActive } from "../../store/ui/actions";
-import { useDropzone } from "react-dropzone";
-import { setExternalFiles } from "../../store/explorer/actions";
-import { PageSection } from "@patternfly/react-core";
-import InfoIcon from "../../components/common/info/InfoIcon";
-import { Typography } from "antd";
-const { Paragraph } = Typography;
+import React, { useRef, useMemo } from 'react'
+import { useNavigate } from 'react-router'
+import Wrapper from '../Layout/PageWrapper'
+import { Button } from 'antd'
+import { AiOutlineUpload } from 'react-icons/ai'
+import { useDispatch } from 'react-redux'
+import { setSidebarActive } from '../../store/ui/actions'
+import { useDropzone } from 'react-dropzone'
+import { setExternalFiles } from '../../store/explorer/actions'
+import { PageSection } from '@patternfly/react-core'
+import InfoIcon from '../../components/common/info/InfoIcon'
+import { Typography } from 'antd'
+const { Paragraph } = Typography
 
-const infoStyle = { fontSize: "1.15em" };
+const infoStyle = { fontSize: '1.15em' }
 
 const baseStyle: React.CSSProperties = {
   flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "20px",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
   borderWidth: 2,
   borderRadius: 2,
-  borderColor: "#eeeeee",
-  borderStyle: "dashed",
-  backgroundColor: "#fafafa",
-  color: "#bdbdbd",
-  outline: "none",
-  transition: "border .24s ease-in-out",
-};
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+}
 
 const activeStyle = {
-  borderColor: "#2196f3",
-};
+  borderColor: '#2196f3',
+}
 
 const acceptStyle = {
-  borderColor: "#00e676",
-};
+  borderColor: '#00e676',
+}
 
 const rejectStyle = {
-  borderColor: "#ff1744",
-};
+  borderColor: '#ff1744',
+}
 
 const VisualizationPage = () => {
-  const fileOpen = useRef<HTMLInputElement>(null);
-  const folderOpen = useRef<HTMLInputElement>(null);
-  const {
-    acceptedFiles,
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone();
+  const fileOpen = useRef<HTMLInputElement>(null)
+  const folderOpen = useRef<HTMLInputElement>(null)
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } =
+    useDropzone()
 
   const style = useMemo(
     () => ({
@@ -62,57 +56,53 @@ const VisualizationPage = () => {
       ...(isDragReject ? rejectStyle : {}),
     }),
     [isDragActive, isDragReject, isDragAccept]
-  );
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  )
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   React.useEffect(() => {
-    document.title = "DICOM Viewer";
-    if (acceptedFiles.length > 0) dispatch(setExternalFiles(acceptedFiles));
-  }, [acceptedFiles, dispatch]);
+    document.title = 'DICOM Viewer'
+    if (acceptedFiles.length > 0) dispatch(setExternalFiles(acceptedFiles))
+  }, [acceptedFiles, dispatch])
 
   const close = React.useCallback(() => {
-    navigate("/gallery");
-  }, [navigate]);
+    navigate('/gallery')
+  }, [navigate])
 
   React.useEffect(() => {
     dispatch(
       setSidebarActive({
-        activeItem: "visualizations",
+        activeItem: 'visualizations',
       })
-    );
-  }, [dispatch]);
+    )
+  }, [dispatch])
 
   const handleOpenFolder = (files: any) => {
-    dispatch(setExternalFiles(files));
-    close();
-  };
+    dispatch(setExternalFiles(files))
+    close()
+  }
   const handleOpenLocalFs = (files: any) => {
-    dispatch(setExternalFiles(files));
-    close();
-  };
+    dispatch(setExternalFiles(files))
+    close()
+  }
 
   const showOpenFolder = () => {
     if (folderOpen.current) {
-      folderOpen.current.click();
+      folderOpen.current.click()
     }
-  };
+  }
   const showOpenFile = () => {
     if (fileOpen.current) {
-      fileOpen.current.click();
+      fileOpen.current.click()
     }
-  };
+  }
 
   return (
     <Wrapper>
       <PageSection variant="light">
         <InfoIcon
           title="DICOM Viewer"
-          p1={
-            <Paragraph style={infoStyle}>
-              Here you get to upload all your files.
-            </Paragraph>
-          }
+          p1={<Paragraph style={infoStyle}>Here you get to upload all your files.</Paragraph>}
         />
       </PageSection>
 
@@ -128,7 +118,7 @@ const VisualizationPage = () => {
           <input
             type="file"
             id="file_open"
-            style={{ display: "none" }}
+            style={{ display: 'none' }}
             ref={fileOpen}
             multiple
             onChange={(e) => handleOpenLocalFs(e.target.files)}
@@ -136,7 +126,7 @@ const VisualizationPage = () => {
           <input
             type="file"
             id="file_folder"
-            style={{ display: "none" }}
+            style={{ display: 'none' }}
             onChange={(e) => handleOpenFolder(e.target.files)}
             multiple
             //@ts-ignore
@@ -147,15 +137,13 @@ const VisualizationPage = () => {
           <section className="container">
             <div {...getRootProps({ style })}>
               <input {...getInputProps()} />
-              <p>
-                Drag &apos;n&apos; drop some files here or click to select files
-              </p>
+              <p>Drag &apos;n&apos; drop some files here or click to select files</p>
             </div>
           </section>
         </div>
       </PageSection>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default VisualizationPage;
+export default VisualizationPage
