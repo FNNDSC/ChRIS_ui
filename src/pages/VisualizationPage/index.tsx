f9d70014e18365051834f5ec388bc18b1f874b3b
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-10-14 05:10:36 +0100

    pulled from master.

--- src/pages/VisualizationPage/index.tsx
+++ src/pages/VisualizationPage/index.tsx
@@ -1,18 +1,18 @@
-import React, { useRef, useMemo } from "react"
-import { useNavigate } from "react-router"
-import Wrapper from "../Layout/PageWrapper"
-import { Button } from "antd"
-import { AiOutlineUpload } from "react-icons/ai"
-import { useDispatch } from "react-redux"
-import { setSidebarActive } from "../../store/ui/actions"
-import { useDropzone } from "react-dropzone"
-import { setExternalFiles } from "../../store/explorer/actions"
-import { PageSection } from "@patternfly/react-core"
-import InfoIcon from "../../components/common/info/InfoIcon"
-import { Typography } from "antd"
-const { Paragraph } = Typography
+import React, { useRef, useMemo } from "react";
+import { useNavigate } from "react-router";
+import Wrapper from "../Layout/PageWrapper";
+import { Button } from "antd";
+import { AiOutlineUpload } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { setSidebarActive } from "../../store/ui/actions";
+import { useDropzone } from "react-dropzone";
+import { setExternalFiles } from "../../store/explorer/actions";
+import { PageSection } from "@patternfly/react-core";
+import InfoIcon from "../../components/common/info/InfoIcon";
+import { Typography } from "antd";
+const { Paragraph } = Typography;
 
-const infoStyle = { fontSize: "1.15em" }
+const infoStyle = { fontSize: "1.15em" };
 
 const btnStyle = { marginRight: "10px" };
 
@@ -30,25 +30,31 @@ const baseStyle: React.CSSProperties = {
   color: "#bdbdbd",
   outline: "none",
   transition: "border .24s ease-in-out",
-}
+};
 
 const activeStyle = {
   borderColor: "#2196f3",
-}
+};
 
 const acceptStyle = {
   borderColor: "#00e676",
-}
+};
 
 const rejectStyle = {
   borderColor: "#ff1744",
-}
+};
 
 const VisualizationPage = () => {
-  const fileOpen = useRef<HTMLInputElement>(null)
-  const folderOpen = useRef<HTMLInputElement>(null)
-  const { acceptedFiles, getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } =
-    useDropzone()
+  const fileOpen = useRef<HTMLInputElement>(null);
+  const folderOpen = useRef<HTMLInputElement>(null);
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone();
 
   const style = useMemo(
     () => ({
@@ -58,46 +64,46 @@ const VisualizationPage = () => {
       ...(isDragReject ? rejectStyle : {}),
     }),
     [isDragActive, isDragReject, isDragAccept]
-  )
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   React.useEffect(() => {
-    document.title = "DICOM Viewer"
-    if (acceptedFiles.length > 0) dispatch(setExternalFiles(acceptedFiles))
-  }, [acceptedFiles, dispatch])
+    document.title = "DICOM Viewer";
+    if (acceptedFiles.length > 0) dispatch(setExternalFiles(acceptedFiles));
+  }, [acceptedFiles, dispatch]);
 
   const close = React.useCallback(() => {
-    navigate("/gallery")
-  }, [navigate])
+    navigate("/gallery");
+  }, [navigate]);
 
   React.useEffect(() => {
     dispatch(
       setSidebarActive({
         activeItem: "visualizations",
       })
-    )
-  }, [dispatch])
+    );
+  }, [dispatch]);
 
   const handleOpenFolder = (files: any) => {
-    dispatch(setExternalFiles(files))
-    close()
-  }
+    dispatch(setExternalFiles(files));
+    close();
+  };
   const handleOpenLocalFs = (files: any) => {
-    dispatch(setExternalFiles(files))
-    close()
-  }
+    dispatch(setExternalFiles(files));
+    close();
+  };
 
   const showOpenFolder = () => {
     if (folderOpen.current) {
-      folderOpen.current.click()
+      folderOpen.current.click();
     }
-  }
+  };
   const showOpenFile = () => {
     if (fileOpen.current) {
-      fileOpen.current.click()
+      fileOpen.current.click();
     }
-  }
+  };
 
   return (
     <Wrapper>
@@ -106,8 +112,9 @@ const VisualizationPage = () => {
           title="DICOM Viewer"
           p1={
             <Paragraph style={infoStyle}>
-              The DICOM Viewer is a convenient way to visualize DICOM images as a stack in your web
-              browser without having to install any programs.
+              The DICOM Viewer is a convenient way to visualize DICOM images as
+              a stack in your web browser without having to install any
+              programs.
             </Paragraph>
           }
         />
@@ -147,13 +154,15 @@ const VisualizationPage = () => {
           <section className="container">
             <div {...getRootProps({ style })}>
               <input {...getInputProps()} />
-              <p>Drag &apos;n&apos; drop some files here or click to select files</p>
+              <p>
+                Drag &apos;n&apos; drop some files here or click to select files
+              </p>
             </div>
           </section>
         </div>
       </PageSection>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default VisualizationPage
+export default VisualizationPage;
