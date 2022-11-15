45c5621070ee1d0b3168d735eb1908604c74f77d
PintoGideon <gideonpinto123@gmail.com>
2022-11-14 22:12:48 -0500

    Bug fix for multiple renders

--- src/components/feed/CreateFeed/utils/pipelines.ts
+++ src/components/feed/CreateFeed/utils/pipelines.ts
@@ -96,13 +96,13 @@ export function intToRGB(i: number) {
 
 export const stringToColour = (
   value: string,
+  log: string,
   saturation = 100,
   lightness = 75
 ) => {
   let hash = 0;
   for (let i = 0; i < value.length; i++) {
     hash = value.charCodeAt(i) + ((hash << 5) - hash);
-    hash = hash && hash;
   }
   return `hsl(${hash % 360}, ${saturation}%, ${lightness}%)`;
 };
