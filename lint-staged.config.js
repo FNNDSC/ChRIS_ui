759d1541d3e558a0b5c50e77250009860c6a0691
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-11-17 11:09:33 +0100

    removed format script since format:fix does what it does.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,ts,tsx}': ['eslint --fix', () => 'tsc-files --noEmit'],
-  '*.{js,ts,tsx,json,css,jsx}': ['npm run format'],
+  '*.{js,ts,tsx,json,css,jsx}': ['npm run format:fix'],
   '*.js': 'eslint --cache --fix',
 }
