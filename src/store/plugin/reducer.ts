5551a4a2f07ee3ee7aaddd51de99c3dd8b175cd6
PintoGideon <gideonpinto123@gmail.com>
2023-01-12 13:13:41 -0500

    Fix Bug Releated to Keyboard Shortcuts

--- src/store/plugin/reducer.ts
+++ src/store/plugin/reducer.ts
@@ -6,10 +6,27 @@ import { PluginActionTypes, IPluginState } from "./types";
 const initialState: IPluginState = {
   parameters: [],
   computeEnv: undefined,
+  nodeOperations: {
+    terminal: false,
+    childNode: false,
+    childPipeline: false,
+    childGraph: false,
+    deleteNode: false,
+  },
 };
 
 const reducer: Reducer<IPluginState> = (state = initialState, action) => {
   switch (action.type) {
+    case PluginActionTypes.GET_NODE_OPERATIONS: {
+      return {
+        ...state,
+        nodeOperations: {
+          ...state.nodeOperations,
+          [action.payload]: !state.nodeOperations[action.payload],
+        },
+      };
+    }
+
     case PluginActionTypes.GET_PARAMS_SUCCESS: {
       const params = action.payload;
       return {
