e39440652c24453ce5c370191374eae3ee083587
David Obodo <obododavidsop@gmail.com>
2023-01-17 13:56:15 +0100

    Console logs

--- src/pages/SinglePluginPage/SinglePlugin.tsx
+++ src/pages/SinglePluginPage/SinglePlugin.tsx
@@ -47,6 +47,11 @@ const SinglePlugin = () => {
     async function fetchPlugins(id: number) {
       const client = ChrisAPIClient.getClient();
       const pluginMeta = await client.getPluginMeta(id);
+      const pluginMetas = await client.getPluginMetas();
+      const pluginMets = pluginMetas.getItems()
+      console.log(pluginMets);
+      console.log(pluginMetas)
+      
       document.title = pluginMeta.data.name;
       setCurrentPluginMeta(pluginMeta);
       const fn = pluginMeta.getPlugins;
