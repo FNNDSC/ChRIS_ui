73941f5a29558599a469e2a048cfe655436c9ef2
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-01-31 13:45:55 +0200

    added keyboard shortcut on the local file upload proccess

--- src/components/common/fileupload/index.tsx
+++ src/components/common/fileupload/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import { Tooltip } from "@patternfly/react-core";
+import React, { useCallback, useContext, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
 import { MdOutlineUploadFile } from "react-icons/md";
 import { CreateFeedContext } from "../../feed/CreateFeed/context";
@@ -8,7 +9,7 @@ const baseStyle: React.CSSProperties = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  justifyContent:"center",
+  justifyContent: "center",
   padding: "20px",
   borderWidth: 2,
   borderRadius: 2,
@@ -55,7 +56,7 @@ const DragAndUpload = ({
   const { state } = useContext(CreateFeedContext);
 
   React.useEffect(() => {
-    if (acceptedFiles.length > 0 ) {
+    if (acceptedFiles.length > 0) {
       handleLocalUploadFiles(acceptedFiles);
     }
   }, [acceptedFiles, handleLocalUploadFiles, state.data.localFiles.length]);
@@ -64,7 +65,23 @@ const DragAndUpload = ({
     if (state.data.localFiles.length == 0) {
       open()
     }
-  }, [ open, state.data.localFiles.length])
+  }, [open, state.data.localFiles.length])
+
+  const handleKeyDown = useCallback((e: any) => {
+    console.log(e)
+    if (e.key == "u") {
+      console.log("called")
+      open()
+    }
+  }, [open])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
   const style = React.useMemo(
     () => ({
       ...baseStyle,
@@ -77,10 +94,15 @@ const DragAndUpload = ({
     [isDragActive, isDragReject, isDragAccept, isFocused]
   );
   return (
-    <section className="container" style={{height:"100%"}}>
+    <section className="container" style={{ height: "100%", position: "relative" }}>
+      <Tooltip content="Press the U key to select a file">
+      <div className="pf-c-chip pf-m-read-only tag"  style={{ position: "absolute", top: "10%", right: "8%" }}>
+        <span className="pf-c-chip__text">U</span>
+      </div>
+      </Tooltip>
       <div {...getRootProps({ style })} >
         <input {...getInputProps()} />
-        <MdOutlineUploadFile size="40"/>
+        <MdOutlineUploadFile size="40" />
         <p>Drag &apos;n&apos; drop some files here or click to select files</p>
       </div>
     </section>
