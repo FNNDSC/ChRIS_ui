9eebac95c53963058b69d9a3df3d781329b39b1c
PintoGideon <gideonpinto123@gmail.com>
2023-04-20 15:00:04 -0400

    Render joins in the pipeline graph

--- src/components/feed/FeedTree/data.ts
+++ src/components/feed/FeedTree/data.ts
@@ -1,4 +1,8 @@
-import { PluginInstance, PluginParameter } from "@fnndsc/chrisapi";
+import {
+  PluginInstance,
+  PluginParameter,
+  PluginPiping,
+} from "@fnndsc/chrisapi";
 import { fetchResource } from "../../../api/common";
 
 export interface Datum {
@@ -90,6 +94,34 @@ export const getTsNodes = async (items: PluginInstance[]) => {
   return parentIds;
 };
 
+export const getTsNodesWithPipings = async (
+  items: PluginPiping[],
+  pluginParameters?: any[]
+) => {
+  const parentIds: {
+    [key: string]: number[];
+  } = {};
+
+  for (let i = 0; i < items.length; i++) {
+    const instance = items[i];
+
+    if (instance.data.plugin_name === "pl-topologicalcopy") {
+      //@ts-ignore
+      pluginParameters.data
+        .filter((param: any) => {
+          return param.plugin_piping_id === instance.data.id;
+        })
+        .forEach((param: any) => {
+          if (param.param_name === "plugininstances") {
+            parentIds[param.plugin_piping_id] = param.value
+              .split(",")
+              .map(Number);
+          }
+        });
+    }
+  }
+  return parentIds;
+};
 export function treeAlgorithm(
   event: any,
   selectedD3Node: any,
