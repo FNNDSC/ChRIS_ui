efaf51455ac6eef3ceb4d576a5dcac37c558cfc7
PintoGideon <gideonpinto123@gmail.com>
2023-04-20 13:47:58 -0400

    Remove app folder

--- app/src/api/models/file-explorer.model.ts
+++
@@ -1,6 +0,0 @@
-// Description: get file type by file extention
-export function getFileExtension(filename: string) {
-  const name = filename.substring(filename.lastIndexOf('.') + 1)
-
-  return name
-}
