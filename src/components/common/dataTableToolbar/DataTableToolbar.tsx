c0d448a2530b037f90508633d74e27163d4b4dc1
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-05-16 19:01:36 +0200

    added filter option for feeds

--- src/components/common/dataTableToolbar/DataTableToolbar.tsx
+++ src/components/common/dataTableToolbar/DataTableToolbar.tsx
@@ -1,42 +1,94 @@
 import React, { useState } from "react";
-import { InputGroup, InputGroupText, TextInput } from "@patternfly/react-core";
-import { AiOutlineSearch } from "react-icons/ai";
+import {TextInput, DropdownToggle, Dropdown, DropdownItem } from "@patternfly/react-core";
 import "./dataTableToolbar.scss";
 
 type AllProps = {
   label: string;
-  onSearch: (term: string) => void;
+  onSearch: (search: string, searchType:string) => void;
 };
+
+export enum FeedsQueryTypes{
+  ID="Id",
+  MIN_ID="Min_Id",
+  MAX_ID="Max_Id",
+  NAME="Name",
+  NAME_EXACT="Name_Exact",
+  NAME_STARTSWITH="Name_Startswith",
+  FILES_FNAME_ICONTAINS="Files_Fname_Icontains",
+  MIN_CREATION_DATE="Min_Creation_Date",
+  MAX_CREATION_DATE="Max_Creation_Date"
+
+}
 
 const DataTableToolbar: React.FunctionComponent<AllProps> = (
   props: AllProps
 ) => {
   const [value, setValue] = useState("");
+  const [dropdownValue, setDropdownValue] = React.useState<string>( FeedsQueryTypes.NAME)
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
 
-  const handleTextInputChange = (value: string) => {
-    // Note: Use this block to filter data in table ***** working
-    setValue(value);
-    props.onSearch(value);
+  const onToggle = (isDropdownOpen: boolean) => {
+    setIsDropdownOpen(isDropdownOpen);
   };
-  const { label } = props;
-  return (
+  const onFocus = () => {
+    const element = document.getElementById('toggle-basic');
+    element?.focus();
+  };
+
+  const onSelect = () => {
+    setIsDropdownOpen(false);
+    onFocus();
+  };
+
+  const updateDropdownValue =(type:string)=>{
+    setDropdownValue(type)
+    setValue("")
+    props.onSearch("", dropdownValue.toLowerCase())
+   }
+
+  const dropdownItems = Object.values(FeedsQueryTypes).map((feed) => {
+    return<DropdownItem key={feed} component="button" onClick={() => updateDropdownValue(feed)}>
+    {feed}
+   </DropdownItem>
+  })
+   return (
     <div className="datatable-toolbar">
-      <InputGroup>
-        <InputGroupText id="brainStructureLabel" className="toolbar-label">
-          <AiOutlineSearch
-            style={{
-              marginRight: "1em",
+      <div
+        style={
+          { display: "flex", justifyContent: "space-between", padding: "0.8rem 0rem" }}
+      >
+       <div style={{ display: "flex", flexDirection: "row" }}>
+          <Dropdown
+            onSelect={onSelect}
+            toggle={
+              <DropdownToggle id="toggle-basic" onToggle={onToggle}>
+                <div style={{ textAlign: "left", padding: "0 0.5em" }}>
+                  <div style={{ fontSize: "smaller", color: "gray" }}>
+                   Search Analysis By
+                  </div>
+                  <div style={{ fontWeight: 600 }}>
+                    {dropdownValue}
+                  </div>
+                </div>
+              </DropdownToggle>
+            }
+            isOpen={isDropdownOpen}
+            dropdownItems={dropdownItems}
+          />
+          <TextInput
+            value={value}
+            type="text"
+            style={{height:"100%"}}
+            placeholder={(dropdownValue)}
+            iconVariant="search"
+            aria-label="search"
+            onChange={(value: string) => {
+              setValue(value);
+              props.onSearch(value, dropdownValue.toLowerCase());
             }}
           />
-          {label}
-        </InputGroupText>
-        <TextInput
-          type="text"
-          aria-label="text input field"
-          value={value}
-          onChange={handleTextInputChange}
-        />
-      </InputGroup>
+        </div>
+    </div>
     </div>
   );
 };
