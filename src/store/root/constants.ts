eeffcdedc3750396bd59fb62a8de150d660ae3c8
PintoGideon <gideonpinto123@gmail.com>
2023-11-10 10:25:19 -0500

    rebase successful

---
+++ src/store/root/constants.ts
@@ -0,0 +1,12 @@
+/*
+*   File:           constants.ts
+*   Description:    This is where the constant files to hold all global action types
+*   Author:         ChRIS UI
+*/
+
+import keyMirror from "keymirror";
+
+export const ActionTypes = keyMirror({
+    INIT_STORE: null
+});
+
