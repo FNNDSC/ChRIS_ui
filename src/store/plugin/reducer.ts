1d6e54e650e9b72afa792209a149d9f821d0da64
PintoGideon <gideonpinto123@gmail.com>
2023-11-10 10:25:19 -0500

    rebase successful

---
+++ src/store/plugin/reducer.ts
@@ -0,0 +1,56 @@
+import { Reducer } from "redux";
+
+import { PluginActionTypes, IPluginState } from "./types";
+
+// Type-safe initialState
+const initialState: IPluginState = {
+  parameters: {
+    dropdown: [],
+    required: [],
+  },
+  computeEnv: undefined,
+  nodeOperations: {
+    terminal: false,
+    childNode: false,
+    childPipeline: false,
+    childGraph: false,
+    deleteNode: false,
+  },
+};
+
+const reducer: Reducer<IPluginState> = (state = initialState, action) => {
+  switch (action.type) {
+    case PluginActionTypes.GET_NODE_OPERATIONS: {
+      return {
+        ...state,
+        nodeOperations: {
+          ...state.nodeOperations,
+          [action.payload]: !state.nodeOperations[action.payload],
+        },
+      };
+    }
+
+    case PluginActionTypes.GET_PARAMS_SUCCESS: {
+      return {
+        ...state,
+        parameters: {
+          required: action.payload.required,
+          dropdown: action.payload.dropdown,
+        },
+      };
+    }
+
+    case PluginActionTypes.GET_COMPUTE_ENV_SUCCESS: {
+      return {
+        ...state,
+        computeEnv: action.payload,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export { reducer as pluginReducer };
