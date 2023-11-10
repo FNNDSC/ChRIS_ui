9238a88c5b12f926d9855a84ae78e819613d50e1
PintoGideon <gideonpinto123@gmail.com>
2023-11-10 08:23:21 -0500

    Fixing merge commits

---
+++ src/store/hooks.ts
@@ -0,0 +1,3 @@
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { RootState } from "./root/applicationState";
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
