c556b4198616094c3275b197a79a7dcb86674501
PintoGideon <gideonpinto123@gmail.com>
2023-11-21 16:15:14 -0500

    Updates to the Library Page

--- src/components/LibraryCopy/index.tsx
+++ src/components/LibraryCopy/index.tsx
@@ -1,5 +1,7 @@
-import { useContext, useState, useMemo } from "react";
+import { useContext, useState, useCallback, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
+
+import ReactJson from "react-json-view";
 import {
   AlertGroup,
   Button,
@@ -10,20 +12,43 @@ import {
   DropdownList,
   DropdownItem,
   MenuToggle,
+  PageSection,
+  Modal,
+  ModalVariant,
+  CodeBlockCode,
+  CodeBlock,
+  FormGroup,
+  Form,
+  Alert as PatternflyAlert,
+  Progress,
 } from "@patternfly/react-core";
+import { Typography } from "antd";
 import { debounce } from "lodash";
 import { Alert } from "antd";
 import { useLocation, useNavigate } from "react-router";
 import ChrisAPIClient from "../../api/chrisapiclient";
 import WrapperConnect from "../Wrapper";
+import DragAndUpload from "../DragFileUpload";
 import Browser from "./Browser";
+import { LocalFileList } from "../CreateFeed/HelperComponent";
 import { SpinContainer } from "../Common";
 import BreadcrumbContainer from "./BreadcrumbContainer";
 import { LibraryContext, LibraryProvider } from "./context/";
 import { clearCart, clearSelectFolder } from "./context/actions";
 import { MainRouterContext } from "../../routes";
-import { fetchResource } from "../../api/common";
-import Client from "@fnndsc/chrisapi";
+import { InfoIcon } from "../Common";
+import FaUpload from "@patternfly/react-icons/dist/esm/icons/upload-icon";
+import type { AxiosResponse } from "axios";
+import { useCookieToken } from "../Common";
+import { useTypedSelector } from "../../store/hooks";
+import {
+  catchError,
+  limitConcurrency,
+  getTimestamp,
+  uploadWrapper,
+} from "../../api/common";
+
+const { Paragraph } = Typography;
 
 export const fetchFilesUnderThisPath = async (path?: string) => {
   if (!path) return;
@@ -86,15 +111,86 @@ const useGetFiles = (computedPath: string) => {
 };
 
 export const LibraryCopyPage = () => {
+  const navigate = useNavigate();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [localFiles, setLocalFiles] = useState<File[]>([]);
+
+  const handleLocalFiles = (files: File[]) => {
+    setLocalFiles(files);
+  };
+
+  const handleFileModal = () => {
+    setIsOpenModal(!isOpenModal);
+  };
+
+  const handleAddFolder = (path: string, user: string) => {
+    navigate(`/librarycopy/${user}/uploads/${path}`);
+  };
+
   return (
     <WrapperConnect>
       <>
-        <LibraryProvider>
-          <Cart />
-          <LocalSearch />
+        <PageSection
+          style={{
+            paddingBottom: "0",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <InfoIcon
+            title="Library"
+            p1={
+              <Paragraph>
+                The Library provides a card-focused mechanism for browsing,
+                viewing, and interacting with data in the ChRIS system. A card
+                is analogous to a file or folder in a convention filesystem, and
+                multiple cards can be grouped into a shopping cart to allow for
+                bulk operations. Simply long press and release a card to add it
+                to the cart. Bulk operations include: <b>Download</b> (which
+                will copy all cart contents to your local filesystem),{" "}
+                <b>Delete</b> (which will permanently remove all data in the
+                cards from ChRIS), and <b>Create</b> which will seed a new
+                analysis with a new root node containing each card as a
+                subdirectory.
+              </Paragraph>
+            }
+          />
+          <div style={{ marginRight: "1rem" }}>
+            <Button
+              variant="primary"
+              onClick={handleFileModal}
+              icon={<FaUpload />}
+            >
+              Upload Files
+            </Button>
+          </div>
+        </PageSection>
 
-          <NormalBrowser />
-        </LibraryProvider>
+        <PageSection
+          style={{
+            paddingTop: "0",
+          }}
+        >
+          <LibraryProvider>
+            <UploadComponent
+              handleFileModal={handleFileModal}
+              handleLocalFiles={handleLocalFiles}
+              uploadFileModal={isOpenModal}
+              handleAddFolder={handleAddFolder}
+              localFiles={localFiles}
+              handleDelete={(name: string) => {
+                const filteredfiles = localFiles.filter(
+                  (file) => file.name !== name,
+                );
+                setLocalFiles(filteredfiles);
+              }}
+            />
+            <Cart />
+            <LocalSearch />
+
+            <NormalBrowser />
+          </LibraryProvider>
+        </PageSection>
       </>
     </WrapperConnect>
   );
@@ -152,7 +248,6 @@ function Cart() {
 
   const createFeed = () => {
     const pathList = selectedPaths.map((path) => path);
-
     router.actions.createFeedWithData(pathList);
   };
 
@@ -306,3 +401,242 @@ function LocalSearch() {
     </div>
   );
 }
+
+interface UploadComponent {
+  handleFileModal: () => void;
+  handleLocalFiles: (files: File[]) => void;
+  handleDelete: (name: string) => void;
+  uploadFileModal: boolean;
+  localFiles: File[];
+  handleAddFolder: (path: string, user: string) => void;
+}
+
+interface FileUpload {
+  file: File;
+  promise: Promise<AxiosResponse<any>>;
+}
+
+const UploadComponent = ({
+  handleFileModal,
+  handleLocalFiles,
+  handleAddFolder,
+  handleDelete,
+  uploadFileModal,
+  localFiles,
+}: UploadComponent) => {
+  const token = useCookieToken();
+  const username = useTypedSelector((state) => state.user.username);
+  const [warning, setWarning] = useState<Record<string, string>>({});
+  const [directoryName, setDirectoryName] = useState("");
+  const [countdown, setCountdown] = useState(5);
+  const [currentFile, setCurrentFile] = useState({});
+  const [countdownInterval, setCountdownInterval] =
+    useState<NodeJS.Timeout | null>(null);
+  const [serverProgress, setServerProgress] = useState(0);
+
+  const handleLocalUploadFiles = (files: any[]) => {
+    setWarning({});
+    handleLocalFiles(files);
+  };
+
+  const handleReset = useCallback(() => {
+    setCurrentFile({});
+    setCountdown(5);
+    setServerProgress(0);
+    handleFileModal();
+    countdownInterval && clearInterval(countdownInterval);
+  }, [countdownInterval, handleFileModal]);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      handleReset();
+    }
+  }, [handleReset, countdown]);
+
+  useEffect(() => {
+    const d = getTimestamp();
+    setDirectoryName(`${d}`);
+  }, [uploadFileModal]);
+
+  const handleUpload = async () => {
+    const client = ChrisAPIClient.getClient();
+    await client.setUrls();
+    const onUploadProgress = (file: any, progressEvent: ProgressEvent) => {
+      const percentCompleted = `${Math.round(
+        (progressEvent.loaded * 100) / progressEvent.total,
+      )}%`;
+      setCurrentFile((prevProgresses) => ({
+        ...prevProgresses,
+        [file.name]: percentCompleted,
+      }));
+    };
+
+    const uploadDirectory = `${username}/uploads/${directoryName}`;
+
+    const fileUploads: FileUpload[] = uploadWrapper(
+      localFiles,
+      client,
+      uploadDirectory,
+      token,
+      onUploadProgress,
+    );
+
+    const completedUploads: number[] = [];
+    const promises = fileUploads.map(
+      ({ promise }) =>
+        () =>
+          promise,
+    );
+    let serverProgressForClosingModal = 0;
+    const results = await limitConcurrency(4, promises, (progress: number) => {
+      setServerProgress(progress);
+      serverProgressForClosingModal = progress;
+    });
+    results.forEach((result, i) => {
+      if (result.status === 201) {
+        completedUploads.push(i);
+      } else {
+        const err = catchError(result);
+        const { file } = fileUploads[i];
+        setWarning({
+          ...warning,
+          [file.name]: err,
+        });
+      }
+    });
+
+    if (
+      completedUploads.length === localFiles.length &&
+      serverProgressForClosingModal === 100
+    ) {
+      handleAddFolder(directoryName, username);
+      const intervalDelay = 2000;
+      const interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, intervalDelay);
+      setCountdownInterval(interval);
+    }
+  };
+
+  return (
+    <Modal
+      title="Upload Files"
+      onClose={() => {
+        handleReset();
+      }}
+      isOpen={uploadFileModal}
+      variant={ModalVariant.large}
+      arial-labelledby="file-upload"
+    >
+      <div style={{ height: "200px" }}>
+        <DragAndUpload handleLocalUploadFiles={handleLocalUploadFiles} />
+      </div>
+      <div style={{ height: "200px", marginTop: "1rem", overflow: "scroll" }}>
+        {localFiles.length > 0 ? (
+          localFiles.map((file, index) => {
+            return (
+              <LocalFileList
+                key={index}
+                handleDeleteDispatch={(name) => {
+                  handleDelete(name);
+                }}
+                file={file}
+                index={index}
+                showIcon={true}
+              />
+            );
+          })
+        ) : (
+          <div>No files Upload</div>
+        )}
+      </div>
+
+      <Form
+        onSubmit={(event) => event.preventDefault()}
+        style={{ marginTop: "1rem" }}
+        isHorizontal
+      >
+        <FormGroup fieldId="directory name" isRequired label="Directory Name">
+          <TextInput
+            isRequired
+            id="horizontal form name"
+            value={directoryName}
+            type="text"
+            name="horizontal-form-name"
+            onChange={(_event, value) => {
+              setDirectoryName(value);
+            }}
+          />
+        </FormGroup>
+      </Form>
+      <div style={{ marginTop: "1.5rem", textAlign: "center" }}>
+        <Button
+          isDisabled={
+            localFiles.length === 0 ||
+            (countdown < 5 && countdown > 0) ||
+            directoryName.length === 0 ||
+            serverProgress > 0
+          }
+          onClick={handleUpload}
+          icon={<FaUpload />}
+          variant="secondary"
+        >
+          Push to File Storage
+        </Button>
+      </div>
+      <CodeBlock
+        style={{ marginTop: "1rem", height: "250px", overflow: "scroll" }}
+      >
+        <CodeBlockCode>
+          {Object.keys(currentFile).length === 0 ? (
+            <span style={{ fontFamily: "monospace" }}>
+              You have no active uploads. Please upload Files from your local
+              computer and hit the &apos;Push to File Storage&apos; button. You
+              can give a directory name for your upload or use the default name
+              above. Your uploads will appear under the &apos;Uploads&apos;
+              space once it is complete.
+            </span>
+          ) : (
+            <ReactJSONView currentFile={currentFile} />
+          )}
+        </CodeBlockCode>
+        <CodeBlockCode>
+          {Object.keys(warning).length > 0 && (
+            <ReactJSONView currentFile={warning} />
+          )}
+        </CodeBlockCode>
+      </CodeBlock>
+      <Progress
+        style={{ marginTop: "1rem" }}
+        value={serverProgress}
+        title={`${serverProgress}% Complete`}
+        measureLocation="outside"
+      />
+      {countdown < 5 && countdown > 0 && (
+        <PatternflyAlert variant="success" title="Successful Upload">
+          The files have been uploaded to the server. This modal will close in{" "}
+          {countdown} seconds.
+        </PatternflyAlert>
+      )}
+    </Modal>
+  );
+};
+
+export const ReactJSONView = ({
+  currentFile,
+}: {
+  currentFile: Record<string, string>;
+}) => {
+  return (
+    <ReactJson
+      style={{ height: "100%" }}
+      displayDataTypes={false}
+      theme="grayscale"
+      src={currentFile}
+      name={null}
+      enableClipboard={false} // Set enableClipboard prop to false
+      displayObjectSize={false} // Set displayObjectSize prop to false
+      collapsed={4}
+    />
+  );
+};
--- src/components/LibrarySearch/index.tsx
+++ src/components/LibrarySearch/index.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { Typography } from "antd";
 import { useNavigate } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router";
@@ -7,14 +8,21 @@ import { LibraryProvider } from "../LibraryCopy/context";
 import ChrisAPIClient from "../../api/chrisapiclient";
 import { fetchResource } from "../../api/common";
 import Browser from "../LibraryCopy/Browser";
-import { Breadcrumb, BreadcrumbItem } from "@patternfly/react-core";
+import { InfoIcon } from "../Common";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	PageSection,
+} from "@patternfly/react-core";
+
+const { Paragraph, Text } = Typography;
 
 const useSearchQuery = (query: URLSearchParams) => {
 	const search = query.get("search");
 	const searchSpace = query.get("space");
 
-	let resultsArray = [];
-
+	let resultsArray: any[] = [];
+	const client = ChrisAPIClient.getClient();
 	if (searchSpace === "feeds") {
 		const payload = {
 			limit: 10,
@@ -23,7 +31,6 @@ const useSearchQuery = (query: URLSearchParams) => {
 		};
 
 		const fetchResourceforSearch = async (payload) => {
-			const client = ChrisAPIClient.getClient();
 			const fn = client.getFeeds;
 			const boundFn = fn.bind(client);
 			return await fetchResource(payload, boundFn);
@@ -34,11 +41,42 @@ const useSearchQuery = (query: URLSearchParams) => {
 			queryFn: () => fetchResourceforSearch(payload),
 		});
 
-		if (searchData.data?.resource.length > 0) {
-			searchData.data?.resource.forEach((result) => {
+		if (searchData.data?.resource?.length > 0) {
+			searchData.data?.resource.forEach((result: any) => {
 				resultsArray.push({
 					folder_path: result.data.creator_username,
 					folder_name: `feed_${result.data.id}`,
+				});
+			});
+		}
+	}
+
+	if (searchSpace === "pacs") {
+		const payload = {
+			limit: 10,
+			offset: 0,
+			fname_icontains_topdir_unique: search,
+		};
+
+		const fetchResourceForSearch = async (payload) => {
+			const fn = client.getPACSFiles;
+			const boundFn = fn.bind(client);
+			return await fetchResource(payload, boundFn);
+		};
+
+		const searchData = useQuery({
+			queryKey: ["search", payload],
+			queryFn: () => fetchResourceForSearch(payload),
+		});
+
+		if (searchData.data?.resource?.length > 0) {
+			searchData.data?.resource.forEach((result: any) => {
+				const folderSplit = result.data.fname.split("/");
+				const folder_path = folderSplit.slice(0, 3).join("/");
+
+				resultsArray.push({
+					folder_path: folder_path,
+					folder_name: folderSplit[3],
 				});
 			});
 		}
@@ -60,13 +98,37 @@ export default function LibrarySearch() {
 
 	return (
 		<WrapperConnect>
-			<LibraryProvider>
-				{searchFolderData &&
-					searchFolderData.length > 0 &&
-					searchFolderData.map((data, index) => {
-						return <SearchBrowser key={index} data={data} />;
-					})}
-			</LibraryProvider>
+			<PageSection>
+				<InfoIcon
+					title="Library Search Results"
+					p2={
+						<Paragraph>
+							In addition to browsing over the entire file space,
+							the Library also allows for powerful searching
+							across the three main parts of the ChRIS Library.
+							The <Text strong>Uploads </Text>
+							shows all the data (organized by folder) that has
+							been uploaded from some external source (typically a
+							filesystem). The{" "}
+							<Text strong>Completed Analyses</Text> allows easy
+							navigation down all the Analyses that have been
+							completed in ChRIS. The <Text strong>SERVICES</Text>{" "}
+							allows easy browsing of data associated with some
+							external service, such as a Picture Archive and
+							Communications System (PACS) database.
+						</Paragraph>
+					}
+				/>
+			</PageSection>
+			<PageSection>
+				<LibraryProvider>
+					{searchFolderData &&
+						searchFolderData.length > 0 &&
+						searchFolderData.map((data, index) => {
+							return <SearchBrowser key={index} data={data} />;
+						})}
+				</LibraryProvider>
+			</PageSection>
 		</WrapperConnect>
 	);
 }
