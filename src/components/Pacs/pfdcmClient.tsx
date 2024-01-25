bbb9df40b84f90f887181cbdbbeab434925d88db
PintoGideon <gideonpinto123@gmail.com>
2024-01-25 11:13:27 -0500

    Remove unused variables

--- src/components/Pacs/pfdcmClient.tsx
+++ src/components/Pacs/pfdcmClient.tsx
@@ -272,7 +272,7 @@ class PfdcmClient {
       if (!imagestatus.request && retrieve) {
         newImageStatus[0].icon = <Spin />;
         newImageStatus[0].description = requestedFiles
-          ? `Requesting ${requestedFiles} ${requestedFiles.length === 1 ? ' file': ' files'}`
+          ? `Requesting ${requestedFiles} ${requestedFiles === 1 ? ' file': ' files'}`
           : `Requesting files`;
       }
 
