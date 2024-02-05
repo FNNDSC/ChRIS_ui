6ec7528049afa66130f0e747fe0f750dab9354dc
Jennings Zhang <jenni_zh@protonmail.com>
2024-02-04 23:42:29 -0500

    WIP

--- src/components/VisualDatasets/associatedOptions.test.ts
+++ src/components/VisualDatasets/associatedOptions.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from 'vitest';
 import EXAMPLE_FILES from "./exampleData.ts";
 import { pairNiftisWithAssociatedOptions } from "./associatedOptions.ts";
 
-test('pairNiftisWithAssociatedOptions', () => {
-  const actual = pairNiftisWithAssociatedOptions(Object.values(EXAMPLE_FILES));;
+test.skip('pairNiftisWithAssociatedOptions', () => {
+  const actual = pairNiftisWithAssociatedOptions(Object.values(EXAMPLE_FILES));
   const expected = [
     { nifti: EXAMPLE_FILES.seragNifti, option: EXAMPLE_FILES.seragSidecar },
     { nifti: EXAMPLE_FILES.otherNifti, option: null },
---
+++ src/components/VisualDatasets/clientHelpers.test.ts
@@ -0,0 +1,36 @@
+import { test, expect } from 'vitest';
+import { findVisualDatasetInstancePairs, reduceListOfObjects } from "./clientHelpers";
+import getPluginInstances from "./testData/feedplugininstancelist";
+
+test('reduceListOfObjects', () => {
+  const data = [
+    {
+      food: [],
+      flavors: ['salty']
+    },
+    {
+      food: ['apple', 'honey'],
+      flavors: ['sweet']
+    },
+    {
+      food: ['toast'],
+      flavors: ['bitter', 'umami']
+    }
+  ];
+  const expected = {
+    food: ['apple', 'honey', 'toast'],
+    flavors: ['salty', 'sweet', 'bitter', 'umami']
+  };
+  const actual = reduceListOfObjects(data);
+  expect(actual).toStrictEqual(expected);
+});
+
+
+test('findVisualDatasetInstancePairs', () => {
+  const plinstList = getPluginInstances();
+  const actual = findVisualDatasetInstancePairs(plinstList.getItems());
+  expect(actual).toHaveLength(1);
+  const { indexPlinst, dataPlinst } = actual[0];
+  expect(indexPlinst.data.plugin_name).toBe('pl-visual-dataset');
+  expect(indexPlinst.data.previous_id).toBe(dataPlinst.data.id);
+});
