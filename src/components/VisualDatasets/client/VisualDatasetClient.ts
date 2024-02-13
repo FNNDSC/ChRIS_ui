2eb3ff2f0cd1465cf14aab1249aafcce8a75472e
Jennings Zhang <jenni_zh@protonmail.com>
2024-02-12 22:11:23 -0500

    Remove feed from VisualDatasetClient

--- src/components/VisualDatasets/client/VisualDatasetClient.ts
+++ src/components/VisualDatasets/client/VisualDatasetClient.ts
@@ -1,13 +1,11 @@
 import { Option } from "fp-ts/Option";
-import { ManifestData } from "./models.ts";
-import { Feed, PluginInstance } from "@fnndsc/chrisapi";
+import { PluginInstance } from "@fnndsc/chrisapi";
 import { FpClient } from "../../../api/fp-chrisapi.ts";
 import { VisualDataset } from "../types.ts";
 import FpFileBrowserFile from "../../../api/fp/fpFileBrowserFile.ts";
 
 class VisualDatasetClient {
   private readonly client: FpClient;
-  private readonly feed: Feed;
   private readonly indexPlinst: PluginInstance;
   private readonly dataPlinst: PluginInstance;
 
@@ -21,7 +19,6 @@ class VisualDatasetClient {
     manifestFile: FpFileBrowserFile,
   ) {
     this.client = client;
-    this.feed = dataset.feed;
     this.dataPlinst = dataset.dataPlinst;
     this.indexPlinst = dataset.indexPlinst;
     this.readmeFile = readmeFile;
