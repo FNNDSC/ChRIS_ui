ce3d76eff3d2bb0aaebfa452591912f0dd64a9e6
Jennings Zhang <jenni_zh@protonmail.com>
2024-02-28 12:05:23 -0500

    Assert canvas size

--- tests/NiivueDatasetViewer/firstPageLoads.spec.ts
+++ tests/NiivueDatasetViewer/firstPageLoads.spec.ts
@@ -13,12 +13,37 @@ loggedOut.test(
   gotoVolumeView,
 );
 
-async function gotoVolumeView({ page }: { page: Page }) {
+async function gotoVolumeView({
+  page,
+  isMobile,
+}: { page: Page; isMobile: boolean }) {
   await page.goto("/");
-  await retryExpandSidebar(page);
+  if (isMobile) {
+    await retryExpandSidebar(page);
+  }
   await page.getByRole("link", { name: "Volume View" }).click();
   await expect(
     page,
     "Volume View redirects immediately to the NiivueDatasetViewer",
   ).toHaveURL(/\/niivue\/\d+.*/);
+
+  const viewportSize = page.viewportSize();
+  if (viewportSize === null) {
+    throw new Error("viewportSize is null");
+  }
+
+  await expect
+    .poll(async () => {
+      const box = await page.locator("canvas").boundingBox();
+      return box?.height || 0;
+    }, "Canvas height should be at least 3/4 of viewport")
+    .toBeGreaterThan((viewportSize.height * 3) / 4);
+  await expect
+    .poll(async () => {
+      const box = await page.locator("canvas").boundingBox();
+      return box?.width || 0;
+    }, "Canvas width should be at least 1/2 of viewport")
+    // A lot of leeway because of a bug.
+    // https://github.com/FNNDSC/ChRIS_ui/issues/1083
+    .toBeGreaterThan(viewportSize.width * 0.4);
 }
