b121b2b1185716cdd82919cda82155bf26c5f9ff
Jennings Zhang <jenni_zh@protonmail.com>
2024-02-29 23:02:58 -0500

    Support for pl-visual-dataset v0.2.3

--- src/datasets/constants.ts
+++ src/datasets/constants.ts
@@ -26,7 +26,13 @@ const constants = {
   /**
    * Versions of `pl-visual-dataset` compatible with this viewer.
    */
-  COMPATIBLE_PL_VISUAL_DATASET_VERSIONS: ["0.1.0", "0.2.0", "0.2.1", "0.2.2"],
+  COMPATIBLE_PL_VISUAL_DATASET_VERSIONS: [
+    "0.1.0",
+    "0.2.0",
+    "0.2.1",
+    "0.2.2",
+    "0.2.3",
+  ],
 };
 
 export default constants;
