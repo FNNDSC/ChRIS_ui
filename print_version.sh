86d50b9997cae4202efb611b4eafb1e156a34761
Jennings Zhang <jenni_zh@protonmail.com>
2024-03-02 12:59:45 -0500

    Print version from commit hash instead of version-0 tag

--- print_version.sh
+++ print_version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # purpose: print out a version string: YYYYMMDD.X-commit(-dirty?)
 #          YYYYMMDD = current date
-#                 X = number of merge commits since the tag version-0
+#                 X = number of merge commits since a specific commit in the past
 #            commit = HEAD commit short sha
 #            -drity = suffix indicating there are uncommitted changes
 
+VERSION_ZERO=28a5018b6f4df807245965315c11580ee7cb1f88
+
 printf "%s.%s-%s%s" \
   "$(date '+%Y%m%d')" \
-  "$(git rev-list --use-bitmap-index --count --merges version-0..HEAD)" \
+  "$(git rev-list --use-bitmap-index --count --merges $VERSION_ZERO..HEAD)" \
   "$(git rev-parse --short HEAD)" \
   "$(git diff --quiet src/ || echo '-dirty')"
