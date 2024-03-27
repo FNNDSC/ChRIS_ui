846d015e6ee975c42849c5be5a0746dc506d81f7
Gideon Pinto <gideonpinto123@gmail.com>
2024-03-26 21:28:59 -0400

    Hotfix: Video support for private files (#1128)
    * hotfix: Fix type mismatch in the VideoDisplay
    
    * refactor: Remove console.log

--- src/components/Preview/displays/VideoDisplay.tsx
+++ src/components/Preview/displays/VideoDisplay.tsx
@@ -12,12 +12,13 @@ const VideoDisplay = (props: AllProps) => {
     : blob
       ? window.URL.createObjectURL(new Blob([blob], { type: blob.type }))
       : "";
-  const sourceType = url ? fileType : blob ? blob.type : "";
+
+  const sourceType = url ? `video/${fileType}` : blob ? blob.type : "";
 
   return (
     // biome-ignore lint/a11y/useMediaCaption: <explanation>
     <video controls width="90%" height="90%">
-      <source src={urlToFetch} type={`video/${sourceType}`} />
+      <source src={urlToFetch} type={sourceType} />
       {/* Fallback message for browsers that do not support video playback */}
       Your browser does not support the video tag.
     </video>
