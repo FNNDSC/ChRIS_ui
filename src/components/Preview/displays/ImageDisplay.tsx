45db7405d60a390b7e0c553ac518e617a848ef6b
Gideon Pinto <gideonpinto123@gmail.com>
2024-03-26 21:10:14 -0400

    feat: Implement preview support for mp4 files (#1127)

--- src/components/Preview/displays/ImageDisplay.tsx
+++ src/components/Preview/displays/ImageDisplay.tsx
@@ -7,9 +7,11 @@ type AllProps = {
 
 const ImageDisplay: React.FunctionComponent<AllProps> = (props: AllProps) => {
   const { fileItem } = props;
-  const url = fileItem.blob
-    ? window.URL.createObjectURL(new Blob([fileItem.blob]))
-    : "";
+  const url = fileItem.url
+    ? fileItem.url
+    : fileItem.blob
+      ? window.URL.createObjectURL(new Blob([fileItem.blob]))
+      : "";
   return (
     <img
       id={props.fileItem.file ? props.fileItem.file.data.fname : ""}
