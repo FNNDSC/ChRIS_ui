e5d341afa82f61183552c6ee7b9600c277280291
Gideon Pinto <gideonpinto123@gmail.com>
2024-03-29 12:40:15 -0400

    feat: Decode redirect URL in login page (#1138)

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -4,11 +4,14 @@ import { useTypedSelector } from "../../store/hooks";
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const isLoggedIn = useTypedSelector(({ user }) => user.isLoggedIn);
   const location = useLocation();
+  const redirectTo = encodeURIComponent(
+    `${location.pathname}${location.search}`,
+  );
 
   return isLoggedIn ? (
     children
   ) : (
-    <Navigate to={`/login?redirectTo=${location.pathname}${location.search}`} />
+    <Navigate to={`/login?redirectTo=${redirectTo}`} />
   );
 };
 
