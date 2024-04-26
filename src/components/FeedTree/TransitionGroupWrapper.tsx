bfabedc247e043d80c665a96e9a8e4c4dc27fcf5
PintoGideon <gideonpinto123@gmail.com>
2024-04-26 09:35:19 -0400

    fix: Fix the rendering of pipelines in the pipelines tree

--- src/components/FeedTree/TransitionGroupWrapper.tsx
+++ src/components/FeedTree/TransitionGroupWrapper.tsx
@@ -8,19 +8,13 @@ type TransitionGroupWrapperProps = {
   children: React.ReactNode;
 };
 
-const TransitionGroupWrapper = React.forwardRef(
-  (props: TransitionGroupWrapperProps, ref: React.Ref<SVGGElement>) => {
-    const innerRef = React.useRef<SVGGElement>(null);
-
-    React.useImperativeHandle(ref, () => innerRef?.current);
-
-    return (
-      <g ref={innerRef} className={props.className} transform={props.transform}>
-        <Marker />
-        {props.children}
-      </g>
-    );
-  },
-);
+const TransitionGroupWrapper = (props: TransitionGroupWrapperProps) => {
+  return (
+    <g className={props.className} transform={props.transform}>
+      <Marker />
+      {props.children}
+    </g>
+  );
+};
 
 export default TransitionGroupWrapper;
