4acf313cadf3540f65f9e0b1e67205923a8730b9
PintoGideon <gideonpinto123@gmail.com>
2024-07-02 12:34:13 -0400

    rebase

---
+++ src/components/NewLibrary/Breadcrumb.tsx
@@ -0,0 +1,37 @@
+import { Breadcrumb, BreadcrumbItem } from "@patternfly/react-core";
+import { HomeIcon } from "../Icons";
+
+const BreadcrumbContainer = ({
+  path,
+  handleFolderClick,
+}: {
+  path: string;
+  handleFolderClick: (path: string) => void;
+}) => {
+  const initialPathSplit = path.split("/");
+
+  return (
+    <Breadcrumb>
+      {initialPathSplit.map((path: string, index) => {
+        return (
+          <BreadcrumbItem
+            onClick={() => {
+              if (index === 0) {
+                handleFolderClick("");
+              } else {
+                const newPath = initialPathSplit.slice(0, index + 1).join("/");
+                handleFolderClick(`/${newPath}`);
+              }
+            }}
+            key={`breadcrumb_${index}`}
+            to="#"
+          >
+            {index === 0 ? <HomeIcon /> : path}
+          </BreadcrumbItem>
+        );
+      })}
+    </Breadcrumb>
+  );
+};
+
+export default BreadcrumbContainer;
