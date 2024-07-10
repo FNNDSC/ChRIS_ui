54b17c1dcba80f2809d5c4805f4d465645f2a57a
PintoGideon <gideonpinto123@gmail.com>
2024-07-10 11:34:57 -0400

    fix: Fix Store Navigation

--- src/components/Wrapper/Sidebar.tsx
+++ src/components/Wrapper/Sidebar.tsx
@@ -17,33 +17,34 @@ import { IUserState } from "../../store/user/types";
 import { useTypedSelector } from "../../store/hooks";
 import { isEmpty } from "lodash";
 
-type AllProps = IUiState & IUserState & ReduxProp;
 type ReduxProp = {
   setSidebarActive: (active: { activeItem: string }) => void;
 };
 
+type AllProps = IUiState & IUserState & ReduxProp;
+
 const Sidebar: React.FC<AllProps> = ({
   isNavOpen,
   sidebarActiveItem,
-}: AllProps) => {
+  setSidebarActive,
+}) => {
   const isLoggedIn = useTypedSelector((state) => state.user.isLoggedIn);
+
   const onSelect = (selectedItem: any) => {
     const { itemId } = selectedItem;
-    if (sidebarActiveItem === itemId) return;
-    setSidebarActive({
-      activeItem: itemId,
-    });
+    if (sidebarActiveItem !== itemId) {
+      setSidebarActive({ activeItem: itemId });
+    }
   };
 
   const urlParam = isLoggedIn ? "private" : "public";
 
-  const renderLink = (to: string, label: string, itemId: string) => {
-    // Don't do this for existing analyses
-    if (sidebarActiveItem !== "analyses" && sidebarActiveItem === itemId) {
-      return <span style={{ color: "gray" }}>{label}</span>;
-    }
-    return <Link to={to}>{label}</Link>;
-  };
+  const renderLink = (to: string, label: string, itemId: string) =>
+    sidebarActiveItem === itemId && sidebarActiveItem !== "analyses" ? (
+      <span style={{ color: "gray" }}>{label}</span>
+    ) : (
+      <Link to={to}>{label}</Link>
+    );
 
   const PageNav = (
     <Nav onSelect={onSelect} aria-label="ChRIS Demo site navigation">
@@ -51,7 +52,6 @@ const Sidebar: React.FC<AllProps> = ({
         <NavItem itemId="overview" isActive={sidebarActiveItem === "overview"}>
           {renderLink("/", "Overview", "overview")}
         </NavItem>
-
         <NavGroup title="Data">
           <NavItem itemId="lib" isActive={sidebarActiveItem === "lib"}>
             {renderLink("/library", "Library", "lib")}
@@ -60,7 +60,6 @@ const Sidebar: React.FC<AllProps> = ({
             {renderLink("/pacs", "PACS Query/Retrieve", "pacs")}
           </NavItem>
         </NavGroup>
-
         <NavGroup title="Analysis">
           <NavItem
             itemId="analyses"
@@ -75,25 +74,64 @@ const Sidebar: React.FC<AllProps> = ({
           <NavItem itemId="catalog" isActive={sidebarActiveItem === "catalog"}>
             {renderLink("/catalog", "Plugins", "catalog")}
           </NavItem>
-
           <NavItem itemId="compute" isActive={sidebarActiveItem === "compute"}>
             {renderLink("/compute", "Compute", "compute")}
           </NavItem>
-
           <NavItem
             itemId="pipelines"
             isActive={sidebarActiveItem === "pipelines"}
           >
             {renderLink("/pipelines", "Pipelines", "pipelines")}
           </NavItem>
-
           <NavItem itemId="dataset" isActive={sidebarActiveItem === "dataset"}>
             {renderLink("/dataset", "Volume View", "dataset")}
           </NavItem>
-
           {!isEmpty(import.meta.env.VITE_CHRIS_STORE_URL) && (
             <NavItem itemId="store" isActive={sidebarActiveItem === "store"}>
               {renderLink("/store", "Store", "store")}
+            </NavItem>
+          )}
+        </NavGroup>
+      </NavList>
+    </Nav>
+  );
+
+  return (
+    <PageSidebar isSidebarOpen={isNavOpen}>
+      <PageSidebarBody>{PageNav}</PageSidebarBody>
+    </PageSidebar>
+  );
+};
+
+const AnonSidebarImpl: React.FC<AllProps> = ({
+  isNavOpen,
+  sidebarActiveItem,
+}) => {
+  const PageNav = (
+    <Nav>
+      <NavList>
+        <NavGroup title="Discover ChRIS">
+          <NavItem
+            itemId="overview"
+            isActive={sidebarActiveItem === "overview"}
+          >
+            <Link to="/">Overview</Link>
+          </NavItem>
+          <NavItem
+            itemId="analyses"
+            isActive={sidebarActiveItem === "analyses"}
+          >
+            <Link to="/feeds?type=public">New and Existing Analyses</Link>
+          </NavItem>
+          <NavItem itemId="catalog" isActive={sidebarActiveItem === "catalog"}>
+            <Link to="/catalog">Plugins</Link>
+          </NavItem>
+          <NavItem itemId="dataset" isActive={sidebarActiveItem === "dataset"}>
+            <Link to="/dataset">Volume View</Link>
+          </NavItem>
+          {!isEmpty(import.meta.env.VITE_CHRIS_STORE_URL) && (
+            <NavItem itemId="store" isActive={sidebarActiveItem === "store"}>
+              <Link to="/store">Store</Link>
             </NavItem>
           )}
         </NavGroup>
@@ -118,51 +156,6 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
     dispatch(setSidebarActive(active)),
 });
 
-const AnonSidebarImpl: React.FC<AllProps> = ({
-  isNavOpen,
-  sidebarActiveItem,
-}: AllProps) => {
-  const PageNav = (
-    <Nav>
-      <NavList>
-        <NavGroup title="Discover ChRIS">
-          <NavItem
-            itemId="analyses"
-            isActive={sidebarActiveItem === "overview"}
-          >
-            <Link to="/">Overview</Link>
-          </NavItem>
-
-          <NavItem
-            itemId="analyses"
-            isActive={sidebarActiveItem === "analyses"}
-          >
-            <Link to="/feeds?type=public">New and Existing Analyses</Link>
-          </NavItem>
-          <NavItem itemId="catalog" isActive={sidebarActiveItem === "catalog"}>
-            <Link to="/catalog">Plugins</Link>
-          </NavItem>
-          <NavItem itemId="dataset" isActive={sidebarActiveItem === "dataset"}>
-            <Link to="/dataset">Volume View</Link>
-          </NavItem>
-
-          {!isEmpty(import.meta.env.VITE_CHRIS_STORE_URL) && (
-            <NavItem itemId="store" isActive={sidebarActiveItem === "store"}>
-              <Link to="store">Store</Link>
-            </NavItem>
-          )}
-        </NavGroup>
-      </NavList>
-    </Nav>
-  );
-
-  return (
-    <PageSidebar isSidebarOpen={isNavOpen}>
-      <PageSidebarBody>{PageNav}</PageSidebarBody>
-    </PageSidebar>
-  );
-};
-
 const AnonSidebar = connect(
   mapStateToProps,
   mapDispatchToProps,
@@ -170,5 +163,4 @@ const AnonSidebar = connect(
 export { AnonSidebar };
 
 const SidebarConnect = connect(mapStateToProps, mapDispatchToProps)(Sidebar);
-
 export default SidebarConnect;
