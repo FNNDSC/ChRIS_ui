03f2b397e33290526236533098b2a38b437b211f
Jennings Zhang <dev@sl.jennin.xyz>
2024-07-12 13:09:20 -0400

    Fix import of viteConfig

--- testing/publicCUBE/vite.config.ts
+++ testing/publicCUBE/vite.config.ts
@@ -1,9 +1,9 @@
 import { mergeConfig } from "vite";
-import viteConfig from './vite.config';
+import viteConfig from "../../vite.config";
 
 // use an existing CUBE running on NERC
 export default mergeConfig(viteConfig, {
-  envDir: './',
+  envDir: "../../",
   server: {
     port: 25173
   }
