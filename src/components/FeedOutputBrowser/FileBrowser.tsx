49b87fc915e8d19733b7621f53cc936a6bc67264
PintoGideon <gideonpinto123@gmail.com>
2024-07-15 15:35:37 -0400

    feat: Update File Browser to use the new api

--- src/components/FeedOutputBrowser/FileBrowser.tsx
+++ src/components/FeedOutputBrowser/FileBrowser.tsx
@@ -1,4 +1,3 @@
-import type { FeedFile } from "@fnndsc/chrisapi";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,18 +8,18 @@ import {
   DrawerPanelBody,
   DrawerPanelContent,
   Grid,
+  Tooltip,
 } from "@patternfly/react-core";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
-import { useMutation } from "@tanstack/react-query";
 import { notification } from "antd";
-import React, { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setFilePreviewPanel } from "../../store/drawer/actions";
 import {
   clearSelectedFile,
   setSelectedFile,
 } from "../../store/explorer/actions";
-import { useTypedSelector } from "../../store/hooks";
+import useDownload, { useTypedSelector } from "../../store/hooks";
 import { ClipboardCopyContainer, SpinContainer } from "../Common";
 import { ThemeContext } from "../DarkTheme/useTheme";
 import { DrawerActionButton } from "../Feeds/DrawerUtils";
@@ -29,156 +28,67 @@ import {
   DownloadIcon,
   FileIcon,
   FileImageIcon,
-  FileTxtIcon,
   FilePdfIcon,
+  FileTxtIcon,
   FolderIcon,
+  HomeIcon,
 } from "../Icons";
 import FileDetailView from "../Preview/FileDetailView";
 import XtkViewer from "../XtkViewer/XtkViewer";
-import { EmptyStateLoader } from "./FeedOutputBrowser";
 import type { FileBrowserProps } from "./types";
 import { bytesToSize } from "./utilities";
 
-const getFileName = (name: string) => {
+export const getFileName = (name: string) => {
   return name.split("/").slice(-1).join("");
 };
 
-let isDownloadInitiated = false;
+const previewAnimation = [{ opacity: "0.0" }, { opacity: "1.0" }];
+
+const previewAnimationTiming = {
+  duration: 1000,
+  iterations: 1,
+};
 
 const FileBrowser = (props: FileBrowserProps) => {
+  const dispatch = useDispatch();
+  const feed = useTypedSelector((state) => state.feed.currentFeed.data);
+  const handleDownloadMutation = useDownload(feed);
+  const [api, contextHolder] = notification.useNotification();
+  const { isSuccess, isError, error: downloadError } = handleDownloadMutation;
   const { isDarkTheme } = useContext(ThemeContext);
   const { pluginFilesPayload, handleFileClick, selected, filesLoading } = props;
   const selectedFile = useTypedSelector((state) => state.explorer.selectedFile);
   const drawerState = useTypedSelector((state) => state.drawers);
-  const feed = useTypedSelector((state) => state.feed.currentFeed.data);
-
-  const dispatch = useDispatch();
-  const [currentRowIndex, setCurrentRowIndex] = React.useState(0);
-  const { files, folders, path } = pluginFilesPayload;
+  const username = useTypedSelector((state) => state.user.username);
+  const { folderFiles, linkFiles, children, path } = pluginFilesPayload;
   const columnNames = {
     name: "Name",
     size: "Size",
     download: "",
   };
-  const items = files && folders ? [...files, ...folders] : [];
-  const { id, plugin_name } = selected.data;
-  const pathSplit = path?.split(`/${plugin_name}_${id}/`);
-  const breadcrumb = path ? pathSplit[1].split("/") : [];
+  const breadcrumb = path.split("/");
+  const currentPath = `home/${username}/feeds/feed_${feed?.data.id}/${selected?.data.plugin_name}_${selected?.data.id}/data`;
 
-  const makeDataSourcePublic = async () => {
-    // Implement logic to make the data source public
-    await feed?.put({
-      //@ts-ignore
-      public: true,
-    });
-  };
-
-  const makeDataSourcePrivate = async () => {
-    // Implement logic to make the data source private again
-    await feed?.put({
-      //@ts-ignore
-      public: false,
-    });
-  };
-
-  const handleDownloadClick = async (item: FeedFile) => {
-    if (item) {
-      const privateFeed = feed?.data.public === false ? true : false;
-      try {
-        const fileName = getFileName(item.data.fname);
-        const link = document.createElement("a");
-
-        const url = item.collection.items[0].links[0].href;
-        if (!url) {
-          throw new Error("Failed to construct the url");
-        }
-
-        // This is highly inconsistent and needs to be investigated further
-        const authorizedUrl = `${url}`; // Add token as a query parameter
-
-        // Make the data source public
-        privateFeed && (await makeDataSourcePublic());
-
-        // Create an anchor element
-
-        link.href = authorizedUrl;
-        link.download = fileName; // Set the download attribute to specify the filename
-        // Append the anchor element to the document body
-        // Listen for the load event on the anchor element
-
-        document.body.appendChild(link);
-        // Programmatically trigger the download
-
-        isDownloadInitiated = true;
-
-        link.click();
-        // Remove the anchor element from the document body after the download is initiated
-        document.body.removeChild(link);
-
-        // Wait for a short delay to ensure download initiation
-        await new Promise((resolve) => setTimeout(resolve, 100));
-
-        // If download is initiated, make the data source private
-        if (isDownloadInitiated && privateFeed) {
-          await makeDataSourcePrivate();
-        }
-        privateFeed && (await makeDataSourcePrivate());
-
-        return item;
-      } catch (e) {
-        throw e;
-      }
-    }
-  };
-
-  const downloadMutation = useMutation({
-    mutationFn: (item: FeedFile) => handleDownloadClick(item),
-  });
-
-  const {
-    isSuccess,
-    isPending,
-    isError,
-    error: downloadError,
-    data,
-  } = downloadMutation;
-  const [api, contextHolder] = notification.useNotification();
-
-  React.useEffect(() => {
-    if (isPending) {
-      api.info({
-        message: "Processing download...",
+  useEffect(() => {
+    if (isSuccess) {
+      api.success({
+        message: "Successfully Triggered the Download",
+        duration: 1,
       });
-    }
-
-    if (data) {
-      const fileName = getFileName(data.data.fname);
-      if (isSuccess) {
-        api.success({
-          message: `Triggered the Download for ${fileName}`,
-        });
-      }
-
-      if (isError) {
-        api.error({
-          message: `Download Error: ${fileName}`,
-          //@ts-ignore
-          description: downloadError.message,
-        });
-      }
 
       setTimeout(() => {
-        downloadMutation.reset();
+        handleDownloadMutation.reset();
       }, 1000);
     }
-  }, [isSuccess, isError, isPending, downloadError]);
 
-  const previewAnimation = [{ opacity: "0.0" }, { opacity: "1.0" }];
-
-  const previewAnimationTiming = {
-    duration: 1000,
-    iterations: 1,
-  };
+    if (isError) {
+      api.error({
+        message: "Download Error",
+        //@ts-ignore
+        description: downloadError.message,
+      });
+    }
+  }, [isSuccess, isError, downloadError]);
 
   const generateBreadcrumb = (value: string, index: number) => {
     const onClick = () => {
@@ -189,20 +99,27 @@ const FileBrowser = (props: FileBrowserProps) => {
       const findIndex = breadcrumb.findIndex((path) => path === value);
       if (findIndex !== -1) {
         const newPathList = breadcrumb.slice(0, findIndex + 1);
-        const combinedPathList = [
-          ...pathSplit[0].split("/"),
-          `${plugin_name}_${id}`,
-          ...newPathList,
-        ];
-        handleFileClick(combinedPathList.join("/"));
+        handleFileClick(newPathList.join("/"));
       }
     };
+
+    // This is somewhat tricky. do not allow the user to click paths before the selected plugin.
+    const disabledIndex = breadcrumb.findIndex(
+      (path) => path === `${selected.data.plugin_name}_${selected.data.id}`,
+    );
+    // If this selected plugin is of the type fs, assume that this is the first node of the tree and could have link files. All the paths
+    // that the user navigates to should not be clickable
+    const shouldNotClick =
+      (disabledIndex > 1 && index <= disabledIndex) ||
+      selected.data.plugin_type === "fs";
 
     return (
       <BreadcrumbItem
         showDivider={true}
         key={index}
-        onClick={onClick}
+        onClick={() => {
+          shouldNotClick ? undefined : onClick();
+        }}
         to={index === breadcrumb.length - 1 ? undefined : "#"}
       >
         {value}
@@ -219,28 +136,15 @@ const FileBrowser = (props: FileBrowserProps) => {
       ?.animate(previewAnimation, previewAnimationTiming);
   };
 
-  const handleItem = (item: string | FeedFile) => {
-    if (typeof item === "string") {
-      handleFileClick(`${path}/${item}`);
-    } else {
+  const handleItem = (item: any, type: string) => {
+    if (type === "link" || type === "folder") {
+      handleFileClick(item.data.path);
+    }
+
+    if (type === "file") {
       toggleAnimation();
       dispatch(setSelectedFile(item));
       !drawerState.preview.open && dispatch(setFilePreviewPanel());
-    }
-  };
-
-  const handleNext = () => {
-    if (currentRowIndex + 1 < items.length) {
-      const item = items[currentRowIndex + 1];
-      setCurrentRowIndex(currentRowIndex + 1);
-      handleItem(item);
-    }
-  };
-  const handlePrevious = () => {
-    if (currentRowIndex - 1 >= 0) {
-      const item = items[currentRowIndex - 1];
-      setCurrentRowIndex(currentRowIndex - 1);
-      handleItem(item);
     }
   };
 
@@ -265,8 +169,6 @@ const FileBrowser = (props: FileBrowserProps) => {
         {drawerState.preview.currentlyActive === "preview" && selectedFile && (
           <FileDetailView
             gallery={true}
-            handleNext={handleNext}
-            handlePrevious={handlePrevious}
             selectedFile={selectedFile}
             preview="large"
             isPublic={feed?.data.public}
@@ -277,8 +179,68 @@ const FileBrowser = (props: FileBrowserProps) => {
     </DrawerPanelContent>
   );
 
+  const tableRowItem = (item: any, type: string) => {
+    let iconType: string;
+    let icon: React.ReactNode = null;
+    let fsize = " ";
+    let fileName = "";
+    iconType = "UNKNOWN FORMAT";
+    const pathList =
+      type === "folder" || type === "link"
+        ? item.data.path.split("/")
+        : item.data.fname.split("/");
+
+    fileName = pathList[pathList.length - 1];
+    if (type === "file" && fileName.indexOf(".") > -1) {
+      iconType = getFileName(fileName)[0].toUpperCase();
+      fsize = bytesToSize(item.data.fsize);
+    } else {
+      iconType = type;
+    }
+    const isPreviewing = selectedFile === item;
+    const backgroundColor = isDarkTheme ? "#002952" : "#E7F1FA";
+
+    icon = getIcon(iconType);
+    const fileNameComponent = (
+      <div
+        className={"file-browser__table--fileName"}
+        style={{
+          background: isPreviewing ? backgroundColor : "",
+        }}
+      >
+        <span>{icon}</span>
+        <span>{fileName}</span>
+      </div>
+    );
+    const downloadComponent =
+      type !== "file" ? undefined : (
+        <Button
+          variant="plain"
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            handleDownloadMutation.mutate(item);
+          }}
+          icon={<DownloadIcon />}
+        />
+      );
+    return (
+      <Tr
+        onClick={() => {
+          handleItem(item, type);
+        }}
+        key={type === "file" ? item.data.fname : item.data.path} // Assuming 'item' has an 'id' property
+      >
+        <Td dataLabel={columnNames.name}>{fileNameComponent}</Td>
+        <Td dataLabel={columnNames.size}>{fsize}</Td>
+        <Td dataLabel={columnNames.download}>{downloadComponent}</Td>
+      </Tr>
+    );
+  };
+
   return (
     <Grid hasGutter className="file-browser">
+      {contextHolder}
       <Drawer position="right" isInline isExpanded={true}>
         <DrawerContent
           panelContent={drawerState.preview.open ? previewPanel : null}
@@ -302,14 +264,19 @@ const FileBrowser = (props: FileBrowserProps) => {
                   <Breadcrumb>{breadcrumb.map(generateBreadcrumb)}</Breadcrumb>
                 </div>
 
-                <div className="file-browser__header__info">
-                  <span className="files-browser__header--fileCount">
-                    {items.length > 1
-                      ? `(${items.length} items)`
-                      : items.length === 1
-                        ? `(${items.length} item)`
-                        : "Empty Directory"}
-                  </span>
+                <div>
+                  {path !== currentPath &&
+                    selected.data.plugin_type === "fs" && (
+                      <Tooltip
+                        content={<span>Go back to the base directory</span>}
+                      >
+                        <Button
+                          onClick={() => handleFileClick(currentPath)}
+                          variant="link"
+                          icon={<HomeIcon />}
+                        />
+                      </Tooltip>
+                    )}
                 </div>
               </div>
               <Table aria-label="file-browser-table" variant="compact">
@@ -322,77 +289,19 @@ const FileBrowser = (props: FileBrowserProps) => {
                 </Thead>
                 {filesLoading ? (
                   <SpinContainer title="Fetching Files for this path..." />
-                ) : !filesLoading && items.length === 0 ? (
-                  <EmptyStateLoader title="Empty Data set" />
                 ) : (
                   <Tbody>
-                    {contextHolder}
-                    {items.map((item: string | FeedFile, index) => {
-                      let type: string;
-                      let icon: React.ReactNode;
-                      let fsize: string;
-                      let fileName: string;
-                      type = "UNKNOWN FORMAT";
-                      const isPreviewing = selectedFile === item;
-
-                      if (typeof item === "string") {
-                        type = "dir";
-                        icon = getIcon(type);
-                        fileName = item;
-                      } else {
-                        fileName = getFileName(item.data.fname);
-                        if (fileName.indexOf(".") > -1) {
-                          type = getFileName(fileName)[0].toUpperCase();
-                        }
-                        fsize = bytesToSize(item.data.fsize);
-                        icon = getIcon(type);
-                      }
-
-                      const backgroundColor = isDarkTheme
-                        ? "#002952"
-                        : "#E7F1FA";
-
-                      const fileNameComponent = (
-                        <div
-                          className={"file-browser__table--fileName"}
-                          style={{
-                            background: isPreviewing ? backgroundColor : "",
-                          }}
-                        >
-                          <span>{icon}</span>
-                          <span>{fileName}</span>
-                        </div>
-                      );
-
-                      const downloadComponent =
-                        typeof item === "string" ? undefined : (
-                          <Button
-                            variant="plain"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              e.preventDefault();
-                              downloadMutation.mutate(item);
-                            }}
-                            icon={<DownloadIcon />}
-                          />
-                        );
-
-                      return (
-                        <Tr
-                          onClick={() => {
-                            handleItem(item);
-                          }}
-                          key={index}
-                        >
-                          <Td dataLabel={columnNames.name}>
-                            {fileNameComponent}
-                          </Td>
-                          <Td dataLabel={columnNames.size}>{fsize}</Td>
-                          <Td dataLabel={columnNames.download}>
-                            {downloadComponent}
-                          </Td>
-                        </Tr>
-                      );
+                    {folderFiles.map((folderFile) => {
+                      const component = tableRowItem(folderFile, "file");
+                      return component;
+                    })}
+                    {linkFiles.map((linkFile) => {
+                      const component = tableRowItem(linkFile, "link");
+                      return component;
+                    })}
+                    {children.map((child) => {
+                      const component = tableRowItem(child, "folder");
+                      return component;
                     })}
                   </Tbody>
                 )}
