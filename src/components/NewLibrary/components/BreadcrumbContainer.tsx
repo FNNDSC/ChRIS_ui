fd296978aecc5f02c76ba3df5f9d022435553f3d
PintoGideon <gideonpinto123@gmail.com>
2024-07-22 12:03:17 -0400

    feat: Breadcrumb to navigate the library

---
+++ src/components/NewLibrary/components/BreadcrumbContainer.tsx
@@ -0,0 +1,57 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Toolbar,
+  ToolbarContent,
+  ToolbarItem,
+} from "@patternfly/react-core";
+
+const BreadcrumbContainer = ({
+  path,
+  handleFolderClick,
+}: {
+  path: string;
+  handleFolderClick: (path: string) => void;
+}) => {
+  // Removing leading and trailing slashes and splitting the path
+  const initialPathSplit =
+    path !== "/" ? path.replace(/^\/|\/$/g, "").split("/") : [];
+
+  const style = {
+    fontSize: "1rem",
+    cursor: "pointer",
+    color: "#1fa7f8",
+  };
+
+  return (
+    <Toolbar>
+      <ToolbarContent>
+        <ToolbarItem>
+          <Breadcrumb>
+            <BreadcrumbItem
+              style={style}
+              onClick={() => handleFolderClick("/library")}
+            >
+              /
+            </BreadcrumbItem>
+
+            {initialPathSplit.map((segment: string, index: number) => {
+              const newPath = `${initialPathSplit.slice(0, index + 1).join("/")}`;
+              return (
+                <BreadcrumbItem
+                  style={style}
+                  key={`breadcrumb-${segment}-${index}`}
+                  onClick={() => handleFolderClick(newPath)}
+                >
+                  {segment}
+                </BreadcrumbItem>
+              );
+            })}
+          </Breadcrumb>
+        </ToolbarItem>
+      </ToolbarContent>
+    </Toolbar>
+  );
+};
+
+export default BreadcrumbContainer;
