b7a3fefbb501e08ad1896b2a9cb723262a4eca27
PintoGideon <gideonpinto123@gmail.com>
2024-07-24 10:54:27 -0400

    feat: Add comments in the pipeline upload file and extract common functionality

--- src/components/PipelinesCopy/utils.ts
+++ src/components/PipelinesCopy/utils.ts
@@ -1,6 +1,5 @@
+import type { Client, Plugin, PluginMeta } from "@fnndsc/chrisapi";
 import axios from "axios";
-import type Client from "@fnndsc/chrisapi";
-import type { Plugin, PluginMeta } from "@fnndsc/chrisapi";
 import { fetchResource } from "../../api/common";
 
 interface Params {
@@ -14,6 +13,14 @@ const defaultParams = {
   limit: 20,
   offset: 0,
 };
+
+/**
+ * Fetches plugin metadata from the ChRIS store.
+ * @param {Client} client - The ChRIS API client.
+ * @param {Params} [params] - Query parameters for fetching plugin metadata.
+ * @returns {Promise<PluginMeta[]>} - A promise that resolves to an array of plugin metadata.
+ * @throws Will throw an error if fetching plugin metadata fails.
+ */
 
 export const fetchPluginMetas = async (client: Client, params?: Params) => {
   const fn = client.getPluginMetas;
@@ -30,6 +37,12 @@ export const fetchPluginMetas = async (client: Client, params?: Params) => {
   }
 };
 
+/**
+ * Fetches plugins associated with a specific plugin metadata.
+ * @param {PluginMeta} pluginMeta - The plugin metadata object.
+ * @returns {Promise<Plugin[]>} - A promise that resolves to an array of plugins.
+ * @throws Will throw an error if fetching plugins associated with the metadata fails.
+ */
 export const fetchPluginForMeta = async (pluginMeta: PluginMeta) => {
   const fn = pluginMeta.getPlugins;
   const boundFn = fn.bind(pluginMeta);
@@ -38,10 +51,34 @@ export const fetchPluginForMeta = async (pluginMeta: PluginMeta) => {
     const { resource: plugins } = await fetchResource(defaultParams, boundFn);
     return plugins as Plugin[];
   } catch (e) {
-    throw new Error("Failed to fetch the plugins assosciated with this meta");
+    throw new Error("Failed to fetch the plugins associated with this meta");
   }
 };
 
+/**
+ * Uploads a pipeline source file to the ChRIS store.
+ * @param {Client} client - The ChRIS API client.
+ * @param {File} file - The file to upload.
+ * @returns {Promise<void>} - A promise that resolves when the file is uploaded.
+ * @throws Will throw an error if the file upload fails.
+ */
+export const uploadPipelineSourceFile = async (client: Client, file: File) => {
+  const url = `${import.meta.env.VITE_CHRIS_UI_URL}pipelines/sourcefiles/`;
+  const formData = new FormData();
+  formData.append("fname", file, file.name);
+  const config = {
+    headers: { Authorization: `Token ${client.auth.token}` },
+  };
+  await axios.post(url, formData, config);
+};
+
+/**
+ * Installs a plugin using admin credentials.
+ * @param {string} adminCred - The admin credentials for authentication.
+ * @param {Plugin} pluginToInstall - The plugin to be installed.
+ * @returns {Promise<any>} - A promise that resolves with the installation response data.
+ * @throws Will throw an error if the plugin installation fails.
+ */
 export const handleInstallPlugin = async (
   adminCred: string,
   pluginToInstall: Plugin,
@@ -76,4 +113,64 @@ export const handleInstallPlugin = async (
       return message;
     }
   }
+};
+
+/**
+ * Fetches a specific plugin from the store by its name and version.
+ * @param {Client} storeClient - The ChRIS store client.
+ * @param {string} pluginMetaName - The name of the plugin metadata.
+ * @param {string} pluginMetaVersion - The version of the plugin metadata.
+ * @returns {Promise<Plugin>} - A promise that resolves to the selected plugin.
+ * @throws Will throw an error if the plugin or its metadata is not found.
+ */
+export const fetchPluginMetasFromStore = async (
+  storeClient: Client,
+  pluginMetaName: string,
+  pluginMetaVersion: string,
+) => {
+  const pluginMetas = await fetchPluginMetas(storeClient, {
+    limit: 1,
+    offset: 0,
+    name_exact: pluginMetaName,
+  });
+
+  if (!pluginMetas || !pluginMetas.length) {
+    throw new Error(`Failed to find ${pluginMetaName} in the store...`);
+  }
+
+  const pluginMeta = pluginMetas[0];
+  const pluginList = await pluginMeta.getPlugins({ limit: 1000 });
+  const plugins = pluginList.getItems();
+
+  if (!plugins) {
+    throw new Error(
+      "Failed to fetch plugins associated with this plugin meta...",
+    );
+  }
+
+  const selectedPlugin = plugins.find(
+    (plugin: Plugin) => plugin.data.version === pluginMetaVersion,
+  );
+  if (!selectedPlugin) {
+    throw new Error(
+      `Failed to find the ${pluginMetaVersion} version of ${pluginMetaName} in the store`,
+    );
+  }
+  return selectedPlugin;
+};
+
+// Function to extract plugin name and version from error message
+export const extractPluginInfo = (
+  errorMessage: string,
+): { name: string; version: string } | null => {
+  // Regular expression to match plugin name and version
+  const regex = /Couldn't find any plugin with name (\S+) and version (\S+)./;
+  const match = errorMessage.match(regex);
+
+  if (match) {
+    const [, name, version] = match;
+    return { name, version };
+  }
+
+  return null;
 };
