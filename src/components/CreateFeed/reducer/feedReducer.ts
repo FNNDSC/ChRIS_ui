3b08807ac4394a0c72cc154ff154b7ccc92d5453
PintoGideon <gideonpinto123@gmail.com>
2024-07-25 13:23:45 -0400

    feat: Refactor feed reducer to use produce from immer

--- src/components/CreateFeed/reducer/feedReducer.ts
+++ src/components/CreateFeed/reducer/feedReducer.ts
@@ -1,11 +1,12 @@
+import { produce } from "immer";
 import {
-  CreateFeedData,
-  CreateFeedActions,
-  CreateFeedState,
+  type CreateFeedData,
+  type CreateFeedActions,
+  type CreateFeedState,
   Types,
 } from "../types/feed";
-import { Key } from "rc-tree/lib/interface";
-import { State as MainRouterContextState } from "../../../routes";
+import type { Key } from "rc-tree/lib/interface";
+import type { State as MainRouterContextState } from "../../../routes";
 
 /*
 import { Series } from "../../../../pages/DataLibrary/Library";
@@ -48,158 +49,120 @@ export function getInitialState(
   };
 }
 
-export const createFeedReducer = (
-  state: CreateFeedState,
-  action: CreateFeedActions,
-): CreateFeedState => {
-  switch (action.type) {
-    case Types.ToggleWizard:
-      return {
-        ...state,
-        wizardOpen: !state.wizardOpen,
-      };
-    case Types.SetStep:
-      return {
-        ...state,
-        step: action.payload.id,
-      };
-    case Types.FeedDescriptionChange:
-      return {
-        ...state,
-        data: { ...state.data, feedDescription: action.payload.value },
-      };
-    case Types.FeedNameChange:
-      return {
-        ...state,
-        data: { ...state.data, feedName: action.payload.value },
-      };
-    case Types.TagsChange:
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          tags: action.payload.tags,
-        },
-      };
-    case Types.SelectedConfig:
-      //  clearCache();
-      return {
-        ...state,
-        selectedConfig: [...action.payload.selectedConfig],
-      };
-    case Types.AddChrisFile: {
-      const file = action.payload.file;
-      const checkedKeysDict: {
-        [key: string]: Key[];
-      } = { ...state.data.checkedKeys };
-
-      if (!state.data.checkedKeys[file]) {
-        checkedKeysDict[file] = action.payload.checkedKeys;
+const createFeedReducer = produce(
+  (draft: CreateFeedState, action: CreateFeedActions) => {
+    switch (action.type) {
+      case Types.ToggleWizard: {
+        draft.wizardOpen = !draft.wizardOpen;
+        break;
       }
 
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          chrisFiles: [...state.data.chrisFiles, action.payload.file],
-          checkedKeys: checkedKeysDict,
-        },
-      };
+      case Types.SetStep: {
+        draft.step = action.payload.id;
+        break;
+      }
+
+      case Types.FeedDescriptionChange: {
+        draft.data.feedDescription = action.payload.value;
+        break;
+      }
+
+      case Types.FeedNameChange: {
+        draft.data.feedName = action.payload.value;
+        break;
+      }
+
+      case Types.TagsChange: {
+        draft.data.tags = action.payload.tags;
+        break;
+      }
+
+      case Types.SelectedConfig: {
+        draft.selectedConfig = [...action.payload.selectedConfig];
+        break;
+      }
+
+      case Types.AddChrisFile: {
+        const file = action.payload.file;
+        const checkedKeysDict: { [key: string]: Key[] } = {
+          ...draft.data.checkedKeys,
+        };
+
+        if (!draft.data.checkedKeys[file]) {
+          checkedKeysDict[file] = action.payload.checkedKeys;
+        }
+
+        draft.data.chrisFiles.push(file);
+        draft.data.checkedKeys = checkedKeysDict;
+        break;
+      }
+
+      case Types.RemoveChrisFile: {
+        draft.data.chrisFiles = draft.data.chrisFiles.filter(
+          (path) => path !== action.payload.file,
+        );
+
+        draft.data.checkedKeys = Object.keys(draft.data.checkedKeys).reduce(
+          (object: { [key: string]: Key[] }, key) => {
+            if (key !== action.payload.file) {
+              object[key] = draft.data.checkedKeys[key];
+            }
+            return object;
+          },
+          {},
+        );
+        break;
+      }
+
+      case Types.ResetChrisFile: {
+        draft.data.chrisFiles = [];
+        draft.data.checkedKeys = {};
+        break;
+      }
+
+      case Types.AddLocalFile: {
+        draft.data.localFiles = action.payload.files;
+        break;
+      }
+
+      case Types.RemoveLocalFile: {
+        draft.data.localFiles = draft.data.localFiles.filter(
+          (file) => file.name !== action.payload.filename,
+        );
+        break;
+      }
+
+      case Types.ResetState: {
+        draft.data = getDefaultCreateFeedData();
+        draft.step = 1;
+        draft.uploadProgress = 0;
+        draft.feedError = {};
+        draft.creatingFeedStatus = "";
+        draft.wizardOpen = false;
+        draft.selectedConfig = [];
+        break;
+      }
+
+      case Types.SetProgress: {
+        draft.uploadProgress = action.payload.value;
+        break;
+      }
+
+      case Types.SetError: {
+        draft.feedError = action.payload.feedError;
+        break;
+      }
+
+      case Types.SetFeedCreationState: {
+        draft.creatingFeedStatus = action.payload.status;
+        break;
+      }
+
+      default:
+        break;
     }
-    case Types.RemoveChrisFile: {
-      let checkedKeysDict: {
-        [key: string]: Key[];
-      } = {};
+  },
+  getInitialState(), // Initialize with the default state
+);
 
-      checkedKeysDict = Object.keys(state.data.checkedKeys).reduce(
-        (object: { [key: string]: Key[] }, key) => {
-          if (key !== action.payload.file) {
-            object[key] = state.data.checkedKeys[key];
-          }
-          return object;
-        },
-        {},
-      );
-
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          chrisFiles: state.data.chrisFiles.filter(
-            (path) => path !== action.payload.file,
-          ),
-          checkedKeys: checkedKeysDict,
-        },
-      };
-    }
-
-    case Types.ResetChrisFile: {
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          chrisFiles: [],
-          checkedKeys: {},
-        },
-      };
-    }
-
-    case Types.AddLocalFile:
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          localFiles: action.payload.files,
-        },
-      };
-
-    case Types.RemoveLocalFile:
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          localFiles: state.data.localFiles.filter(
-            (file) => file.name !== action.payload.filename,
-          ),
-        },
-      };
-
-    case Types.ResetState: {
-      //clearCache();
-      return {
-        ...state,
-        data: getDefaultCreateFeedData(),
-        step: 1,
-        uploadProgress: 0,
-        feedError: {},
-        creatingFeedStatus: "",
-        wizardOpen: false,
-        selectedConfig: [],
-      };
-    }
-
-    case Types.SetProgress: {
-      return {
-        ...state,
-        uploadProgress: action.payload.value,
-      };
-    }
-
-    case Types.SetError: {
-      return {
-        ...state,
-        feedError: action.payload.feedError,
-      };
-    }
-
-    case Types.SetFeedCreationState: {
-      return {
-        ...state,
-        creatingFeedStatus: action.payload.status,
-      };
-    }
-
-    default:
-      return state;
-  }
-};
+export { createFeedReducer };
