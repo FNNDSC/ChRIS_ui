0d3e06cce6d8bb7f7aaef4fa3a6608e672207586
PintoGideon <gideonpinto123@gmail.com>
2024-08-06 12:35:17 -0400

    feat: Add a home icon to the library page

--- src/components/NewLibrary/components/BreadcrumbContainer.tsx
+++ src/components/NewLibrary/components/BreadcrumbContainer.tsx
@@ -1,18 +1,25 @@
 import {
   Breadcrumb,
   BreadcrumbItem,
+  Button,
   Toolbar,
   ToolbarContent,
   ToolbarItem,
+  Tooltip,
 } from "@patternfly/react-core";
+import { useNavigate } from "react-router";
+import { HomeIcon } from "../../Icons";
 
 const BreadcrumbContainer = ({
   path,
   handleFolderClick,
+  username,
 }: {
   path: string;
   handleFolderClick: (path: string) => void;
+  username: string;
 }) => {
+  const navigate = useNavigate();
   // Removing leading and trailing slashes and splitting the path
   const initialPathSplit =
     path !== "/" ? path.replace(/^\/|\/$/g, "").split("/") : [];
@@ -23,34 +30,52 @@ const BreadcrumbContainer = ({
     color: "#1fa7f8",
   };
 
-  return (
-    <Toolbar style={{ paddingTop: "0", paddingLeft: "0", paddingBottom: "0" }}>
-      <ToolbarContent>
-        <ToolbarItem>
-          <Breadcrumb>
-            <BreadcrumbItem
-              style={style}
-              onClick={() => handleFolderClick("/library")}
-            >
-              /
-            </BreadcrumbItem>
+  const showHomeButton = path !== `home/${username}`;
 
-            {initialPathSplit.map((segment: string, index: number) => {
-              const newPath = `${initialPathSplit.slice(0, index + 1).join("/")}`;
-              return (
-                <BreadcrumbItem
-                  style={style}
-                  key={`breadcrumb-${segment}`}
-                  onClick={() => handleFolderClick(newPath)}
-                >
-                  {segment}
-                </BreadcrumbItem>
-              );
-            })}
-          </Breadcrumb>
-        </ToolbarItem>
-      </ToolbarContent>
-    </Toolbar>
+  return (
+    <>
+      <Toolbar
+        style={{ paddingTop: "0", paddingLeft: "0", paddingBottom: "0" }}
+      >
+        <ToolbarContent>
+          <ToolbarItem>
+            <Breadcrumb>
+              <BreadcrumbItem
+                style={style}
+                onClick={() => handleFolderClick("/library")}
+              >
+                /
+              </BreadcrumbItem>
+
+              {initialPathSplit.map((segment: string, index: number) => {
+                const newPath = `${initialPathSplit.slice(0, index + 1).join("/")}`;
+                return (
+                  <BreadcrumbItem
+                    style={style}
+                    key={`breadcrumb-${segment}`}
+                    onClick={() => handleFolderClick(newPath)}
+                  >
+                    {segment}
+                  </BreadcrumbItem>
+                );
+              })}
+            </Breadcrumb>
+          </ToolbarItem>
+        </ToolbarContent>
+      </Toolbar>
+
+      {showHomeButton && (
+        <Tooltip content="Go to your home directory">
+          <Button
+            onClick={() => {
+              navigate(`home/${username}`);
+            }}
+            icon={<HomeIcon />}
+            variant="link"
+          />
+        </Tooltip>
+      )}
+    </>
   );
 };
 
