cb0e54cd701c573ea0cfd71face1f8b5e94cf299
PintoGideon <gideonpinto123@gmail.com>
2024-08-23 11:47:24 -0400

    fix: Set the correct MIME type for the image display when a blob url is created

--- src/components/Preview/displays/ImageDisplay.tsx
+++ src/components/Preview/displays/ImageDisplay.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { IFileBlob } from "../../../api/model";
+import React, { useEffect, useState } from "react";
+import type { IFileBlob } from "../../../api/model";
 
 type AllProps = {
   fileItem: IFileBlob;
@@ -7,16 +7,34 @@ type AllProps = {
 
 const ImageDisplay: React.FunctionComponent<AllProps> = (props: AllProps) => {
   const { fileItem } = props;
-  const url = fileItem.url
-    ? fileItem.url
-    : fileItem.blob
-      ? window.URL.createObjectURL(new Blob([fileItem.blob]))
-      : "";
+  const [url, setUrl] = useState<string>("");
+
+  useEffect(() => {
+    if (fileItem.url) {
+      setUrl(fileItem.url);
+    } else if (fileItem.blob) {
+      // Specify the correct MIME type when creating the Blob
+      const blob = new Blob([fileItem.blob], { type: "image/png" });
+      const objectUrl = URL.createObjectURL(blob);
+      setUrl(objectUrl);
+
+      // Clean up the object URL when the component unmounts
+      return () => {
+        URL.revokeObjectURL(objectUrl);
+      };
+    }
+  }, [fileItem]);
+
+  if (!url) return null;
+
   return (
+    // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
     <img
-      id={props.fileItem.file ? props.fileItem.file.data.fname : ""}
+      id={fileItem.file ? fileItem.file.data.fname : ""}
       src={url}
       alt=""
+      // Prevent default behavior on click
+      onClick={(e) => e.preventDefault()}
     />
   );
 };
