c16518ccc4546d2e97b1f8492f04e3603d949ce3
PintoGideon <gideonpinto123@gmail.com>
2024-09-24 13:46:22 -0400

    feat: update upload saga

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import macrosPlugin from "vite-plugin-babel-macros";
 import IstanbulPlugin from "vite-plugin-istanbul";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // Workaround for the ✘ [ERROR] Failed to resolve entry for package "fs". The package may have incorrect main/module/exports specified in its package.json. [plugin vite:dep-pre-bundle]
+    nodePolyfills({
+      include: ["fs"],
+    }),
     macrosPlugin(), // used for getting version string
     ...(process.env.USE_BABEL_PLUGIN_ISTANBUL
       ? [
