6cb863edc9c2c2ab92a69ab6364b968084005e84
Jennings Zhang <dev@sl.jennin.xyz>
2024-09-24 02:19:15 -0400

    Fix pfdcm tests since removal of fp-ts monads

--- src/api/pfdcm/index.test.ts
+++ src/api/pfdcm/index.test.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "vitest";
 import { PfdcmClient, Configuration } from "./index.ts";
-import { pipe } from "fp-ts/function";
-import * as TE from "fp-ts/TaskEither";
 import { PfdcmEnvironmentalDetailApi } from "./generated";
 
 test("PfdcmClient", async (context) => {
@@ -18,21 +16,13 @@ test("PfdcmClient", async (context) => {
   }
 
   const client = new PfdcmClient(configuration);
-  const getServices = pipe(client.getPacsServices(), TE.toUnion);
-  expect(await getServices()).toContain("MINICHRISORTHANC");
+  expect(await client.getPacsServices()).toContain("MINICHRISORTHANC");
 
-  const query = { patientID: "1449c1d" };
-  const queryAndAssertStudies = pipe(
-    client.query("MINICHRISORTHANC", query),
-    TE.mapLeft((e) => expect(e).toBeNull()),
-    TE.map((list) => {
-      const { study, series } = list[0];
-      expect(study.PatientName).toBe("anonymized");
-      expect(study.PatientBirthDate).toStrictEqual(new Date(2009, 6, 1));
-      expect(series.map((s) => s.SeriesDescription.trim())).toContain(
-        "SAG MPRAGE 220 FOV",
-      );
-    }),
+  const list = await client.query("MINICHRISORTHANC", { patientID: "1449c1d" });
+  const { study, series } = list[0];
+  expect(study.PatientName).toBe("anonymized");
+  expect(study.PatientBirthDate).toStrictEqual(new Date(2009, 6, 1));
+  expect(series.map((s) => s.SeriesDescription.trim())).toContain(
+    "SAG MPRAGE 220 FOV",
   );
-  await queryAndAssertStudies();
 });
