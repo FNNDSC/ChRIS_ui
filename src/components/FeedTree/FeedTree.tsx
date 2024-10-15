1f514253fff74b7ee67915a59a04d1dc4cb1fe33
PintoGideon <gideonpinto123@gmail.com>
2024-10-15 16:08:29 -0400

    fix: Resize the terminal correctly

--- src/components/FeedTree/FeedTree.tsx
+++ src/components/FeedTree/FeedTree.tsx
@@ -142,9 +142,7 @@ const FeedTree = ({
       let newLinks: HierarchyPointLink<TreeNodeDatum>[] = [];
 
       if (data && data.length > 0) {
-        const rootNode = d3Tree(
-          hierarchy(data[0], (d) => (d.__rd3t?.collapsed ? null : d.children)),
-        );
+        const rootNode = d3Tree(hierarchy(data[0], (d) => d.children));
         nodes = rootNode.descendants();
         links = rootNode.links();
 
@@ -270,157 +268,155 @@ const FeedTree = ({
 
   return (
     <div className="feed-tree setFlex grabbable mode_tree" ref={divRef}>
-      <div>
-        {/* Controls Section */}
-        <div className="feed-tree__container--labels">
-          {/* Orientation Toggle */}
-          <div className="feed-tree__orientation">
-            {switchState.orientation === "vertical" ? (
-              <RotateLeft
-                onClick={() => handleChange(Feature.ORIENTATION, "horizontal")}
-                className="feed-tree__orientation--icon"
-                aria-label="Rotate Left"
-                aria-hidden="true"
-              />
-            ) : (
-              <RotateRight
-                className="feed-tree__orientation--icon"
-                aria-label="Rotate Right"
-                aria-hidden="true"
-                onClick={() => handleChange(Feature.ORIENTATION, "vertical")}
-              />
-            )}
-          </div>
-
-          {/* Toggle Labels Switch */}
-          <div className="feed-tree__control">
-            <Switch
-              id="labels"
-              label="Hide Labels"
-              labelOff="Show Labels"
-              isChecked={switchState.toggleLabels}
-              aria-checked={switchState.toggleLabels}
-              onChange={() => handleChange(Feature.TOGGLE_LABELS)}
-              aria-label="Toggle label visibility"
+      {/* Controls Section */}
+      <div className="feed-tree__container--labels">
+        {/* Orientation Toggle */}
+        <div className="feed-tree__orientation">
+          {switchState.orientation === "vertical" ? (
+            <RotateLeft
+              onClick={() => handleChange(Feature.ORIENTATION, "horizontal")}
+              className="feed-tree__orientation--icon"
+              aria-label="Rotate Left"
+              aria-hidden="true"
             />
-          </div>
-
-          {/* Switch Layout Toggle */}
-          <div className="feed-tree__control">
-            <Switch
-              id="layout"
-              label="Switch Layout"
-              labelOff="2D"
-              isChecked={currentLayout}
-              aria-checked={currentLayout}
-              onChange={() => changeLayout()}
-              aria-label="Toggle graph layout from 2D to 3D"
+          ) : (
+            <RotateRight
+              className="feed-tree__orientation--icon"
+              aria-label="Rotate Right"
+              aria-hidden="true"
+              onClick={() => handleChange(Feature.ORIENTATION, "vertical")}
             />
-          </div>
-
-          {/* Scale Nodes Switch and Dropdown */}
-          <div className="feed-tree__control feed-tree__individual-scale">
-            <Switch
-              id="individual-scale"
-              label="Scale Nodes On"
-              labelOff="Scale Nodes Off"
-              isChecked={overlayScale.enabled}
-              aria-checked={overlayScale.enabled}
-              onChange={() => handleChange(Feature.SCALE_ENABLED)}
-              aria-label="Scale nodes"
-            />
-
-            {overlayScale.enabled && (
-              <div className="dropdown-wrap">
-                <NodeScaleDropdown
-                  selected={overlayScale.type}
-                  onChange={(type) => handleChange(Feature.SCALE_TYPE, type)}
-                />
-              </div>
-            )}
-          </div>
-
-          {/* Search Box Switch */}
-          <div className="feed-tree__control">
-            <Switch
-              id="search"
-              label="Search On"
-              labelOff="Search Off"
-              isChecked={switchState.searchBox}
-              aria-checked={switchState.searchBox}
-              onChange={() => handleChange(Feature.SEARCH_BOX)}
-              aria-label="Toggle search a node in the tree"
-            />
-          </div>
-
-          {/* Search Input */}
-          <div className="feed-tree__control">
-            {switchState.searchBox && (
-              <TextInput
-                value={switchState.searchFilter}
-                onChange={(_event, value: string) =>
-                  updateState((draft) => {
-                    draft.switchState.searchFilter = value;
-                  })
-                }
-                aria-label="Search nodes"
-                placeholder="Search..."
-              />
-            )}
-          </div>
+          )}
         </div>
 
-        {/* Tree Visualization */}
-        {treeState.translate.x > 0 && treeState.translate.y > 0 && (
-          <svg
-            ref={svgRef}
-            focusable="true"
-            className={SVG_CLASS_NAME}
-            width="100%"
-            height="100%"
-            role="img"
-            aria-label="Feed Tree Visualization"
-          >
-            <g ref={gRef} className={GRAPH_CLASS_NAME}>
-              <TransitionGroupWrapper
-                className={GRAPH_CLASS_NAME}
-                component="g"
-                transform={`translate(${treeState.translate.x},${treeState.translate.y}) scale(${INITIAL_SCALE})`}
-              >
-                {/* Render Links */}
-                {links?.map((linkData, i) => (
-                  <Link
-                    key={`link${
-                      // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                      i
-                    }`}
-                    orientation={switchState.orientation}
-                    linkData={linkData}
-                    isDarkTheme={isDarkTheme}
-                  />
-                ))}
+        {/* Toggle Labels Switch */}
+        <div className="feed-tree__control">
+          <Switch
+            id="labels"
+            label="Hide Labels"
+            labelOff="Show Labels"
+            isChecked={switchState.toggleLabels}
+            aria-checked={switchState.toggleLabels}
+            onChange={() => handleChange(Feature.TOGGLE_LABELS)}
+            aria-label="Toggle label visibility"
+          />
+        </div>
 
-                {/* Render Nodes */}
-                {nodes?.map(({ data, x, y, parent }) => (
-                  <NodeWrapper
-                    key={`node${data.id}`}
-                    data={data}
-                    position={{ x, y }}
-                    parent={parent}
-                    onNodeClick={(item) => onNodeClick(item)}
-                    orientation={switchState.orientation}
-                    toggleLabel={switchState.toggleLabels}
-                    overlayScale={
-                      overlayScale.enabled ? overlayScale.type : undefined
-                    }
-                    searchFilter={switchState.searchFilter}
-                  />
-                ))}
-              </TransitionGroupWrapper>
-            </g>
-          </svg>
-        )}
+        {/* Switch Layout Toggle */}
+        <div className="feed-tree__control">
+          <Switch
+            id="layout"
+            label="Switch Layout"
+            labelOff="2D"
+            isChecked={currentLayout}
+            aria-checked={currentLayout}
+            onChange={() => changeLayout()}
+            aria-label="Toggle graph layout from 2D to 3D"
+          />
+        </div>
+
+        {/* Scale Nodes Switch and Dropdown */}
+        <div className="feed-tree__control feed-tree__individual-scale">
+          <Switch
+            id="individual-scale"
+            label="Scale Nodes On"
+            labelOff="Scale Nodes Off"
+            isChecked={overlayScale.enabled}
+            aria-checked={overlayScale.enabled}
+            onChange={() => handleChange(Feature.SCALE_ENABLED)}
+            aria-label="Scale nodes"
+          />
+
+          {overlayScale.enabled && (
+            <div className="dropdown-wrap">
+              <NodeScaleDropdown
+                selected={overlayScale.type}
+                onChange={(type) => handleChange(Feature.SCALE_TYPE, type)}
+              />
+            </div>
+          )}
+        </div>
+
+        {/* Search Box Switch */}
+        <div className="feed-tree__control">
+          <Switch
+            id="search"
+            label="Search On"
+            labelOff="Search Off"
+            isChecked={switchState.searchBox}
+            aria-checked={switchState.searchBox}
+            onChange={() => handleChange(Feature.SEARCH_BOX)}
+            aria-label="Toggle search a node in the tree"
+          />
+        </div>
+
+        {/* Search Input */}
+        <div className="feed-tree__control">
+          {switchState.searchBox && (
+            <TextInput
+              value={switchState.searchFilter}
+              onChange={(_event, value: string) =>
+                updateState((draft) => {
+                  draft.switchState.searchFilter = value;
+                })
+              }
+              aria-label="Search nodes"
+              placeholder="Search..."
+            />
+          )}
+        </div>
       </div>
+
+      {/* Tree Visualization */}
+      {treeState.translate.x > 0 && treeState.translate.y > 0 && (
+        <svg
+          ref={svgRef}
+          focusable="true"
+          className={SVG_CLASS_NAME}
+          width="100%"
+          height="100%"
+          role="img"
+          aria-label="Feed Tree Visualization"
+        >
+          <g ref={gRef} className={GRAPH_CLASS_NAME}>
+            <TransitionGroupWrapper
+              className={GRAPH_CLASS_NAME}
+              component="g"
+              transform={`translate(${treeState.translate.x},${treeState.translate.y}) scale(${INITIAL_SCALE})`}
+            >
+              {/* Render Links */}
+              {links?.map((linkData, i) => (
+                <Link
+                  key={`link${
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    i
+                  }`}
+                  orientation={switchState.orientation}
+                  linkData={linkData}
+                  isDarkTheme={isDarkTheme}
+                />
+              ))}
+
+              {/* Render Nodes */}
+              {nodes?.map(({ data, x, y, parent }) => (
+                <NodeWrapper
+                  key={`node${data.id}`}
+                  data={data}
+                  position={{ x, y }}
+                  parent={parent}
+                  onNodeClick={(item) => onNodeClick(item)}
+                  orientation={switchState.orientation}
+                  toggleLabel={switchState.toggleLabels}
+                  overlayScale={
+                    overlayScale.enabled ? overlayScale.type : undefined
+                  }
+                  searchFilter={switchState.searchFilter}
+                />
+              ))}
+            </TransitionGroupWrapper>
+          </g>
+        </svg>
+      )}
     </div>
   );
 };
