6306b24ab919db021f4ba4cdc71292f3c6f5cc4c
PintoGideon <gideonpinto123@gmail.com>
2025-01-16 14:51:22 -0500

    feat: Update terminal resizing

--- src/components/NodeDetails/LogTerminal.tsx
+++ src/components/NodeDetails/LogTerminal.tsx
@@ -11,16 +11,23 @@ const LogTerminal = ({ text }: LogTerminalProps) => {
   );
 
   const containerStyle = {
-    height: "100%",
+    height: isTerminalMaximized ? "100vh" : "100%",
     width: "100%",
     display: "flex",
     flexDirection: "column" as const,
     flexGrow: 1,
   };
 
+  const logViewerStyle = {
+    flexGrow: 1,
+    overflow: "auto",
+    height: "100%",
+    width: "100%",
+  };
+
   return (
     <div style={containerStyle}>
-      <LogViewer hasLineNumbers={false} data={text} />
+      <LogViewer style={logViewerStyle} hasLineNumbers={false} data={text} />
     </div>
   );
 };
