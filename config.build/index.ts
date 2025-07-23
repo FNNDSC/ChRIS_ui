7f65fcf9e1fa79ff47f86d5636999c0b444b8123
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-07-23 09:33:57 -0400

    add config

--- config.build/index.ts
+++ config.build/index.ts
@@ -1,3 +1,14 @@
 export default {
-  API_ROOT: "b87f4fda-012a-4564-8c36-096e496cbedf"
+  API_ROOT: "b87f4fda-012a-4564-8c36-096e496cbedf",
+  PFDCM_ROOT: "700e747e-619f-4007-ad8d-47677c0d0b46",
+  PUBLIC_USER: "fnndsc",
+
+  STORE_ROOT: "79e63221-ccf9-43c3-bf0d-45d05db72c48",
+
+  OHIF_ROOT: "ea1cf042-73f0-43a1-93da-86ff93e5ac19",
+
+  ACKEE_SERVER: "79a96963-2e17-405e-94bd-4f2433e5cce8",
+  ACKEE_DOMAIN_ID: "e8fe722b-986c-4aaf-ba04-c10d21e4aca1",
+
+  CUBE_POLL_INTERVAL_MS: "ca4bef0b-95c9-4a02-abda-590b51b9b07e"
 }
--- config.tmpl/index.ts
+++ config.tmpl/index.ts
@@ -1,4 +1,14 @@
 export default {
   API_ROOT: "http://localhost:8000/api/v1",
-  PFDCM_ROOT: "http://localhost:4005/api/v1"
+  PFDCM_ROOT: "http://localhost:4005/api/v1",
+  PUBLIC_USER: "chris",
+
+  STORE_ROOT: "",
+
+  OHIF_ROOT: "",
+
+  ACKEE_SERVER: "",
+  ACKEE_DOMAIN_ID: "",
+
+  CUBE_POLL_INTERVAL_MS: ""
 }
