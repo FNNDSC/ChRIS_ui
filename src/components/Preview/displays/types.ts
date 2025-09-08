0da9c22740feb151861501edd1a22bb7e3f627d7
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-09-08 15:37:17 -0400

    different scenarios for niivue-viewer.
    img-4096: gray / 0 / 4096
    zmap: viridis / -4 / 4
    label: freesurfer / 0 / 256
    img-256: gray / 0 /  256
    img-65536: gray / 0 / 65536

---
+++ src/components/Preview/displays/types.ts
@@ -0,0 +1,37 @@
+import type { SLICE_TYPE } from "@niivue/niivue";
+
+export enum DisplayType {
+  IMG4096 = "img-4096",
+  ZMap = "zmap",
+  Label = "label",
+  IMG256 = "img-256",
+  IMG65536 = "img-65536",
+  Relative = "relative", // relative to the min-max of the image.
+  Other = "other", // other manual settings.
+}
+
+export enum DisplayColorMap {
+  Gray = "gray",
+  Plasma = "plasma",
+  Viridis = "viridis",
+  Freesurfer = "freesurfer",
+}
+
+export type DisplayTypeMap = {
+  [key: string]: {
+    colorMap: DisplayColorMap;
+    calMin: number;
+    calMax: number;
+  };
+};
+
+export enum SliceType {
+  Axial = "axial",
+  Coronal = "coronal",
+  Sagittal = "sagittal",
+  Multiplanar = "multiplanar",
+}
+
+export type SliceTypeMap = {
+  [key: string]: SLICE_TYPE;
+};
