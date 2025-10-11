a0260933f8235935832863f201af91b08b2db64f
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-10-11 12:04:43 -0400

    update react-reducer-utils to 8.0.0

--- src/components/Pacs/index.tsx
+++ src/components/Pacs/index.tsx
@@ -1,11 +1,6 @@
-import type { ModuleToFunc } from "react-reducer-utils";
-import { useReducer } from "react-reducer-utils";
-import type * as DoPacs from "../../reducers/pacs";
 import { InfoSection } from "../Common";
 import Wrapper from "../Wrapper";
 import PacsApp from "./PacsApp.tsx";
-
-type TDoPacs = ModuleToFunc<typeof DoPacs>;
 
 const PacsTitle = () => (
   <InfoSection
